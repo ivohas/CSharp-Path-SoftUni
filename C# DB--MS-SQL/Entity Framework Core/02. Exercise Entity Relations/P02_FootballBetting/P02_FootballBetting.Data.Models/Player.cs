43912490aefe044364650c38b941dbb1ba316615
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:28 +0300

    Create Player.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Player.cs
@@ -0,0 +1,33 @@
+﻿
+using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Player
+{
+    public Player()
+    {
+        this.PlayerStatistics = new HashSet<PlayerStatistic>();
+    }
+
+    [Key]
+    public int PlayerId { get; set; }
+    [Required]
+    [MaxLength(ValidationConstans.PlayerNameMaxLenght)]
+    public string Name { get; set; } = null!;
+    public bool IsInjured { get; set; }
+    public int SquadNumber { get; set; }
+
+
+    [ForeignKey(nameof(Team))]
+    public int? TeamId { get; set; }
+    public virtual Team? Team { get; set; }
+
+    [ForeignKey(nameof(Position))]
+    public int PositionId { get; set; }
+    public virtual Position Position  { get; set; } = null!;
+
+    public virtual ICollection<PlayerStatistic> PlayerStatistics { get; set; }
+}
