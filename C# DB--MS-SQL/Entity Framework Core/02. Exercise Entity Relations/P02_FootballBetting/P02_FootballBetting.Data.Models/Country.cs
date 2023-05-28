6aea8843c10d70c584da908c1632761b1bca7d0e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:14 +0300

    Create Country.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Country
+{
+    public Country()
+    {
+        this.Towns = new HashSet<Town>();
+    }
+    [Key]
+    public int CountryId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstans.CountryNameMaxLenght)]
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Town> Towns { get; set; }
+    // todo
+}
