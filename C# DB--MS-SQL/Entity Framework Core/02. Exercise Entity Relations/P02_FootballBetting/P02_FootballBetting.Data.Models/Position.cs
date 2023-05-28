582c07d025e86c93b21f775684ef9ffa99d28fcf
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:32 +0300

    Create Position.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Position.cs
@@ -0,0 +1,21 @@
+﻿using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Position
+{
+    public Position()
+    {
+        this.Players = new HashSet<Player>(); 
+    }
+
+    [Key]
+    public int PositionId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstans.PositionNameMaxLenght)]
+    public string Name { get; set; } = null!;
+
+    public  virtual ICollection<Player> Players { get; set; }
+}
