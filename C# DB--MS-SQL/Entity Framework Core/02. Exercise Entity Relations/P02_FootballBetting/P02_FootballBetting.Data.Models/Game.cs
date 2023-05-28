a6eeccd4e03b23a3dbbbdd8692d1a005d127c8e7
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:21 +0300

    Create Game.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Game.cs
@@ -0,0 +1,49 @@
+﻿
+using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Game
+{
+    public Game()
+    {
+        this.PlayerStatistics = new HashSet<PlayerStatistic>();
+        this.Bets = new HashSet<Bet>();
+    }
+    // PR. Key 
+    [Key]
+    public int GameId { get; set; }
+
+
+    // Teams in the game
+    [ForeignKey(nameof(HomeTeam))]
+    public int HomeTeamId { get; set; }
+    public virtual Team HomeTeam { get; set; } = null!;
+
+    [ForeignKey(nameof(AwayTeam))]
+    public int AwayTeamId { get; set; }
+    public virtual Team AwayTeam { get; set; } = null!;
+
+
+    // Gols in the game 
+    public byte HomeTeamGoals { get; set; }
+    public byte AwayTeamGoals { get; set; }
+
+    // Time for teh game
+    public DateTime DateTime { get; set; }
+
+    // Bets 
+    public double HomeTeamBetRate { get; set; }
+    public double AwayTeamBetRate { get; set; }
+    public double DrawBetRate { get; set; }
+
+    // Result 
+    [MaxLength(ValidationConstans.GameResultMaxLenght)]
+    public string? Result { get; set; }
+
+    public virtual ICollection<PlayerStatistic> PlayerStatistics { get; set; }
+
+    public virtual ICollection<Bet> Bets { get; set; }
+}
