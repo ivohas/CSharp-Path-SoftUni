ecb285d54f4b2005ef804079629f87e4e57f16f1
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:34 +0300

    Create Team.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Team.cs
@@ -0,0 +1,54 @@
+﻿using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Team
+{
+    public Team()
+    {
+        this.HomeGames = new HashSet<Game>();
+        this.AwayGame = new HashSet<Game>();
+
+        this.Players = new HashSet<Player>();
+    }
+
+    [Key] // Primary key 
+    public int TeamId { get; set; }
+
+    [Required] // Not null
+    [MaxLength(ValidationConstans.TeamNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [MaxLength(ValidationConstans.TeamLogoUrlMaxLengh)]
+    public string? LogoUrl { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstans.TeamInitialsLenght)]
+     public string Initials { get; set; } = null!;
+
+    public decimal Budget { get; set; }
+
+    [ForeignKey(nameof(PrimaryKitColor))]
+    public int PrimaryKitColorId { get; set; }
+    public virtual Color PrimaryKitColor { get; set; } = null!;
+
+    [ForeignKey(nameof(SecondaryKitColor))]
+    public int SecondaryKitColorId { get; set; } 
+
+    public virtual Color SecondaryKitColor { get; set; }= null!;
+
+    [ForeignKey(nameof(Town))]
+      public int TownId { get; set; }
+    public virtual Town Town { get; set; } = null!;
+
+
+    [InverseProperty(nameof(Game.HomeTeam))]
+    public virtual ICollection<Game> HomeGames { get; set; }
+
+    [InverseProperty(nameof(Game.AwayTeam))]
+    public virtual ICollection<Game> AwayGame { get; set; }
+
+    public virtual ICollection<Player> Players { get; set; }
+}
