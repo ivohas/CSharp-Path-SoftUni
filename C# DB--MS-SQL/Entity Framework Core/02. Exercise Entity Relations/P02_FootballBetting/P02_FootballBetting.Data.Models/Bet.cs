67837b51403c47f6dc91ef03954bf442200515a0
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:11 +0300

    Create Bet.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Bet.cs
@@ -0,0 +1,27 @@
+﻿
+using P02_FootballBetting.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Bet
+{
+    [Key]
+    public int BetId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public Prediction Prediction { get; set; }
+
+    public DateTime DateTime { get; set; }
+
+    [ForeignKey(nameof(User))]
+    public int UserId { get; set; }
+    public virtual User User { get; set; } = null!;
+
+
+    [ForeignKey(nameof(Game))]
+    public int GameId { get; set; }
+    public virtual Game Game { get; set; } = null!;
+}
