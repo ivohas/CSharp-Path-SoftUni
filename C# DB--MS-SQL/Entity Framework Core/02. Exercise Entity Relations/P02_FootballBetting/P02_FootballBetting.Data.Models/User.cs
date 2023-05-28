20ec4d05a4ce37699f23b07ba1d1f0980ce4e7ec
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:38 +0300

    Create User.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,35 @@
+﻿using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class User
+{
+    public User()
+    {
+        this.Bets = new HashSet<Bet>();
+    }
+    [Key]
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstans.UsernameMaxLength)]
+    public string Username { get; set; } = null!;
+    [Required]
+    [MaxLength(ValidationConstans.PasswordMaxLength)]
+    public string Password { get; set; } = null!;
+
+
+    [Required]
+    [MaxLength(ValidationConstans.EmailMaxLenght)]
+    public string Email { get; set; } = null!;
+
+
+    [Required] 
+    [MaxLength(ValidationConstans.UsernameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    public decimal Balance { get; set; }
+
+    public virtual ICollection<Bet>  Bets { get; set; }
+}
