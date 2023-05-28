35d1f5815fa22c8622ac94f9e0a124219b2221ba
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:09 +0300

    Create Color.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Color.cs
@@ -0,0 +1,27 @@
+﻿using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Color
+{
+
+    public Color()
+    {
+        this.SecondaryKitColor = new HashSet<Team>();
+        this.PrimaryKitColor = new HashSet<Team>();
+    }
+    [Key]
+    public int ColorId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstans.ColorTeamMaxLenght)]
+    public string Name { get; set; } = null!;
+
+    // todo relacii
+    [InverseProperty(nameof(Team.PrimaryKitColor))]
+    public virtual ICollection<Team> PrimaryKitColor { get; set; }
+    [InverseProperty(nameof(Team.SecondaryKitColor))]
+    public  virtual ICollection<Team> SecondaryKitColor { get; set; }
+}
