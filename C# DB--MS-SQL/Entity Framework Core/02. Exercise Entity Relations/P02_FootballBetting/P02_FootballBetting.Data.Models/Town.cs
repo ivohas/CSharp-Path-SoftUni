2c3ba51b52a2ae90712000746830fbd9b8b24fc3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-28 17:33:36 +0300

    Create Town.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Town.cs
@@ -0,0 +1,28 @@
+﻿using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Town
+{
+    public Town()
+    {
+        this.Teams = new HashSet<Team>();  
+    }
+
+    [Key]
+    public int TownId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstans.TownNameMaxLengh)]
+    public string Name { get; set; } = null!;
+
+
+    [Required]
+    [ForeignKey(nameof(Country))]
+    public int CountryId { get; set; }
+    public virtual  Country Country { get; set; } = null!;
+
+    public virtual ICollection<Team> Teams { get; set; }
+}
