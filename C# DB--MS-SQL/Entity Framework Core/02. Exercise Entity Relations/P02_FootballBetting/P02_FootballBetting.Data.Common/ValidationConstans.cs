25f434e2c1e91c2a4e12b727517db20e79393b8a
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-29 21:52:20 +0300

    Create ValidationConstans.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Common/ValidationConstans.cs
@@ -0,0 +1,35 @@
+﻿namespace P02_FootballBetting.Data.Common;
+
+public static class ValidationConstans
+{
+    // Team
+    public const int TeamNameMaxLength = 50;
+    public const int TeamLogoUrlMaxLengh = 2048;
+    public const int TeamInitialsLenght = 4;
+
+    // Color
+    public const int ColorTeamMaxLenght = 10;
+
+    // Town
+    public const int TownNameMaxLengh = 58;
+
+    // Country 
+    public const int CountryNameMaxLenght = 56;
+
+    // Player
+    public const int PlayerNameMaxLenght = 100;
+
+    // Position
+    public const int PositionNameMaxLenght = 50;
+
+    // Game
+
+    public const int GameResultMaxLenght = 10;
+    // User
+
+    public const int UsernameMaxLength = 36;
+    public const int PasswordMaxLength = 256;
+    public const int EmailMaxLenght = 256;
+    public const int UserNameMaxLenght = 100;
+}
+
