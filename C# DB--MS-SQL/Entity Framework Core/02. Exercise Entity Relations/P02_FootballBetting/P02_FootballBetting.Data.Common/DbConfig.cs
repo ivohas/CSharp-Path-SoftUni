31ce55136f60f36ca49fd2ab21c0eef357d89908
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-29 21:52:14 +0300

    Create DbConfig.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Common/DbConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace P02_FootballBetting.Data.Common
+{
+    public static class DbConfig
+    {
+        public const string ConnectionString =
+            @"Server=.\SQLEXPRESS;Database=bet388;Integrated Security= True;Encrypt= False;";
+    }
+}
