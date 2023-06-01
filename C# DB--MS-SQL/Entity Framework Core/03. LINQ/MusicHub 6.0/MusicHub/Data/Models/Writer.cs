f1510c6314c6a9a9bb609e70330ea9d97b74c8a8
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-01 17:46:12 +0300

    Create Writer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/03. LINQ/MusicHub 6.0/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models;
+
+ public class Writer
+{
+    public Writer()
+    {
+        this.Songs= new HashSet<Song>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.WriterNameMaxLenght)]
+    public string Name { get; set; } = null!;
+
+    public string? Pseudonym  { get; set; }
+
+    public virtual ICollection<Song> Songs { get; set; }
+}
