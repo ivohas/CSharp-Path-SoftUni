e66fdbcdc4752167ff0fd3cd42980c419928f183
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-01 17:45:40 +0300

    Create Performer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/03. LINQ/MusicHub 6.0/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models;
+
+public class Performer
+{
+    public Performer()
+    {
+        this.PerformerSongs = new HashSet<SongPerformer>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.PerformerFirstNameMaxLenght)]
+    public string FirstName { get; set; } = null!;
+
+    [MaxLength(ValidationConstants.PerformerFirstNameMaxLenght)]
+    public string LastName { get; set; } = null!;
+    public int Age { get; set; }
+
+    public decimal NetWorth { get; set; }
+
+    public virtual ICollection<SongPerformer> PerformerSongs  { get; set; }
+}
