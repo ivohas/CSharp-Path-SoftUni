d80365078a9dcc5d08a6ed7cab162c9e01bcf64f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-01 17:45:43 +0300

    Create Producer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/03. LINQ/MusicHub 6.0/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models;
+
+public class Producer
+{
+    public Producer()
+    {
+        this.Albums = new HashSet<Album>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Pseudonym  { get; set; }
+
+    public string? PhoneNumber  { get; set; }
+
+    public virtual ICollection<Album> Albums { get; set; }
+}
