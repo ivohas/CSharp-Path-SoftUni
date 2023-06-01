9819a1ff05039b770ef1876a5249e0f3ca0425d1
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-01 17:45:33 +0300

    Create Album.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/03. LINQ/MusicHub 6.0/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models;
+
+public  class Album
+{
+    public Album()
+    {
+        this.Songs = new HashSet<Song>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.AlbumNameMaxLenght)]
+    public string Name { get; set; }=null!;
+
+    public DateTime ReleaseDate { get; set; }
+
+    [NotMapped]
+    public decimal Price 
+        => this.Songs.Sum(s => s.Price);
+    // to do 
+
+    [ForeignKey(nameof(Producer))]
+    public int? ProducerId { get; set; }
+
+    public virtual Producer? Producer { get; set; }
+
+    public virtual ICollection<Song> Songs { get; set; }
+}
