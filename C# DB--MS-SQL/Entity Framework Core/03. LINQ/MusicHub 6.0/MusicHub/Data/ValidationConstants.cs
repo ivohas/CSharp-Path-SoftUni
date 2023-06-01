6f175a39a6305e7f552e255302767daa83ac94b9
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-01 17:46:51 +0300

    Create ValidationConstants.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/03. LINQ/MusicHub 6.0/MusicHub/Data/ValidationConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data;
+
+public static class ValidationConstants
+{
+
+    public const int SongNameMaxLenght = 20;
+    public const int AlbumNameMaxLenght = 40;
+    public const int PerformerFirstNameMaxLenght = 20;
+    public const int WriterNameMaxLenght = 20;
+
+
+}
