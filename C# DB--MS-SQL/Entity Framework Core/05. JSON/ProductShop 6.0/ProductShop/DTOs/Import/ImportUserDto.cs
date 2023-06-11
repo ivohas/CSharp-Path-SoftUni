2f65a2598e94233f00fc428d140847357f6ccc97
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-11 03:27:59 +0300

    Create ImportUserDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/05. JSON/ProductShop 6.0/ProductShop/DTOs/Import/ImportUserDto.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Import;
+
+public class ImportUserDto
+{
+    [JsonProperty("firstName")]
+    public string? FirstName { get; set; }
+    [JsonProperty("lastName")]
+    public string LastName { get; set; }
+    [JsonProperty("age")]
+    public string? Age { get; set; }
+}
