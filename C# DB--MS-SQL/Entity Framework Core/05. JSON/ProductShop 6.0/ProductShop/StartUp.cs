b5ec3db7d30a17c2be6fc8feffb825cbafddc7bf
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-12 19:14:31 +0300

    Create StartUp.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/05. JSON/ProductShop 6.0/ProductShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            string inputJson = File
+                .ReadAllText(@"../../../Datasets/users.json");
+            string result = ImportUsers(context, inputJson);
+            Console.WriteLine(result);
+
+
+           
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        { 
+            var mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+            var userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+            ICollection<User> validUsers = new HashSet<User>();
+            foreach (ImportUserDto uD in userDtos)
+            {
+                var user = mapper.Map<User>(uD);
+                validUsers.Add(user);
+            }
+            context.Users.AddRange(validUsers);
+            context.SaveChanges();
+            return $"Successfully imported {validUsers.Count}";
+        }
+    }
+} 
