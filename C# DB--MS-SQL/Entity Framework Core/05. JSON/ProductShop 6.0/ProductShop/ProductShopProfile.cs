b851f78cdc2aa3c963cf2c2bd813490f0ad6754b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-12 19:14:25 +0300

    Create ProductShopProfile.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/05. JSON/ProductShop 6.0/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile() 
+        {
+            this.CreateMap<ImportUserDto, User>();
+        }
+    }
+}
