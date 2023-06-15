083335891da7e1a1bb65adaa7585034f0f60341b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-15 18:06:38 +0300

    Create Cast.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Cast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.Data.Models
+{
+    public class Cast
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public bool IsMainCharacter  { get; set; }
+
+        [Required]
+        [RegularExpression(@"\+44-[0-9]{2}-[0-9]{3}-[0-9]{4}")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [ForeignKey(nameof(Play))]
+        [Required]
+        public int PlayId  { get; set; }
+        public Play Play { get; set; }
+    }
+}
