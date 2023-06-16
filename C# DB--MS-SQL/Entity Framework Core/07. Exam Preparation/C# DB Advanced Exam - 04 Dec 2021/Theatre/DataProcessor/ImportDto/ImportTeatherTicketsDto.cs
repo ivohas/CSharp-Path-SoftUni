097cfa66e565b9d5da3ae8895de59b08d962995f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-16 14:58:48 +0300

    Create ImportTeatherTicketsDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportTeatherTicketsDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportTeatherTicketsDto
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 4)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [Range(typeof(sbyte), "1", "10")]
+        public sbyte NumberOfHalls { get; set; }
+
+        [MaxLength(30)]
+        [MinLength(4)]
+        [Required]
+        public string Director { get; set; } = null!;
+
+        public ImportTicketsDto[] Tickets { get; set; }
+    }
+}
