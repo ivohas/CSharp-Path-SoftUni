22c6a3841de8f426ffd816d8bce7c209100b45f3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-16 14:58:42 +0300

    Create ImportCastDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportCastDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Cast")]
+    public class ImportCastDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        [XmlElement("FullName")]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [XmlElement("IsMainCharacter")]
+        public bool IsMainCharacter { get; set; }
+
+        [Required]
+        [RegularExpression(@"\+44-[0-9]{2}-[0-9]{3}-[0-9]{4}")]
+        [XmlElement("PhoneNumber")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        [XmlElement("PlayId")]
+        public int PlayId { get; set; }
+
+    }
+}
