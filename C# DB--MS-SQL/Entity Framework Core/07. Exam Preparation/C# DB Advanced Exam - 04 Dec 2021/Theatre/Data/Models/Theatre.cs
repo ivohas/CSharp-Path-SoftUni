ed739084611dbea59b152e8b1937b7fe7f048e42
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-16 14:59:32 +0300

    Create Theatre.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.Data.Models
+{
+    public class Theatre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        [MinLength(4)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(10)]
+        [MinLength(1)]
+        [Required]
+        public sbyte NumberOfHalls  { get; set; }
+
+        [MaxLength(30)]
+        [MinLength(4)]
+        [Required]
+        public string Director { get; set; } = null!;
+
+        public ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+   }
+}
