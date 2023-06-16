4e72c094044e8297039e904872a45a2feb0d03d3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-16 14:58:40 +0300

    Create ExportPlayDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ExportDto/ExportPlayDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ExportDto
+{
+    [XmlType("Play")]
+    public class ExportPlayDto
+    {
+        [XmlAttribute("Title")]
+        public string Title { get; set; } = null!;
+        [XmlAttribute("Duration")]
+        public string Duration { get; set; }
+        [XmlAttribute("Rating")]
+        public string Rating { get; set; }
+
+        [XmlAttribute("Genre")]
+        public string Genre { get; set; }
+
+        [XmlArray("Actors")]
+        public ExportActorsDto[] Actors { get; set; }
+    }
+
+    [XmlType("Actor")]
+    public class ExportActorsDto {
+
+        [XmlAttribute("FullName")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("MainCharacter")]
+        public string MainCharacter { get; set; }
+    }
+}
