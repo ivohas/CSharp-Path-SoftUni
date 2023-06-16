2a4726afe3ad3ea6492e51d365e2ed6d8e9428a3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-16 14:58:45 +0300

    Create ImportPlayDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportPlayDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Play")]
+    public class ImportPlayDto
+    {
+
+        [XmlElement("Title")]
+        [MaxLength(50)]
+        [MinLength(4)]
+        [Required]
+        public string Title { get; set; } = null!;
+
+        // Min Span 
+        [Required]
+        [XmlElement("Duration")]
+        public string Duration { get; set; }
+
+        [Range(0.00, 10.00)]
+        [Required]
+        [XmlElement("Raiting")]
+        // May couze error
+        public float Rating { get; set; }
+
+        [XmlElement("Genre")]
+        public string Genre { get; set; }
+
+        [MaxLength(700)]
+        [Required]
+        [XmlElement("Description")]
+        public string Description { get; set; } = null!;
+
+        [MaxLength(30)]
+        [MinLength(4)]
+        [Required]
+        [XmlElement("Screenwriter")]
+        public string Screenwriter { get; set; } = null!;
+
+    }
+}
