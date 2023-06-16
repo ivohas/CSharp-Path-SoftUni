87f03e05cccaccb1a3bfca423013a203b8cbb124
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-16 14:58:50 +0300

    Create ImportTicketsDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportTicketsDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportTicketsDto
+    {
+        [Required]
+        [Range(typeof(decimal), "1.00", "100.00")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(typeof(sbyte), "1", "10")]
+        public sbyte RowNumber { get; set; }
+
+        public int PlayId { get; set; }
+    }
+}
