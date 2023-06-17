3c3eb1f63248f5e69d834bb2ef2b93a65f2b9898
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-17 10:24:25 +0300

    Create Ticket.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.Data.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "1.00", "100.00")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1,10)]
+        public sbyte RowNumber  { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Play))]
+        public int PlayId  { get; set; }
+
+        public Play Play { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Theatre))]
+        public int TheatreId  { get; set; }
+
+        public Theatre Theatre { get; set; }
+    }
+}
