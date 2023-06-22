ec6d86081f10a1a9dca9db001fc33f0d61d11a2f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:31 +0300

    Create PositionType.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Enums/PositionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.Data.Models.Enums
+{
+
+    [XmlType(IncludeInSchema =true)]
+    public enum PositionType
+    {
+        Goalkeeper=0,
+        Defender=1, 
+        Midfielder=2, 
+        Forward=3
+
+    }
+}
