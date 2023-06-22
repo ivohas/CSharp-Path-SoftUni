8a8b7152151b4f95099e9305748de1ba072f992f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:56 +0300

    Create TeamDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/TeamDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class TeamDto
+    {
+
+        public string? Name { get; set; } = null!;
+        public string? Nationality { get; set; } = null!;
+        public string? Trophies { get; set; }
+        public int[]? Footballers { get; set; }
+    }
+}
