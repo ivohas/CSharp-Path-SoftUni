5c96d49b95737a6e11be1e5f978d214ca16bb2e6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:58 +0300

    Create Serializer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/Serializer.cs
@@ -0,0 +1,46 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+
+    public class Serializer
+    {
+        public static string ExportCoachesWithTheirFootballers(FootballersContext context)
+        {
+            return "";
+        }
+
+        public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
+        {
+           var teams = context
+                .Teams
+                .Where(t => t.TeamsFootballers.Any(tf => tf.Footballer.ContractStartDate >= date))
+                .ToArray()
+                .Select(t => new
+                {
+                    t.Name,
+                    Footballers = t.TeamsFootballers
+                        .Where(tf => tf.Footballer.ContractStartDate >= date)
+                        .ToArray()
+                        .OrderByDescending(tf => tf.Footballer.ContractEndDate)
+                        .ThenBy(tf => tf.Footballer.Name)
+                        .Select(tf => new
+                        {
+                            FootballerName = tf.Footballer.Name,
+                            ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                            ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                            BestSkillType = tf.Footballer.BestSkillType.ToString(),
+                            PositionType = tf.Footballer.PositionType.ToString()
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(t => t.Footballers.Length)
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+            return JsonConvert.SerializeObject(teams, Formatting.Indented);
+        }
+    }
+}
