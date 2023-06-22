b72726d9bfdd8a2a04b52e3b5c69eacbd8f3f472
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:44 +0300

    Create CoachCollection.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/CoachCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlRoot("Coaches")]
+    public class CoachCollection
+    {
+        [XmlArray("Coaches")]
+        [XmlArrayItem("Coach")]
+        public CoachDto[] Coaches { get; set; }
+    }
+}
