ffe52e85530f73ebdefd935303c190c8c1f438f0
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:36 +0300

    Create Team.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        [RegularExpression(@"^[a-zA-Z0-9\s\.\-]+$")]
+        // More atributes
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public int Trophies  { get; set; }
+
+        public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; } = new HashSet<TeamFootballer>();
+    }
+}
