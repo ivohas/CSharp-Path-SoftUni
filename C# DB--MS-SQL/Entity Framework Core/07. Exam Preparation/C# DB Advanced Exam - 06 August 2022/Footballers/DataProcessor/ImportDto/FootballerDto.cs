7a027071ddb770511ed48543a59ae1aa77e0c3a6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:48 +0300

    Create FootballerDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/FootballerDto.cs
@@ -0,0 +1,31 @@
+﻿using Footballers.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class FootballerDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string? Name { get; set; }
+
+        [XmlElement("ContractStartDate")]
+        [Required]
+        public string? ContractStartDate { get; set; }
+
+        [XmlElement("ContractEndDate")]
+        public string? ContractEndDate { get; set; }
+
+        [XmlElement("PositionType")]
+        [Required]
+        public int PositionType { get; set; }
+
+        [XmlElement("BestSkillType")]
+        [Required]
+        public int BestSkillType { get; set; }
+
+    }
+}
