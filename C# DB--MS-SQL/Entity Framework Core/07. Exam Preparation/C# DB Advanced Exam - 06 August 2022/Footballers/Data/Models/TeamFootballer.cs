a3a2aae31a2f5550169e428ea5c309660da6eaa6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:40 +0300

    Create TeamFootballer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/TeamFootballer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public class TeamFootballer
+    {
+        [Required]
+        [ForeignKey(nameof(TeamId))]
+        public int TeamId  { get; set; }
+
+        public virtual  Team Team { get; set; }
+
+        [ForeignKey(nameof(FootballerId))]
+        public int FootballerId  { get; set; }
+
+        public virtual Footballer Footballer { get; set; }
+    }
+}
