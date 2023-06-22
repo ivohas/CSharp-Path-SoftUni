e716f999516ba529d10e41cf69c2c8b62119af86
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:46 +0300

    Create CoachDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/CoachDto.cs
@@ -0,0 +1,31 @@
+﻿using Footballers.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+
+    public class CoachDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string? Name { get; set; }
+
+        [XmlElement("Nationality")]
+        [Required]
+        public string? Nationality { get; set; }
+
+
+        [XmlArray("Footballers")]
+        [XmlArrayItem("Footballer")]
+        public virtual FootballerDto[] Footballers { get; set; }
+    }
+}
