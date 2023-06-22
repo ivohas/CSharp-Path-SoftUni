4f38faf0e1bb636a326b3d66daf026add363da10
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:55:49 +0300

    Create BestSkillType.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Enums/BestSkillType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.Data.Models.Enums
+{
+    [XmlType(IncludeInSchema =true)]
+    public enum BestSkillType
+    {
+        Defence=0,
+        Dribble=1,
+        Pass=2, 
+        Shoot=3,
+        Speed=4
+
+    }
+}
