31af9a821c8b8fd5ad0eff040fcda1a2b9687b2d
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:29 +0300

    Create Coach.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public class Coach
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string Name { get; set; } = null!;
+
+        [Required(AllowEmptyStrings =false)]
+        public string Nationality { get; set; } = null!;
+
+        public virtual ICollection<Footballer> Footballers { get; set; } = new HashSet<Footballer>();
+    }
+}
