afacb9647f692d890bb8261ba36b76fb98a4e69d
ivohas <105879092+ivohas@users.noreply.github.com>
2023-06-22 17:56:34 +0300

    Create Footballer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Footballer.cs
@@ -0,0 +1,42 @@
+﻿using Footballers.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public class Footballer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public DateTime ContractStartDate  { get; set; }
+
+        public DateTime ContractEndDate  { get; set; }
+
+        [Required]
+        public PositionType PositionType { get; set; }
+
+        [Required]
+        public BestSkillType BestSkillType { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CoachId))]
+        // Foreign key 
+        public int CoachId  { get; set; }
+
+        public virtual Coach Coach { get; set; } = null!;
+
+        public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; } = new HashSet<TeamFootballer>();
+    }
+}
