11df71b61e037e26a32f0d4625740e624bbacc57
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-03 23:04:36 +0300

    Create Client.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/Data/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public string Type { get; set; } = null!;
+
+       public ICollection<ClientTruck> ClientsTrucks { get; set; } = new HashSet<ClientTruck>();
+    }
+}
