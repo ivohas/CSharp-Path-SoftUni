4a50b5d9bce2727521fb96b63218eeb0d38f28c6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:08:06 +0300

    Create Truck.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.Data.Models
+{
+    public class Truck
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(8)]
+     
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        public string VinNumber  { get; set; }
+
+        [Range(950,1420)]
+        public int TankCapacity  { get; set; }
+
+        [Range(5000,29000)]
+        public int CargoCapacity  { get; set; }
+
+        [Required]
+        public CategoryType CategoryType  { get; set; }
+
+        public MakeType MakeType  { get; set; }
+
+        [Required]
+        public int DespatcherId  { get; set; }
+
+        public Despatcher Despatcher { get; set; }
+
+        public ICollection<ClientTruck> ClientsTrucks { get; set; } = new HashSet<ClientTruck>();
+    }
+}
