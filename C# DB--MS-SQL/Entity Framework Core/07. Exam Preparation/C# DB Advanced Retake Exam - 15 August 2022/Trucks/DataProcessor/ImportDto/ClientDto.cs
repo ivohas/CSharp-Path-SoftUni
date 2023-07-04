a8a5d02fde46fdb0854c799418c1501174f547b1
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:08:19 +0300

    Create ClientDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ImportDto/ClientDto.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+ 
+    public class ClientDto
+    {
+
+        
+        [JsonProperty("Name")]
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        public string Name { get; set; } 
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [JsonProperty("Nationality")]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        [JsonProperty("Type")]
+        public string Type { get; set; } = null!;
+
+        [JsonProperty("Trucks")]
+        public int[] TrucksId { get; set; }
+    }
+}
