7a2b9669cd13fe8c4ba91ff04adf6dfbf5795420
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:08:23 +0300

    Create TruckDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ImportDto/TruckDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    [XmlType("Truck")]
+    public class TruckDto
+    {
+
+        [XmlElement("RegistrationNumber")]
+        [MaxLength(8)]
+        [MinLength(8)]
+        [RegularExpression(@"[A-Z]{2}\d{4}[A-Z]{2}")]
+        public string? RegistrationNumber { get; set; }
+
+        [MinLength(17)]
+        [MaxLength(17)]
+        [Required]
+        [XmlElement("VinNumber")]
+        public string VinNumber { get; set; } = null!;
+
+        [Range(950, 1420)]
+        [XmlElement("TankCapacity")]
+        public int TankCapacity { get; set; }
+
+        [Range(5000, 29000)]
+        [XmlElement("CargoCapacity")]
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        [XmlElement("CategoryType")]
+        [Range(0, 3)]
+        public int CategoryType { get; set; }
+
+        [XmlElement("MakeType")]
+        [Required]
+        [Range(0,4)]
+        public int MakeType { get; set; }      
+    }
+}
