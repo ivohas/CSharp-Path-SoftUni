c6913a84e96cb4bc3e83ab41db473fb451e5c751
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:08:21 +0300

    Create DespatcherDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ImportDto/DespatcherDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    [XmlType("Despatcher")]
+    public class DespatcherDto
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("Position")]
+        [Required]
+        public string Position { get; set; } = null!;
+
+        [XmlArray("Trucks")]
+        public TruckDto[] Trucks { get; set; }
+    }
+}
