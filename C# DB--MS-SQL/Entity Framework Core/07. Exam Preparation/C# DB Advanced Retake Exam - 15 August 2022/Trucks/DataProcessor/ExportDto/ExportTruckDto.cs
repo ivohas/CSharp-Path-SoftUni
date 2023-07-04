4940468451060b1a1dec21c32245f52eef68dd12
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:08:13 +0300

    Create ExportTruckDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ExportDto/ExportTruckDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Truck")]
+    public class ExportTruckDto
+    {
+        [XmlElement("RegistrationNumber")]
+        public string RegistrationNumber { get; set; } = null!;
+        [XmlElement("Make")]
+        public string Make { get; set; }= null!;
+    }
+}
