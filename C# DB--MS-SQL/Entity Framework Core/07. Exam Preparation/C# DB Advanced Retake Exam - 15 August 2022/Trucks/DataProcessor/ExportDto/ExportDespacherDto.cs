a67ef554a4c2c10a0951ed2ae986672cc3dbca53
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:08:10 +0300

    Create ExportDespacherDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ExportDto/ExportDespacherDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Despatcher")]
+    public class ExportDespacherDto
+    {
+        [XmlElement("DespatcherName")]
+        public string DespatcherName { get; set; } = null!;
+
+        [XmlAttribute("TrucksCount")]
+        public int TrucksCount { get; set; }
+
+        [XmlArray("Trucks")]
+        public ExportTruckDto[] Trucks { get; set; } = null!;
+    }
+}
