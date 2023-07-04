2bf96f79d0c886e077d13e9c491728cdbe1c12cd
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-04 19:07:59 +0300

    Create TrucksContext.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/Data/TrucksContext.cs
@@ -0,0 +1,41 @@
+﻿namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Trucks.Data.Models;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext()
+        { 
+        }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClientTruck>()
+                .HasKey(pk => new { pk.TruckId, pk.ClientId });
+
+        }
+
+        public DbSet<ClientTruck> ClientsTrucks { get; set; }
+        public DbSet<Despatcher> Despatchers { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Truck> Trucks { get; set; }
+    }
+}
