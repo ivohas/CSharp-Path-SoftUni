df7b56fa4b814c5dd1d7e6630461771802e4efb2
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-15 13:03:16 +0300

    Create Despatcher.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class Despatcher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        public string? Position { get; set; }
+
+        public ICollection<Truck> Trucks { get; set; } = new HashSet<Truck>();
+    }
+}
