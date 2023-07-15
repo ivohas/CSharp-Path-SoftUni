20082c0ec29690601ae447e71f644155ff7c0c90
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-15 13:03:14 +0300

    Create ClientTruck.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 15 August 2022/Trucks/Data/Models/ClientTruck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class ClientTruck
+    {
+        [Required]
+        [ForeignKey(nameof(ClientId))]
+        public int ClientId  { get; set; }
+       
+        public Client? Client { get; set; }
+
+        [ForeignKey(nameof(TruckId))]
+        [Required]
+        public int TruckId  { get; set; }
+
+        public Truck? Truck { get; set; }
+    }
+}
