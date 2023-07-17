9f378bf0e45eb2cdcbf716ac3250301ede01b035
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:24 +0300

    Create Shell.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Shell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Range(2, 1_680)]
+        [Required]
+        public double ShellWeight { get; set; }
+
+        [MinLength(4)]
+        [MaxLength(30)]
+        [Required]
+        public string Caliber { get; set; } = null!;
+
+        public virtual ICollection<Gun> Guns { get; set; } = new HashSet<Gun>();
+    }
+}
