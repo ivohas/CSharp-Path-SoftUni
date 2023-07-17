04a50f419fcf019cb6943efa0dc210bdeb4999db
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:47 +0300

    Create ImportManufacturerDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/DataProcessor/ImportDto/ImportManufacturerDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Manufacturer")]
+    public class ImportManufacturerDto
+    {
+
+ 
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(40)]
+        
+        // Unique
+        [XmlElement("ManufacturerName")]
+        public string ManufacturerName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(10)]
+        [XmlElement("Founded")]
+        public string Founded { get; set; } = null!;
+    }
+}
