94d152762d48d65d01ec286f50af3e5977f8e072
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:05:53 +0300

    Create ArtilleryContext.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/ArtilleryContext.cs
@@ -0,0 +1,46 @@
+﻿namespace Artillery.Data
+{
+    using Artillery.Data.Models;
+    using Artillery.DataProcessor.ImportDto;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    public class ArtilleryContext : DbContext
+    {
+        public ArtilleryContext() 
+        { 
+        }
+
+        public ArtilleryContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CountryGun>(g =>
+            {
+                g.HasKey(cg => new { cg.CountryId, cg.GunId });
+            });
+
+
+            
+        }
+
+        public DbSet<CountryGun> CountriesGuns { get; set; }
+        public DbSet<Gun> Guns { get; set; }
+        public DbSet<Shell> Shells { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
