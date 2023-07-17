da47b366ac5fdfe1bb2da917f8f268fae780a090
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:05:58 +0300

    Create Configuration.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Artillery.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.\SQLEXPRESS;Database=Artillery;Integrated Security=True;Encrypt=False";
+    }
+}
