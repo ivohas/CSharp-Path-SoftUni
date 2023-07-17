096334cec3adcc2e572ad0632d03f4d5fca110ed
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:38 +0300

    Create ImportCountriesDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/DataProcessor/ImportDto/ImportCountriesDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Country")]
+    public class ImportCountriesDto
+    { 
+         
+    [MaxLength(60)]
+    [MinLength(4)]
+    [Required]
+    [XmlElement("CountryName")]
+    public string CountryName { get; set; } = null!;
+
+    [Required]
+  
+    [Range(50_000, 10_000_000)]
+    [XmlElement("ArmySize")]
+    public int ArmySize { get; set; }
+    
+    }
+}
