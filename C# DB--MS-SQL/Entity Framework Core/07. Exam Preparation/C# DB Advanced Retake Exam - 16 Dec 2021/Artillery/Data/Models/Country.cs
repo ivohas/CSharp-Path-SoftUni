d3385fd996ab39cfdb8e34219edff832c15073b7
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:02 +0300

    Create Country.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(60)]
+        [MinLength(4)]
+        [Required]
+        public string CountryName { get; set; } = null!;
+
+        [Required]
+        [Range(50_000, 10_000_000)]
+        public int ArmySize { get; set; }
+
+       public virtual ICollection<CountryGun> CountriesGuns { get; set; } = new HashSet<CountryGun>(); 
+    }
+}
