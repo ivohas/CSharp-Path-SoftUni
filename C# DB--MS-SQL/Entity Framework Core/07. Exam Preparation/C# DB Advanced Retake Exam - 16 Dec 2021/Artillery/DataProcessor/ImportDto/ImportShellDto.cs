f32e9de47b0995ede219c9803c8c2a77aa439438
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:51 +0300

    Create ImportShellDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/DataProcessor/ImportDto/ImportShellDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Shell")]
+    public class ImportShellDto
+    {
+
+
+
+        [Range(2, 1_680)]
+        [XmlElement("ShellWeight")]
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 4)]
+        [XmlElement("Caliber")]
+        public string Caliber { get; set; }
+    }
+}
