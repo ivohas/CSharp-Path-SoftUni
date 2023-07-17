bf8c55d1f28dc4c781630dd52cf86b19bfe8ae6d
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:34 +0300

    Create GunsDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/DataProcessor/ImportDto/GunsDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    public class GunsDto
+    {
+
+        public int ManufacturerId { get; set; }
+        [Range(100, 1_350_000)]
+        public int GunWeight { get; set; }
+
+        [Range(2.00, 35.00)]
+        public double BarrelLength { get; set; }
+
+        public int? NumberBuild { get; set; }
+        [Range(1, 100_000)]
+        public int Range { get; set; }
+
+        [Required]
+        public string GunType { get; set; }
+
+        public int ShellId { get; set; }
+
+        public ImportCountryGunDto[] Countries { get; set; }
+    }
+}
