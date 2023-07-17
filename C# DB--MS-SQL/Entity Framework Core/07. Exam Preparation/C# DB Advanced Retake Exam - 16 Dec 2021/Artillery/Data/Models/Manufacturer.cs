81154aebd1826941e278621f3ee4c7442e021cef
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:21 +0300

    Create Manufacturer.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(40)]
+        // Unique
+        public string ManufacturerName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(10)]
+        public string Founded { get; set; } = null!;
+
+        public virtual ICollection<Gun> Guns { get; set; } = new HashSet<Gun>();
+    }
+}
