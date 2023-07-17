271cbe3ca2110adefe604ca26861aba61cfec3e6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-17 17:06:17 +0300

    Create Gun.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/07. Exam Preparation/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Gun.cs
@@ -0,0 +1,47 @@
+﻿using Artillery.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Gun
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Manufacturer))]
+        public int ManufacturerId  { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+
+        [Required]
+        [Range(100, 1_350_000)]
+        public int GunWeight { get; set; }
+
+        [Range(2.00, 35.00)]
+        [Required]
+        public double BarrelLength  { get; set; }
+
+        public int? NumberBuild  { get; set; }
+
+        [Required]
+        [Range(1, 100_000)]
+        public int Range  { get; set; }
+
+        [Required]
+        public GunType GunType { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Shell))]
+        public int ShellId  { get; set; }
+
+        public Shell Shell { get; set; }
+
+        public virtual ICollection<CountryGun> CountriesGuns { get; set; } = new HashSet<CountryGun>();
+    }
+}
