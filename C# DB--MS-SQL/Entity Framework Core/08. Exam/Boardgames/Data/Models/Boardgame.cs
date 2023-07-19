ba176a3c4990ebf5257fd26aa5bcb282f61bddd8
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:40 +0300

    Create Boardgame.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/Data/Models/Boardgame.cs
@@ -0,0 +1,43 @@
+﻿using Boardgames.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.Data.Models
+{
+    public class Boardgame
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [MinLength(10)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Range(1.00,10.00)]
+        [Required]
+        public double Rating  { get; set; }
+
+        [Range(2018,2023)]
+        [Required]
+        public int YearPublished  { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+        [Required]
+        public string Mechanics { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Creator))]
+        public int CreatorId  { get; set; }
+
+        public Creator Creator { get; set; }
+
+        public ICollection<BoardgameSeller> BoardgamesSellers { get; set; } = new HashSet<BoardgameSeller>();
+    }
+}
