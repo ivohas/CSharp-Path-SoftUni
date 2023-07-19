51641d512eb1d5d9ef8fe589832b7760797a4edf
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:27:03 +0300

    Create SellerDtoImport.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/DataProcessor/ImportDto/SellerDtoImport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    public class SellerDtoImport
+    {
+        [MaxLength(20)]
+        [MinLength(5)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(30)]
+        [MinLength(2)]
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public string Country { get; set; } = null!;
+
+
+        [Required]
+        [RegularExpression(@"^www\.[a-zA-Z0-9-]+\.com$")]
+        public string Website { get; set; } = null!;
+
+
+        public int[] Boardgames { get; set; }
+    }
+}
