4a6d0160dbc204dea3c06630894bf259ddea0ec5
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:44 +0300

    Create Creator.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/Data/Models/Creator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.Data.Models
+{
+    public class Creator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(7)]
+        [MinLength(2)]
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [MaxLength(7)]
+        [MinLength(2)]
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        public ICollection<Boardgame> Boardgames  { get; set; } = new HashSet<Boardgame>();
+    }
+}
