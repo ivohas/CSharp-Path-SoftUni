29499980cb01162f0f7ea27fbb0b5554f416beb9
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:56 +0300

    Create ExportCreatorDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/DataProcessor/ExportDto/ExportCreatorDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto
+{
+    [XmlType("Creator")]
+    public class ExportCreatorDto
+    {
+        [XmlAttribute("BoardgamesCount")]
+        public string BoardgamesCount { get; set; }
+
+        public string CreatorName { get; set; }
+
+        [XmlArray("Boardgames")]
+        public BoardGameExportDto[] Boardgames { get; set; }
+    }
+}
