8af84654c17d144642f78d890195846790d2ac7b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:33 +0300

    Create BoardgamesContext.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/Data/BoardgamesContext.cs
@@ -0,0 +1,38 @@
+﻿namespace Boardgames.Data
+{
+    using Boardgames.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    
+    public class BoardgamesContext : DbContext
+    {
+        public BoardgamesContext()
+        { 
+        }
+
+        public BoardgamesContext(DbContextOptions options)
+            : base(options) 
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<BoardgameSeller>()
+                .HasKey(b => new { b.SellerId, b.BoardgameId });
+        }
+
+        public DbSet<BoardgameSeller> BoardgamesSellers { get; set; }
+        public DbSet<Creator> Creators { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Boardgame> Boardgames { get; set; }
+    }
+}
