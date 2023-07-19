9bde1d5f0f13b6d7a0f936e877c557e490a95610
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:52 +0300

    Create BoardGameExportDto.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/DataProcessor/ExportDto/BoardGameExportDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto
+{
+    [XmlType("Boardgame")]
+    public class BoardGameExportDto
+    {
+        [MaxLength(20)]
+        [MinLength(10)]
+        [Required]
+        [XmlElement("BoardgameName")]
+        public string BoardgameName { get; set; } = null!;
+        [Range(2018, 2023)]
+        [Required]
+        [XmlElement("BoardgameYearPublished")]
+        public string BoardgameYearPublished { get; set; }
+    }
+}
