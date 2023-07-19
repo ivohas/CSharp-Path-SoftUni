d2bf85c263527492822db53983ac2155cc45cb95
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:38 +0300

    Create Configuration.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Boardgames.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.\SQLEXPRESS;Database=Boardgames;Integrated Security=True;Encrypt=False";
+    }
+}
