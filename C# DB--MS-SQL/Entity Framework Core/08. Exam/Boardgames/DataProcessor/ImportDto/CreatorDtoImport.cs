78d8975da9397d87a9aeacd78fff4a8cd8206f7e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:57 +0300

    Create CreatorDtoImport.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/DataProcessor/ImportDto/CreatorDtoImport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    [XmlType("Creator")]
+    public class CreatorDtoImport
+    {
+        [MaxLength(7)]
+        [MinLength(2)]
+        [Required]
+        [XmlElement("FirstName")]
+        public string FirstName { get; set; } = null!;
+
+        [MaxLength(7)]
+        [MinLength(2)]
+        [Required]
+        [XmlElement("LastName")]
+        public string LastName { get; set; } = null!;
+
+        [XmlArray("Boardgames")]
+        public GameDto[] Boardgames { get; set; }
+    }
+}
