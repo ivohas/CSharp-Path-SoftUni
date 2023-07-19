8716e30b2ec1e9e6ec79c5e8de033afbbc358a3c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-19 21:26:48 +0300

    Create Seller.cs

---
+++ C# DB--MS-SQL/Entity Framework Core/08. Exam/Boardgames/Data/Models/Seller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.Data.Models
+{
+    public class Seller
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [MaxLength(20)]
+        [MinLength(5)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(30)]
+        [MinLength(2)]
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public string Country { get; set; } = null!;
+
+
+        [Required]
+        [RegularExpression(@"^www\.[a-zA-Z0-9-]+\.com$")]
+        public string Website { get; set; } = null!;
+
+        public ICollection<BoardgameSeller> BoardgamesSellers  { get; set; } = new HashSet<BoardgameSeller>();
+    }
+}
