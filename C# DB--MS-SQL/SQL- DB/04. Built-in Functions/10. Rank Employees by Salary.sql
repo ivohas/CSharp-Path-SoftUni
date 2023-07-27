2c33452d94bc04cd16a5c1be4ef4ad19fade38b4
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-27 21:59:45 +0300

    Create 10. Rank Employees by Salary.sql

---
+++ C# DB--MS-SQL/SQL- DB/04. Built-in Functions/10. Rank Employees by Salary.sql
@@ -0,0 +1,9 @@
+	SELECT [EmployeeID],
+	       [FirstName],
+		   [LastName],
+		   [Salary],
+           DENSE_RANK() OVER(PARTITION BY[SALARY] ORDER BY [EmployeeID])
+  AS [Rank]
+  FROM Employees
+  WHERE [Salary]  BETWEEN 10000 AND 50000
+  ORDER BY [Salary] DESC
