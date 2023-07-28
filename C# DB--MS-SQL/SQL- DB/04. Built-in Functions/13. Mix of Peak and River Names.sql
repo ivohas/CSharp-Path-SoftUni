f08ebad3c19df0837db9fe5d81b3cb399446bc89
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-28 19:05:34 +0300

    Create 13. Mix of Peak and River Names.sql

---
+++ C# DB--MS-SQL/SQL- DB/04. Built-in Functions/13. Mix of Peak and River Names.sql
@@ -0,0 +1,6 @@
+SELECT Peaks.PeakName,
+       Rivers.RiverName,
+       LOWER(CONCAT(LEFT(Peaks.PeakName, LEN(Peaks.PeakName)-1), Rivers.RiverName)) AS Mix
+FROM Peaks
+     JOIN Rivers ON RIGHT(Peaks.PeakName, 1) = LEFT(Rivers.RiverName, 1)
+ORDER BY Mix;
