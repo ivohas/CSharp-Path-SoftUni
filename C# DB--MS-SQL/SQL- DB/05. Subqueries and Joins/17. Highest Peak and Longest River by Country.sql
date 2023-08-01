c3a4773dfa1df133d5992c031829a6c815508cf6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-01 22:35:55 +0300

    Create 17. Highest Peak and Longest River by Country.sql

---
+++ C# DB--MS-SQL/SQL- DB/05. Subqueries and Joins/17. Highest Peak and Longest River by Country.sql
@@ -0,0 +1,31 @@
+SELECT TOP (5) peaks.CountryName,
+               peaks.Elevation AS HighestPeakElevation,
+               rivers.Length AS LongestRiverLength
+FROM
+(
+    SELECT c.CountryName,
+           c.CountryCode,
+           DENSE_RANK() OVER(PARTITION BY c.CountryName ORDER BY p.Elevation DESC) AS DescendingElevationRank,
+           p.Elevation
+    FROM Countries AS c
+         FULL OUTER JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+         FULL OUTER JOIN Mountains AS m ON mc.MountainId = m.Id
+         FULL OUTER JOIN Peaks AS p ON m.Id = p.MountainId
+) AS peaks
+FULL OUTER JOIN
+(
+    SELECT c.CountryName,
+           c.CountryCode,
+           DENSE_RANK() OVER(PARTITION BY c.CountryCode ORDER BY r.Length DESC) AS DescendingRiversLenghRank,
+           r.Length
+    FROM Countries AS c
+         FULL OUTER JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+         FULL OUTER JOIN Rivers AS r ON cr.RiverId = r.Id
+) AS rivers ON peaks.CountryCode = rivers.CountryCode
+WHERE peaks.DescendingElevationRank = 1
+      AND rivers.DescendingRiversLenghRank = 1
+      AND (peaks.Elevation IS NOT NULL
+           OR rivers.Length IS NOT NULL)
+ORDER BY HighestPeakElevation DESC,
+         LongestRiverLength DESC,
+         CountryName; 
