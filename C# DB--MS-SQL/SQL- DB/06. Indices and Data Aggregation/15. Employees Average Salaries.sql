d1b8b4b5c05655f3d18cb5b9ee0b3737ec78e967
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-06 21:59:03 +0300

    Create 15. Employees Average Salaries.sql

---
+++ C# DB--MS-SQL/SQL- DB/06. Indices and Data Aggregation/15. Employees Average Salaries.sql
@@ -0,0 +1,17 @@
+SELECT *
+INTO NewTable
+FROM Employees
+WHERE Salary > 30000;
+
+DELETE FROM NewTable
+WHERE ManagerID = 42;
+
+UPDATE NewTable
+  SET
+      Salary += 5000
+WHERE DepartmentID = 1;
+
+SELECT DepartmentID,
+       AVG(Salary) AS [AverageSalary]
+FROM NewTable
+GROUP BY DepartmentID; 
