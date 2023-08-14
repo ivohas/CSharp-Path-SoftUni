a52096e3b17568e7a48aae720c544573cbc90375
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-14 14:11:05 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            /*int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();*/
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                sum += number;
+
+                array[i] = number;
+            }
+
+            Console.WriteLine(String.Join(" ", array));
+            Console.WriteLine(sum);
+        }
+    }
+}
