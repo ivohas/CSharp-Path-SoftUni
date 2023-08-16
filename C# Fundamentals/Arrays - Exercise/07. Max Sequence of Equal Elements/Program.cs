22577364b718037f4aaa096f78e525ad58d8169b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-16 12:20:37 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int longestSeqLength = 1;
+            int longestSeqStart = 0;
+            int currentSeqLength = 1;
+            int currentSeqStart = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    currentSeqLength++;
+
+                    if (currentSeqLength > longestSeqLength)
+                    {
+                        longestSeqLength = currentSeqLength;
+                        longestSeqStart = currentSeqStart;
+                    }
+                }
+                else
+                {
+                    currentSeqLength = 1;
+                    currentSeqStart = i;
+                }
+            }
+
+            for (int i = longestSeqStart; i < longestSeqStart + longestSeqLength; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
