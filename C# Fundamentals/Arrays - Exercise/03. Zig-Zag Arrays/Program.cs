904a8915fb00bfb05ea2a31b67df76daa62618ce
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-16 12:20:18 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = input[0];
+                    secondArray[i] = input[1];
+                }
+                else
+                {
+                    secondArray[i] = input[0];
+                    firstArray[i] = input[1];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(String.Join(" ", secondArray));
+        }
+    }
+}
