765624f51ffec418a047445d520d67d6d716087f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-16 12:20:13 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string[] secondArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] arraySum = new string[firstArray.Length];
+
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                for (int k = 0; k < secondArray.Length; k++)
+                {
+                    if (firstArray[i] == secondArray[k])
+                    {
+                        Console.Write(firstArray[i]+ " ");
+                    }
+                }
+            }
+
+        }
+    }
+}
