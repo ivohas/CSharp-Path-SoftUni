55613352f3db6ae75991c8b4b671cecfe0161fae
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-16 12:20:43 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int magicSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == magicSum)
+                    {
+                        Console.WriteLine($"{array[i]} {array[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
