23a356d6973e57633d04dd56450e8ddcd6252427
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:10:50 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
