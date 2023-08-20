37ee7285b569dc494feb1048b8875ac44eeb9819
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:11:00 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listOfNums = Console.ReadLine()
+                                         .Split();
+
+            for (int i = listOfNums.Length - 1; i >= 0; i--)
+            {
+                Console.Write(listOfNums[i] + " ");
+            }
+
+        }
+    }
+}
