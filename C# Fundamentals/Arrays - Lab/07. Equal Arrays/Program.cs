c3be424b50e95c218b055a79f08a2ef53e1281ae
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:11:21 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                  .Split()
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            int[] array2 = Console.ReadLine()
+                                  .Split()
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+            }
+
+            int sum = array.Sum();
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
