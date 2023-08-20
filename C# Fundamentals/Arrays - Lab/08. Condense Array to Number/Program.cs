40ca921d8e00d355a4c5f688a7177d000b0bfc4b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:11:33 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            while (array.Length > 1)
+            {
+                int[] condensed = new int[array.Length - 1];
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = array[i] + array[i + 1];
+                }
+
+                array = condensed;
+            }
+
+            Console.WriteLine(array[0]);
+        }
+    }
+}
