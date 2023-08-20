9b2549bf266c9a334f87fd672cd69d3c28f523b2
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:10:55 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] listOfNums = Console.ReadLine()
+                                        .Split()
+                                        .Select(double.Parse)
+                                        .ToArray();
+
+            for (int i = 0; i < listOfNums.Length; i++)
+            {
+                Console.WriteLine($"{listOfNums[i]} => {Math.Round(listOfNums[i], MidpointRounding.AwayFromZero)}");
+            }
+
+        }
+    }
+}
