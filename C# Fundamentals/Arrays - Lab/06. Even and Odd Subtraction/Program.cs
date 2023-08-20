8bf950933cc5be378bade07765a1406311d99fc7
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:11:16 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEven += numbers[i];
+                }
+                else if (numbers[i] % 2 != 0)
+                {
+                    sumOdd += numbers[i];
+                }
+            }
+
+            Console.WriteLine($"{sumEven - sumOdd}");
+        }
+    }
+}
