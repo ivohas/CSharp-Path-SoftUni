4265e42f743179836ca8161271163dc94733c649
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-20 14:10:47 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string[] dayOfWeek = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+
+            if (day >= 1 && day <= dayOfWeek.Length)
+            {
+                Console.WriteLine(dayOfWeek[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
