2b8f43b5da5bcd7cdbd7da7831ae33ff446655e6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-24 11:02:28 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - More Exercise/04. Fold and Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace _04._Fold_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int k = input.Length / 4;
+            int[] output = new int[2 * k];
+
+            for (int i = 0; i < k; i++)
+            {
+                output[i] = input[k - (i + 1)] + input[k + i];
+                output[output.Length - 1 - i] = input[output.Length - 1 - i + k] + input[(output.Length - 1 - i) + (k + 2 * i + 1)];
+            }
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
