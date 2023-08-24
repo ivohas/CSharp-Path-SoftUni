236e34a4344abcc25769211054c7105c912af7d1
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-24 11:03:28 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Exercise/06. Courses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courseInfo = new Dictionary<string, List<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] courseArgs = command
+                    .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string courseName = courseArgs[0];
+                string studentName = courseArgs[1];
+
+                if (!courseInfo.ContainsKey(courseName))
+                {
+                    courseInfo[courseName] = new List<string>();
+                }
+
+                courseInfo[courseName].Add(studentName);
+
+            }
+
+            PrintCoursesInfo(courseInfo);
+        }
+
+        static void PrintCoursesInfo(Dictionary<string, List<string>> courseInfo)
+        {
+            foreach (var kvp in courseInfo)
+            {
+                string courseName = kvp.Key;
+                List<string> students = kvp.Value;
+
+                Console.WriteLine($"{courseName}: {students.Count}");
+                foreach (var studentName in students)
+                {
+                    Console.WriteLine($"-- {studentName}");
+                }
+            }
+        }
+    }
+}
