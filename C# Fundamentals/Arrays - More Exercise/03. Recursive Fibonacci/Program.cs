2a76b747992ab1096dbd1f2f09f641187e33cfcb
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-24 11:02:20 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int positionInFibonacciSequence = int.Parse(Console.ReadLine());
+                int[] fibonacciSequence = new int[50];
+
+                fibonacciSequence[0] = 1;
+                fibonacciSequence[1] = 1;
+
+                if (positionInFibonacciSequence > 2)
+                {
+                    for (int i = 2; i < positionInFibonacciSequence; i++)
+                    {
+                        fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+                    }
+                }
+                Console.WriteLine(fibonacciSequence[positionInFibonacciSequence - 1]);
+        }
+    }
+}
