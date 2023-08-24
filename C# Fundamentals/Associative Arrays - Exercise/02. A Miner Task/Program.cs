a398a5afbf0617e32af075e38048b5d3fd6a13b5
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-24 11:02:48 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, long> resources = new Dictionary<string, long>();
+
+            string resource;
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources[resource] = 0;
+                }
+
+                resources[resource] += quantity;
+            }
+
+            foreach (var rqp in resources)
+            {
+                Console.WriteLine($"{rqp.Key} -> {rqp.Value}");
+            }
+        }
+    }
+}
