6a7f9b2af4e0b0303cdf14d8483198bd92fb9332
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-24 11:02:33 +0300

    Create Program.cs

---
+++ C# Fundamentals/Arrays - More Exercise/05. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Longest_Increasing_Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            int[] lis;
+            int[] len = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                len[i] = 1;
+                prev[i] = -1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (sequence[j] < sequence[i] && len[j] >= len[i])
+                    {
+                        len[i] = 1 + len[j];
+                        prev[i] = j;
+                    }
+                }
+                if (len[i] > maxLength)
+                {
+                    maxLength = len[i];
+                    lastIndex = i;
+                }
+            }
+            lis = new int[maxLength];
+            for (int i = 0; i < maxLength; i++)
+            {
+                lis[i] = sequence[lastIndex];
+                lastIndex = prev[lastIndex];
+            }
+            Array.Reverse(lis);
+            Console.WriteLine(string.Join(" ", lis));
+        }
+    }
+}
