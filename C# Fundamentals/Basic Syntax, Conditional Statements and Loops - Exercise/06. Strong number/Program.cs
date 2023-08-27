a000e9beb621167f9cf9aa0c3fb638a58b266b21
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-27 13:39:09 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06._Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            //145! = 1! +4! + 5!
+            long factorielSum = 0;
+
+            for (int i = 0; i <= number.Length - 1; i++)
+            {
+                char currentCharacter = number[i];
+                int currentDigit = (int)currentCharacter - 48;
+
+                long currentDigitFactoriel = 1;
+                for (int r = currentDigit; r > 1; r--)
+                {
+                    currentDigitFactoriel *= r;
+                }
+
+                factorielSum += currentDigitFactoriel;
+            }
+
+            if (factorielSum == int.Parse(number))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
