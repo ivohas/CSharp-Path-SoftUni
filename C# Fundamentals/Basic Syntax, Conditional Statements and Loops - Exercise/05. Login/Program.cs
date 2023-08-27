62f7b496d45b58df4d2e6f6d7897a179090dff49
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-27 13:39:04 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string username = Console.ReadLine();
+
+            //action + output
+            //reverse the username
+            string password = string.Empty;
+            string passwordInput = string.Empty;
+
+            for (int reverseIndex = username.Length - 1; reverseIndex >= 0; reverseIndex--)
+            {
+                password += (username[reverseIndex]);
+                //+= string concatenation very slow!!!
+            }
+
+            for (int count = 1; count <= 4; count++)
+            {
+                passwordInput = Console.ReadLine();
+
+                if (passwordInput == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    if (count == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect password. Try again.");
+                        continue; // can be omitted
+                    }
+                }
+            }
+
+        }
+    }
+}
