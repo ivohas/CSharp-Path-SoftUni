8422f57ba7f33fd136b30ad9fd743fe3a3fb4c82
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-27 13:39:17 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 0;
+            for (int i = 0; i < n; i++)
+            {
+                number++;
+                for (int a = 0; a < number; a++)
+                {
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
