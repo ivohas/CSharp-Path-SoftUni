99e7c6aa55d280dd2d76e6387d0008b10ecfd1c1
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-27 13:39:27 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboarPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+
+            double sum = 0;
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += headsetPrice;
+                }
+                if (i % 3 == 0)
+                {
+                    sum += mousePrice;
+                }
+                if (i % 6 == 0)
+                {
+                    sum += keyboarPrice;
+                }
+                if (i % 12 == 0)
+                {
+                    sum += displayPrice;
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {sum:f2} lv.");
+
+        }
+    }
+}
