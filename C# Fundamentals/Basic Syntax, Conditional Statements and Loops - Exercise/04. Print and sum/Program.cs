be29fa0278e97f5927e348ee920e414568d4ddf6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-27 13:38:59 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            Console.Write(start + " ");
+
+            for (int i = start; i < end; i++)
+            {
+                sum += start;
+                start++;
+                Console.Write(start + " ");
+            }
+            Console.WriteLine();
+            sum += start;
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
