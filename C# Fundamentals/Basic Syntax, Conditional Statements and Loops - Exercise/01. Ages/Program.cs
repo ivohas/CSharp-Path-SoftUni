fab1504ea1a0d4b0b1e7cdac5afc4abae54599a7
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-27 13:38:35 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/01. Ages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string kind = "";
+
+            if (age >= 0 && age <= 2)
+            {
+                kind = "baby";
+            }
+            else if (age > 2 && age < 14)
+            {
+                kind = "child";
+            }
+            else if (age > 13 && age < 20)
+            {
+                kind = "teenager";
+            }
+            else if (age > 19 && age < 66)
+            {
+                kind = "adult";
+            }
+            else if (age >= 6)
+            {
+                kind = "elder";
+            }
+            Console.WriteLine(kind);
+        }
+    }
+}
