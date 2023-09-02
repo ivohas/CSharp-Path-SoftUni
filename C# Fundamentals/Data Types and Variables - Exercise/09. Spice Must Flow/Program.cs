de9feb5d5c8fc2e34edf4b8770b04f7e11f664a8
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-02 22:41:48 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+
+            int spiceProduced = 0;
+            int dayCounter = 0;
+            if (startingYield < 100)
+            {
+
+            }
+            else
+            {
+                while (startingYield >= 100)
+                {
+                    spiceProduced += startingYield - 26;
+                    startingYield -= 10;
+                    dayCounter++;
+                }
+
+                spiceProduced -= 26;
+
+            }
+            Console.WriteLine(dayCounter);
+            Console.WriteLine(spiceProduced);
+        }
+    }
+}
