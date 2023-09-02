9d1e721a406703bf40e1583dae5b43fc42068592
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-02 22:41:37 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string biggestKeg = "";
+            double maxVolume = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string KegName = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * radius * radius * height;
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestKeg = KegName;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
