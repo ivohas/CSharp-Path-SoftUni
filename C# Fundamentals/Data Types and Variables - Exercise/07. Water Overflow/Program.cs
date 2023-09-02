8c1285f463d7d49c8c18d572f97f237162172a7e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-02 22:41:30 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int L = int.Parse(Console.ReadLine());
+
+                sum += L;
+
+                if (sum > 255)
+                {
+                    sum -= L;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
