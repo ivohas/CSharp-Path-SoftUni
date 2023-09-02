20a956df51543e6a9576ea4e5362bda222266930
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-02 22:41:00 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            do
+            {
+                sum += (number % 10);
+                number /= 10;
+            } while (number > 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
