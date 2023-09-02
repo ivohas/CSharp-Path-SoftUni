c8b95faafd9c49e20b5061bbf8a1f0a6e744f669
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-02 22:41:04 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleQ = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = 0;
+            int counter = 0;
+
+            if (capacity > peopleQ)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                courses = peopleQ / capacity;
+                if (peopleQ % capacity == 0)
+                {
+                    Console.WriteLine(courses);
+                }
+                else if (peopleQ % capacity != 0)
+                {
+                    for (int i = capacity; i <= peopleQ; i += capacity)
+                    {
+                        counter++;
+                    }
+                    Console.WriteLine(courses + 1);
+                }
+            }
+        }
+    }
+}
