c692b8346fc367512955318cc9d1c0be896f60df
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-02 22:41:20 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            char c;
+            string text;
+            for (int i = start; i <= end; i++)
+            {
+                c = (char)i;
+                text = c.ToString();
+                Console.Write(text + " ");
+            }
+        }
+    }
+}
