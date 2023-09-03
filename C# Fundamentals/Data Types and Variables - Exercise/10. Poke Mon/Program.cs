8a1cb2fb2b503b05d6ca432cda77d25310c03849
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-04 00:19:13 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokemonPower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int targetsPoked = 0;
+
+            double originalValue = pokemonPower * 0.50;
+
+            while (pokemonPower >= distance)
+            {
+                if (pokemonPower == originalValue)
+                {
+                    if (exhaustionFactor > 0)
+                    {
+                        pokemonPower = pokemonPower / exhaustionFactor;
+
+                        if (pokemonPower < distance)
+                        {
+                            break;
+                        }
+                    }
+
+                }
+
+                pokemonPower -= distance;
+                targetsPoked++;
+            }
+
+            Console.WriteLine(pokemonPower);
+            Console.WriteLine(targetsPoked);
+        }
+    }
+}
