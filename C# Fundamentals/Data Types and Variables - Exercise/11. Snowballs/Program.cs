69c941878a78d6a9253daf1eae29cf0a70e7ffdb
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-04 00:19:17 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double maxValue = 0;
+
+            string bestSnowball = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                int snow = int.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                int quality = int.Parse(Console.ReadLine());
+
+                double value = Math.Pow(snow / time, quality);
+
+                if (value >= maxValue)
+                {
+                    maxValue = value;
+                    bestSnowball = ($"{snow} : {time} = {value} ({quality})");
+                }
+            }
+            
+            Console.WriteLine(bestSnowball);
+        }
+    }
+}
