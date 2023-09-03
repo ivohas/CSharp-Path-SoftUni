ca580e20ccf028c0e8d6c3e5310cad7edcaa3336
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-04 00:19:24 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+
+            meters /= 1000;
+
+            Console.WriteLine($"{meters:f2}");
+        }
+
+
+    }
+}
