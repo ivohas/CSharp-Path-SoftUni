d904c2c2f0b5160315251660f31bc519d5623612
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-04 00:19:41 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Lab/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+
+            int years = centuries * 100;
+
+            double days = Math.Abs(365.2422 * years);
+
+            double hours = days * 60;
+
+            double minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours {minutes} minutes");
+
+
+        }
+    }
+}
