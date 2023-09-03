90be53917f3406306d79a6cb910a51baf500129c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-04 00:20:27 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double eps = 0.000001;
+
+                double n1 = double.Parse(Console.ReadLine());
+                double n2 = double.Parse(Console.ReadLine()); 
+                bool isEqual = Math.Abs(n1 - n2) < eps;
+
+                if (isEqual)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }    
+
+        }
+    }
+}
