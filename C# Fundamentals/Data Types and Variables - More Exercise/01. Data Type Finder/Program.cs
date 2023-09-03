04d89d9d152763c162fb6cf246fb59ffb49a1672
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-04 00:19:59 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int valueInt;
+            float valueFloat;
+            char valueChar;
+            bool valueBool;
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out valueInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out valueFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out valueChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out valueBool))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
