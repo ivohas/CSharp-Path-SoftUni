43c58d93322286e0bc62791981f033a5197726a3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-08 00:07:05 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace _06._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = BigInteger.Parse(Console.ReadLine());
+
+            int openCount = 0;
+            int closeCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+                if (text == "(")
+                {
+                    openCount++;
+                }
+                else if (text == ")")
+                {
+                    closeCount++;
+                    if (openCount - closeCount != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            if (closeCount == openCount)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
