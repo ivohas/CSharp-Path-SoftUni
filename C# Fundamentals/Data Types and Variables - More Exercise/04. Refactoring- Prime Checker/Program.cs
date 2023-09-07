231d21225721ed331102f099252ff8e2f6a81b93
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-08 00:06:50 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/04. Refactoring- Prime Checker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Refactoring__Prime_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                bool isItTrue = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isItTrue = false;
+                        break;
+                    }
+                }
+                if (isItTrue)
+                {
+                    Console.WriteLine($"{i} -> true");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> false");
+                }
+            }
+
+        }
+    }
+}
