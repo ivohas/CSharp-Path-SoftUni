06471498c52b88efdc62b067ad5de33b7ec13b69
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-08 00:07:22 +0300

    Create Program.cs

---
+++ C# Fundamentals/Exams/Programming_Fundamentals_Final_Exam/Destination_Mapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> destionations = new List<string>();
+            int travelPoints = 0;
+
+            string input = Console.ReadLine();
+            Regex dests = new Regex(@"(\=|\/)(?<destination>[A-Z][A-Za-z]{2,})(\1)");
+            MatchCollection matches = dests.Matches(input);
+
+            foreach (Match currMatch in matches)
+            {
+                string currDestination = currMatch.Groups["destination"].Value;
+                destionations.Add(currDestination);
+                travelPoints += currDestination.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destionations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
