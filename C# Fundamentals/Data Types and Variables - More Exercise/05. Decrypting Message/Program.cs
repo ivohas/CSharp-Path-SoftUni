a5176d36da40aa4488b56f590f700486b2789a25
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-08 00:06:58 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string decryptedText = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                decryptedText += (char) (char.Parse(Console.ReadLine()) + key);
+            }
+            Console.WriteLine(decryptedText);
+        }
+    }
+}
