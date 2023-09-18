38bf78c6711bcd5b02302a646d5204740d6ab7c3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:30:34 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Exercise/07. Student Academy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (students.ContainsKey(studentName))
+                {
+                    students[studentName] = ((students[studentName] + studentGrade) / 2);
+                }
+                else
+                {
+                    students[studentName] = studentGrade;
+                }
+
+            }
+
+            foreach (var student in students)
+            {
+                if (student.Value >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+                }
+            }
+        }
+    }
+}
