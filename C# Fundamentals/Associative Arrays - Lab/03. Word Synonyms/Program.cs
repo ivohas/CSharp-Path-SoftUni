898a9c391a2bb81519a1300611cf721e8acb9235
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:30:54 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> synonymList = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < pairCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (synonymList.ContainsKey(word))
+                {
+                    synonymList[word].Add(synonym);
+                }
+                else
+                {
+                    List<string> newSymList = new List<string>() { synonym };
+                    synonymList.Add(word, newSymList);
+                }
+            }
+
+            foreach (var item in synonymList)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
