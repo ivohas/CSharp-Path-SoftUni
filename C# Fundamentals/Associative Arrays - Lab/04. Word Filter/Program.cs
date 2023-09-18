80c7e35fec199c770eddfe71e708b5f60b96f041
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:31:05 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            string[] filteredWords = words.Where(x => x.Length % 2 == 0).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, filteredWords));
+        }
+    }
+}
