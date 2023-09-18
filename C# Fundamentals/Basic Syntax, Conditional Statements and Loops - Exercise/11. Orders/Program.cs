96ef30a46be69a2d0ba87370fa53a02bd5252982
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:31:13 +0300

    Create Program.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/11. Orders/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double pricePerCapsule = 0;
+            int days = 0;
+            int capsulesCount = 0;
+            double totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                pricePerCapsule = double.Parse(Console.ReadLine());
+                days = int.Parse((Console.ReadLine()));
+                capsulesCount = int.Parse((Console.ReadLine()));
+
+                double sum = pricePerCapsule * days * capsulesCount;
+                Console.WriteLine($"The price for the coffee is: ${sum:f2}");
+                totalSum += sum;
+            }
+
+            Console.WriteLine($"Total: ${totalSum:f2}");
+        }
+    }
+}
