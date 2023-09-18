3344ed486eb63be76cc38f1e5a60b48ef357e5b2
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:30:38 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> companies = new Dictionary<string, HashSet<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] companyArgs = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = companyArgs[0];
+                string employeeId = companyArgs[1];
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new HashSet<string>() { employeeId });
+                }
+                else
+                {
+                    companies[companyName].Add(employeeId);
+                }
+
+            }
+            foreach (var company in companies)
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var id in company.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
