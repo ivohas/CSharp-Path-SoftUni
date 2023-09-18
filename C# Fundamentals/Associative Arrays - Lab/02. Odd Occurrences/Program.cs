c4e5c2ac6d46e4cd13f6acf8737a0ae978c0b761
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:30:49 +0300

    Create Program.cs

---
+++ C# Fundamentals/Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerIndex = word.ToLower();
+                if (counts.ContainsKey(wordInLowerIndex))
+                {
+                    counts[wordInLowerIndex]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerIndex, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write($"{count.Key} ");
+                }
+            }
+        }
+    }
+}
