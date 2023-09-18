144f3b5ec0f5f694a7dc3e507627bbd9cfb902fc
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-18 14:35:29 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
