dc352ca50dbb6836ca2e63bf3a84d30559cfea29
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-20 08:10:22 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> numbersString = Console.ReadLine().Split('|').Reverse().ToList();
+
+            numbersString.RemoveAll(x => x == "|");
+
+            String listOfNumbers = String.Join(" ", numbersString);
+            listOfNumbers = Regex.Replace(listOfNumbers, " {2,}", " ");
+
+            Console.WriteLine(listOfNumbers.Trim());
+        }
+    }
+}
