36deb164df9c0e7ebb22fe679a2f74f50eab0bcb
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-20 08:09:31 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Exercise/02. Change List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string command;
+            int index = 0;
+            int number = 0;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] splitedCmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (splitedCmdArgs.Length == 2)
+                {
+                    number = int.Parse(splitedCmdArgs[1]);
+                    numbers.RemoveAll(x => x == number);
+                }
+                else if (splitedCmdArgs.Length == 3)
+                {
+                    number =  int.Parse((splitedCmdArgs[1]));
+                    index = int.Parse(splitedCmdArgs[2]);
+                    numbers.Insert(index, number);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
