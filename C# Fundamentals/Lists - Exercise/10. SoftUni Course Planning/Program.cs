184be6c4797dadc427e6768e452b48afe381411e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-21 08:11:57 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Exercise/10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10._SoftUni_Course_Planning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
