dae6ba5cc4b1bc99ad3fe80a74c5ba4d2980e82a
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-21 08:12:25 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
