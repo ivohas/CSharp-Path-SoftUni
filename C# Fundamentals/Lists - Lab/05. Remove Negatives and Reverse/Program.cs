5803b2883f4d5c40fd9c27c08c0f3bb25e11deea
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-21 08:12:29 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            numbers.RemoveAll(x => x < 0);
+
+            numbers.Reverse();
+
+            if (numbers.Count < 1)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            else Console.WriteLine(String.Join(" ", numbers));
+            
+        }
+    }
+}
