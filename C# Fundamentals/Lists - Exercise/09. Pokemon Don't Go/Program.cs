c4289b7299e89c87911777d52f2e2006d79b7327
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-21 08:11:05 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Exercise/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
