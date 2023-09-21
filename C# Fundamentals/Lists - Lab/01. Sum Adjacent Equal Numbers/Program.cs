8635b968466d2cb1662a1528cba1f67316f81b0c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-21 08:12:05 +0300

    Create Program.cs

---
+++ C# Fundamentals/Lists - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+            int index = 1;
+            while (index < numbers.Count)
+            {
+                if (numbers[index] == numbers[index - 1])
+                {
+                    numbers[index - 1] += numbers[index];
+                    numbers.RemoveAt(index);
+                    index = 1;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
