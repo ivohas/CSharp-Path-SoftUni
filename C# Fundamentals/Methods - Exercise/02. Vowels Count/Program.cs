f4123d58f3b2cb8be24e8a79871c6eb0ade04f95
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-22 11:16:27 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            int vowelsCount = GetVowelsCount(word);
+
+            Console.WriteLine(vowelsCount);
+        }
+
+        static int GetVowelsCount(string word)
+        {
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u'};
+
+            int vowelsCount = 0;
+            foreach (char vowel in word.ToLower())
+            {
+                if (vowels.Contains(vowel))
+                {
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+    }
+}
