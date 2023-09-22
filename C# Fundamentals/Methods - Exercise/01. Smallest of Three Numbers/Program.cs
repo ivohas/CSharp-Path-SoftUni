dcfac25ec9e95b473acc2532b7db6ef7abb604cc
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-22 11:16:22 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            int smallestNumber = GetSmallestNumber(number1, number2, number3);
+
+            Console.WriteLine(smallestNumber);
+        }
+
+        static int GetSmallestNumber(int number1, int number2, int number3)
+        {
+            int[] array = new int[3];
+            
+            array[0] = number1;
+            array[1] = number2;
+            array[2] = number3;
+
+            int smallestNumber = array.Min();
+
+            return smallestNumber;
+        }
+    }
+}
