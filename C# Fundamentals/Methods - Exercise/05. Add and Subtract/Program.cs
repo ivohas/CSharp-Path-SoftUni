7c0cfe70e5e588924def232e3a088ac9a973009b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-22 11:16:44 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int sum = Sum(a, b, c);
+            sum = Substraction(sum, c);
+            Console.WriteLine(sum);
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            int sum = a + b;
+
+            return sum;
+        }
+
+        static int Substraction(int sum, int c)
+        {
+            sum -= c;
+
+            return sum;
+        }
+    }
+}
