b594f6f17075cd58deefd83909ee3084a07902d9
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-22 11:16:52 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            MiddleSymbol(input);
+        }
+
+        static void MiddleSymbol(string word)
+        {
+            if (word.Length % 2 == 1)
+            {
+                Console.WriteLine($"{word[word.Length / 2]}");
+            }
+            else
+            {
+                Console.WriteLine($"{word[word.Length / 2 - 1]}{word[word.Length / 2]}");
+            }
+        }
+    }
+}
