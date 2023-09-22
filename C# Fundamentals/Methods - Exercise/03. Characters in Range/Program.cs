d05611da1a9e19ae20d669ea5a1204f62da35589
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-22 11:16:31 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char input1 = char.Parse(Console.ReadLine());
+            char input2 = char.Parse(Console.ReadLine());
+
+            CharactersInRange(input1, input2);
+        }
+
+        static void CharactersInRange(char firstChar, char secondChar)
+        {
+            int starterChar = Math.Min(firstChar, secondChar);
+            int endChar = Math.Max(firstChar, secondChar);
+
+            for (int i = starterChar + 1; i < endChar; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
