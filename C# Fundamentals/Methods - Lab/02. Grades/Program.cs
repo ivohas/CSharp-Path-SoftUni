5a677b845f82165a44c6acdf3bc2a4c32eed775f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:37:14 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/02. Grades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            Grade(grade);
+        }
+
+        static void Grade(double grade)
+        {
+            if (grade >= 2.00 && grade < 3)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3 && grade < 3.50)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade < 4.50)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade < 5.50)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
