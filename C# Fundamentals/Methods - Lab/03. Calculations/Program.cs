7ecf401fa497dfd5bd76d9765ecf633ae281daa1
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:37:18 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Action(a, b, action);
+        }
+        static void Action(int a, int b, string action)
+        {
+            int sum = 0;
+            if (action == "add")
+            {
+                sum = a + b;
+            }
+            else if (action == "multiply")
+            {
+                sum = a * b;
+            }
+            else if (action == "substract")
+            {
+                sum = a - b;
+            }
+            else if (action == "divide")
+            {
+                sum = a / b;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
