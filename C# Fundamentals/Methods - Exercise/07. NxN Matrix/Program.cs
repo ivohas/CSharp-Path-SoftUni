b8b16e5a35d60156a525be1615dbed3fb41a40d6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:36:36 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintMatrix(number);
+        }
+
+        static void PrintMatrix(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write($"{number} ");
+                }
+                    Console.WriteLine();
+            }
+        }
+    }
+}
