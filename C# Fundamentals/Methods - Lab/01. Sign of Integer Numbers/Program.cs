97e9df5e06add005aa4a0dd1c3c85ffb91753f55
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:37:08 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintNumberSign(number);
+        }
+
+        static void PrintNumberSign(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative. ");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive. ");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero. ");
+            }
+        }
+    }
+}
