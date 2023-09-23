c0f0ee4d0f09b25b096aacaeed8d556813d5e256
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:36:40 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            PrintFactorial(firstNumber, secondNumber);
+        }
+
+        static void PrintFactorial(int firstNumber, int secondNumber)
+        {
+            double firstFactorial = 1;
+            double secondFactorial = 1;
+
+            for (int i = firstNumber; i > 0; i--)
+            {
+                firstFactorial = firstFactorial * i;
+            }
+            for (int i = secondNumber; i > 0; i--)
+            {
+                secondFactorial = secondFactorial * i;
+            }
+
+            double result = firstFactorial / secondFactorial;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
