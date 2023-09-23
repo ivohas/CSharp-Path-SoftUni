ca835f697b3adfe9e9bd083f4763f8b9f0bd565c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-24 00:31:17 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string commmand = Console.ReadLine();
+
+            PrintDataType(commmand);
+        }
+
+        static void PrintDataType(string command)
+        {
+            switch (command)
+            {
+                case "int":
+                    int integer = int.Parse(Console.ReadLine());
+                    integer *= 2;
+                    Console.WriteLine(integer);
+                    break;
+                case "real":
+                    double realNumber = double.Parse(Console.ReadLine());
+                    realNumber *= 1.5;
+                    Console.WriteLine($"{realNumber:f2}");
+                    break;
+                case "string":
+                    string word = Console.ReadLine();
+                    Console.WriteLine($"${word}$");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
