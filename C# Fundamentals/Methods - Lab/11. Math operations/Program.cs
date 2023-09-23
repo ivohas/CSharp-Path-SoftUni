289a48355093d4ba68b71a5b41707c2b1e5b399f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-24 00:31:11 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = int.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+
+            double result = Operator(a, @operator, b);
+            Console.WriteLine(result);
+        }
+
+        static double Operator(double a, char @operator, double b)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                        result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
