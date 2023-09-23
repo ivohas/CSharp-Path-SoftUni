b8db7475f6454bfb762798b8e5f29bf8316ce7c3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:36:51 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Exercise/10. Top Number/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _10._Top_Number
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int toNumber = int.Parse(Console.ReadLine());
+
+            PrintNumbersFromTo(toNumber);
+        }
+
+        private static void PrintNumbersFromTo(int toNumber)
+        {
+            for (int i = 1; i <= toNumber; i++)
+            {
+                if (IsSumOfDigitsDivisibleBy8(i) && AtleastOneDigitIsOdd(i))
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+        static bool IsSumOfDigitsDivisibleBy8(int number)
+        {
+            int digitSum = 0;
+            while (number > 0)
+            {
+                digitSum += number % 10;
+                number /= 10;
+            }
+            if (digitSum % 8 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool AtleastOneDigitIsOdd(int toNumber)
+        {
+            while (toNumber > 0)
+            {
+                if (toNumber % 10 % 2 == 1)
+                {
+                    return true;
+                }
+                    toNumber /= 10;
+            }
+
+            return false;
+        }
+    }
+}
