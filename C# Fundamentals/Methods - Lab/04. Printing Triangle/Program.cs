0ebc50b7271c507feb68c0ed35027ef5bc161f51
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-23 12:37:25 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= height; i++)
+            {
+                PrintLine(1, i);
+            }
+            for (int i = height - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
