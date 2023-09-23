23ac526f6d5c46ce9647c35f2341ce01e0fabe5d
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-24 00:31:33 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - More Exercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _05._Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            PrintPositiveOrNegative(firstNumber, secondNumber, thirdNumber);
+        }
+
+        static void PrintPositiveOrNegative(int firstNumber, int secondNumber, int ThirdNumber)
+        {
+            int minusCounter = 0;
+
+            if (firstNumber == 0 || secondNumber == 0 || ThirdNumber == 0)
+            {
+                Console.WriteLine("zero");
+                return;
+            }
+
+            if (firstNumber / 1 == -(Math.Abs(firstNumber)))
+            {
+                minusCounter++;
+            }
+            if (secondNumber / 1 == -(Math.Abs(secondNumber)))
+            {
+                minusCounter++;
+            }
+            if (ThirdNumber / 1 == -(Math.Abs(ThirdNumber)))
+            {
+                minusCounter++;
+            }
+
+            if (minusCounter % 2 != 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("positive");
+            }
+        }
+    }
+}
