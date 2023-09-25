40ca7af4a7cd1a92f424fd65744222d301119065
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-25 19:03:15 +0300

    Create Program.cs

---
+++ C# Fundamentals/Objects and Classes - Exercise/07. Order by Age/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Order_by_Age
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Somebody> somebodies = new List<Somebody>();
+
+            string command;
+        BREAK:
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(' ').ToArray();
+
+                string name = cmdArgs[0];
+                string id = cmdArgs[1];
+                int age = int.Parse(cmdArgs[2]);
+
+                Somebody somebodyy = new Somebody(name, id, age);
+
+                foreach (Somebody somebody in somebodies)
+                {
+                    if (somebody.ID == id)
+                    {
+                        somebody.Update(name, age);
+                        goto BREAK;
+                    }
+                }
+
+                somebodies.Add(somebodyy);
+
+            }
+
+            somebodies = somebodies.OrderBy(x => x.Age).ToList();
+
+            foreach (Somebody somebody1 in somebodies)
+            {
+                Console.WriteLine(somebody1);
+            }
+        }
+    }
+
+    class Somebody
+    {
+        public string Name { get; set; }
+
+        public string ID { get; set; }
+
+        public int Age { get; set; }
+
+        public Somebody(string name, string id, int age)
+        {
+            this.Name = name;
+            this.ID = id;
+            this.Age = age;
+        }
+        public void Update(string newName, int newAge)
+        {
+            Name = newName;
+            Age = newAge;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+
+    }
+}
