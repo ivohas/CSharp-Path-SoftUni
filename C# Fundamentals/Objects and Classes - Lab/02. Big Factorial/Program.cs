9faae2dc357d316010f518ffacf783a1f19a9885
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-25 19:03:31 +0300

    Create Program.cs

---
+++ C# Fundamentals/Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
