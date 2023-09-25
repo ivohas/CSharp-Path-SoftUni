fa1c927457ba535b61b90a2f86ad0a97b4a37283
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-25 19:03:18 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/09. Greater of Two Values/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _09._Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(first, second));
+            }
+            else if (type == "char")
+            {
+                char first = char.Parse(Console.ReadLine());
+                char second = char.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(first, second));
+            }
+            else if (type == "string")
+            {
+                string first= Console.ReadLine();
+                string second= Console.ReadLine();
+                Console.WriteLine(GetMax(first, second));
+            }
+        }
+
+        static int GetMax(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        static char GetMax(char first, char second)
+        {
+            return (first > second) ? first : second;
+        }
+
+        static string GetMax(string first, string second)
+        {
+            if (first.CompareTo(second) > 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
