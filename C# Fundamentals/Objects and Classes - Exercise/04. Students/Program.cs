c890298b42b0e7ab10f3179de6364f820457e804
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-25 18:59:23 +0300

    Create Program.cs

---
+++ C# Fundamentals/Objects and Classes - Exercise/04. Students/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<Student> students = new List<Student>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] studentArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string currentStudentFirstName = studentArgs[0];
+                string currentStudentLastName = studentArgs[1];
+                double studentGrade = double.Parse(studentArgs[2]);
+
+                Student currStudent = new Student(currentStudentFirstName, currentStudentLastName, studentGrade);
+
+                students.Add(currStudent);
+
+            }
+
+            foreach (Student student in students.OrderByDescending(s => s.Grade))
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+    }
+}
