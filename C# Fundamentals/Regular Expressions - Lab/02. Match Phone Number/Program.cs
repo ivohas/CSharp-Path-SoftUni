6264aca5abf85eb81894aca50a9bd76cc80211c6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-27 08:11:50 +0300

    Create Program.cs

---
+++ C# Fundamentals/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            string input = Console.ReadLine();
+
+            string[] matches = Regex
+                .Matches(input, pattern)
+                .Select(x => x.Value)
+                .ToArray();
+            
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
