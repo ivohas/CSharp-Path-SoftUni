6c188a2ef893f31383a01c38baea1d0d6bdf6bdc
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-27 08:11:45 +0300

    Create Program.cs

---
+++ C# Fundamentals/Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b([A-Z][a-z]{1,}) ([A-Z][a-z]{1,})\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match item in matches)
+            {
+                Console.Write((item.Value) + " ");
+            }
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
