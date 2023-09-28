0d2a7e44b821121416bd5c4ad3b8066b62ed2b83
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-28 10:15:37 +0300

    Create Class1.cs

---
+++ C# Fundamentals/Reverse Polish Notation/Reverse Polish Notation/Reverse Polish Notation/Class1.cs
@@ -0,0 +1,81 @@
+﻿namespace Reverse_Polish_Notation
+{
+
+    using System;
+    using System.Linq;
+    public class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            string? input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+            string result = ReversePolishNotationConvertor(input, stack);
+            Console.WriteLine((result));
+            // int finalResult = CalculateReversePolishNotation(result);
+            // Console.WriteLine(finalResult);
+        }
+
+        public static string ReversePolishNotationConvertor(string input, Stack<char> stack)
+        {
+            string result = string.Empty;
+
+            foreach (var symbol in input)
+            {
+                if (symbol == '(')
+                {
+                    stack.Push(symbol);
+                }
+                else if (symbol == '*' || symbol == '/')
+                {
+                    stack.Push(symbol);
+                }
+                else if (symbol == '+' || symbol == '-')
+                {
+                    while (stack.Count > 0 && (stack.Peek() == '+' || stack.Peek() == '-' || stack.Peek() == '*' || stack.Peek() == '/'))
+                    {
+                        char currSymbol = stack.Pop();
+                        result += currSymbol;
+                    }
+                    if (symbol != '(' || symbol != ')')
+                    {
+                        stack.Push(symbol);
+                    }
+
+                }
+                else if (symbol == ')')
+                {
+                    while (stack.Peek() != '(' && stack.Count > 0)
+                    {
+                        char currSymbol = stack.Pop();
+                        if (currSymbol != '(')
+                        {
+                            result += currSymbol;
+                        }
+
+
+                    }
+
+                    if (stack.Peek() == '(' && stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+
+                }
+                else
+                {
+                    result += symbol;
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+
+            return result;
+        }
+
+    }
+}
