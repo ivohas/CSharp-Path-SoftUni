c3795b85564a06e6dde045789e7770bcb942e413
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-29 10:03:40 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawInput = Console.ReadLine();
+
+            char[] CaesarCipher = new char[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                CaesarCipher[i] = (char)(rawInput[i] + 3);
+            }
+
+            Console.WriteLine(CaesarCipher);
+        }
+    }
+}
