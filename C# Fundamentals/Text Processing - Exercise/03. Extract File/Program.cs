9fa46c17c7c895a81ae448f3e681c8900735304b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-29 10:03:32 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            string fileInfo = filePath.Substring(filePath.LastIndexOf('\\') + 1);
+
+            string fileName = fileInfo.Substring(0, fileInfo.LastIndexOf('.'));
+            string fileExtension = fileInfo.Substring(fileInfo.LastIndexOf('.') + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+
+        }
+    }
+}
