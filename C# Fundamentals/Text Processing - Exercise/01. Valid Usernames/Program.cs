d592f8590a3ba2a9fdae094d96aba4f066802ea3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-29 10:03:18 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawInput = Console.ReadLine();
+
+            string[] usernames = rawInput.Split(", ");
+
+            List<string> validatedUsernames = new List<string>();
+
+            bool isValid = false;
+
+            foreach (string username in usernames)
+            {
+                if (isUsernameValid(username))
+                {
+                    validatedUsernames.Add(username);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, validatedUsernames));
+
+        }
+
+        static bool isUsernameValid(string username)
+        {
+            if (username.Length >= 3 && username.Length <= 16)
+            {
+                foreach (char symbol in username)
+                {
+                    if (char.IsLetterOrDigit(symbol) || symbol == '-' || symbol == '_')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
