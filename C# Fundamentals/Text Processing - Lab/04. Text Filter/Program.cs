a616b1c610cca2029adbb26f5e3232d16e13a9db
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-30 11:12:37 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            foreach (string word in bannedWords)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
