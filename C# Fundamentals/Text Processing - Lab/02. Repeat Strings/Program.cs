1af2c4dbd66b0abb3050cb051544f57db8666ed5
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-30 11:12:27 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+
+            StringBuilder wordsArr = new StringBuilder();
+
+            foreach (var word in words)
+            {
+
+                int count = word.Length;
+                
+                for (int i = 0; i < count; i++)
+                {
+                    wordsArr.Append(word);
+                }
+            }
+
+            Console.WriteLine(wordsArr);
+        }
+    }
+}
