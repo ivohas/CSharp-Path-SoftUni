666f1b3a47087cdcf23e49f2864c554d99fa757e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-30 11:12:42 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine(text.Where(x => char.IsDigit(x)).ToArray());
+            Console.WriteLine(text.Where(x => char.IsLetter(x)).ToArray());
+            Console.WriteLine(text.Where(x => !char.IsLetterOrDigit(x)).ToArray());
+        }
+    }
+}
