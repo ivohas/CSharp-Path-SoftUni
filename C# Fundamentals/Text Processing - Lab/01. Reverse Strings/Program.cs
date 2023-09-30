cc0a6b1f9d6edc3bca3a60e0e91169e13430e98f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-09-30 11:12:24 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                char[] wordArr = command.ToCharArray();
+                Array.Reverse(wordArr);
+                string reversedWord = new string(wordArr);
+
+                Console.WriteLine($"{command} = {reversedWord}");
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
