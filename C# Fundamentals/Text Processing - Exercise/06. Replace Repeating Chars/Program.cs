ace1c7710feb42c1cfc694314ef92efbe91326df
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-01 11:02:03 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> text = Console.ReadLine().ToList();
+
+            for (int i = 0; i < text.Count - 1; i++)
+            {
+                char currentChar = text[i];
+                char nextChar = text[i + 1];
+                if (currentChar == nextChar)
+                {
+                    text.RemoveAt(i);
+                    i--;
+                }
+            }
+            Console.WriteLine(String.Join("", text));
+        }
+    }
+}
