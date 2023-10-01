636771a63532d8388b4492b6881f66981245a337
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-01 11:01:59 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            BigInteger result = number * multiplier;
+
+            Console.WriteLine(result);
+        }
+    }
+}
