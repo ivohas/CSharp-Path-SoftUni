0ab709320ad484631070ac1450a0fd8ed4736617
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-01 11:02:10 +0300

    Create Program.cs

---
+++ C# Fundamentals/Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            StringBuilder outputText = new StringBuilder();
+            int bombPower = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                char currChar = inputString[i];
+
+                if (currChar == '>')
+                {
+                    int currBombPower = GetIntValueOfCharacter(inputString[i + 1]); 
+
+                    outputText.Append(currChar);
+                    bombPower += currBombPower;
+                }
+                else
+                {
+                    if (bombPower > 0)
+                    {
+                        bombPower --;
+                    }
+                    else
+                    {
+                        outputText.Append(currChar);
+                    }
+                }
+            }
+
+            Console.WriteLine(outputText.ToString());
+
+        }
+
+        static int GetIntValueOfCharacter(char ch)
+        {
+            return (int)ch - 48;
+        }
+    }
+}
