bd40d5a651f86644b62fbf530f2870881195ab2a
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-03 17:42:57 +0300

    Create Type.cs

---
+++ C# Web/ASP.NET Fundamentals/Exam/a/Homies_Skeleton/Homies/Data/Models/Type.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Data.Models
+{
+    using static Common.EntityValidationConstraints.Type;
+    public class Type
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public List<Event> Events { get; set; } = new List<Event>();
+    }
+}
