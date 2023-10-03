e8b1b04b0599e04de1911f85c9950f056a47bb17
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-03 17:43:32 +0300

    Create AddEventViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/Exam/a/Homies_Skeleton/Homies/Models/AddEventViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Homies.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Models
+{
+    using static Common.EntityValidationConstraints.Event;
+    public class AddEventViewModel
+    {
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(DescriptionMinLength)]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string Start { get; set; } = null!;
+
+        [Required]
+        public string End { get; set; } = null!;
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int TypeId { get; set; }
+
+        public List<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+
+    }
+}
