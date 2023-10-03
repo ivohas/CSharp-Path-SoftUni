7cf83b12f250dd6efed71c3f4d718dbad3992a26
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-03 17:42:55 +0300

    Create Event.cs

---
+++ C# Web/ASP.NET Fundamentals/Exam/a/Homies_Skeleton/Homies/Data/Models/Event.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homies.Data.Models
+{
+    using static Common.EntityValidationConstraints.Event;
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(DescriptionMinLength)]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Organiser))]
+        public string OrganiserId { get; set; } = null!;
+
+        [Required]
+        public IdentityUser Organiser { get; set; } = null!;
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd H:mm}")]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd H:mm}")]
+        public DateTime Start { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd H:mm}")]
+        public DateTime End { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Type))]
+        public int TypeId { get; set; }
+
+        [Required]
+        public Type Type { get; set; } = null!;
+
+        public List<EventParticipant> EventsParticipants { get; set; } = new List<EventParticipant>();
+
+    }
+}
