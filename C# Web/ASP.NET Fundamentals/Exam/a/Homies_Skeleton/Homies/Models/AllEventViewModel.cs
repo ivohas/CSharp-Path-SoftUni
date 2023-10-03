63d3be5eb542a67670016c2c058a92299a36f694
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-03 17:43:40 +0300

    Create AllEventViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/Exam/a/Homies_Skeleton/Homies/Models/AllEventViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Homies.Models
+{
+    public class AllEventViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Start { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public string Organiser { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? End { get; set; }
+
+        public string? CreatedOn { get; set; }
+
+        public int? TypeId { get; set; }
+        public List<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+    }
+}
