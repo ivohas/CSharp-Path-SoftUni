3a92c1f6d2cdf8b07b0e7dc04e80254e79d49d33
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-04 08:58:44 +0300

    Create HomiesDbContext.cs

---
+++ C# Web/ASP.NET Fundamentals/Exam/a/Homies_Skeleton/Homies/Data/HomiesDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Homies.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Data
+{
+    using Type = Data.Models.Type;
+    public class HomiesDbContext : IdentityDbContext
+    {
+        public HomiesDbContext(DbContextOptions<HomiesDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Type>()
+                .HasData(new Type()
+                {
+                    Id = 1,
+                    Name = "Animals"
+                },
+                new Type()
+                {
+                    Id = 2,
+                    Name = "Fun"
+                },
+                new Type()
+                {
+                    Id = 3,
+                    Name = "Discussion"
+                },
+                new Type()
+                {
+                    Id = 4,
+                    Name = "Work"
+                });
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasKey(x => new { x.EventId, x.HelperId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Type> Types { get; set; }
+
+        public DbSet<EventParticipant> EventParticipants { get; set; }
+    }
+}
