4a97d70caf3b57f9eaa54fbe778af85d0f582dfe
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-04 08:58:41 +0300

    Create HomeController.cs

---
+++ C# Web/ASP.NET Fundamentals/Exam/a/Homies_Skeleton/Homies/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Homies.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homies.Controllers
+{
+    public class HomeController : BaseController
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Event");
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
