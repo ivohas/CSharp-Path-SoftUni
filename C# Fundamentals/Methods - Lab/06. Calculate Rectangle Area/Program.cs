5f2f1d3b32f27151d5cc838527c45e699a4d9c84
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-05 16:47:07 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            RectangleArea(a, b);
+        }
+
+        static void RectangleArea(int a, int b)
+        {
+            int sum = a * b;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
