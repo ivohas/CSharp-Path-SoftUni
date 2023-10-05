3f04f5c1b64b4c1a84d7aacc764084eeeaef6e30
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-05 17:59:54 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double powerResult = MathPower(@base, power);
+            Console.WriteLine(powerResult);
+        }
+
+        static double MathPower(double @base, double power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= @base;
+            }
+
+            return result;
+        }
+    }
+}
