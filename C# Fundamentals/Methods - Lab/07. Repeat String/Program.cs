738afbf021c1c32a9c03cfb0979bcb11531c4154
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-05 16:49:13 +0300

    Create Program.cs

---
+++ C# Fundamentals/Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            Cycle(line, n);
+        }
+
+        static void Cycle(string line, int n)
+        {
+            for (int i = 0; i<n; i++)
+			{
+                Console.Write(line);
+			}
+        }
+    }
+}
