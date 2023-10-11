3dd9085d0c4cd21a65d6cbde5de173440d0d89d2
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-11 18:15:38 +0300

    Create IEventService.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Event/Homies/Contracts/IEventService.cs
@@ -0,0 +1,17 @@
+﻿using Homies.Models;
+
+namespace Homies.Contracts
+{
+    public interface IEventService
+    {
+        Task AddEventAsync(AddOrEditViewModel model, string? userId, string orginezer);
+        Task AddToCollectionAsync(string userId, AddOrEditViewModel eventa);
+        Task EditEventAsync(AddOrEditViewModel model);
+        Task<ICollection<AllEventViewModel>> GetAllEventAsync();
+        Task<ICollection<TypeViewModel>> GetAllTypesAsync();
+        Task<AddOrEditViewModel?> GetEventByIdAsync(int id);
+        Task<DetailsViewModel> GetEventDetailsByIdAsync(int id);
+        Task<ICollection<AllEventViewModel>> GetJoinedEventsByIdAsync(string userId);
+        Task LeaveThisEventAsync(string userId, AddOrEditViewModel eventa);
+    }
+}
