d1d5ba8d36ac327d2552f279f0a073c844b97401
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-11 18:15:53 +0300

    Create Event.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Event/Homies/Data/Models/Event.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homies.Data.Models
+{
+    public class Event
+    {
+        public Event()
+        {
+            this.EventsParticipants = new HashSet<EventParticipant>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(150)]
+        public string Description { get; set; } = null!;
+        [Required]
+        public string OrganiserId { get; set; } = null!;
+        [Required]
+        [ForeignKey(nameof(OrganiserId))]
+        public IdentityUser Organiser { get; set; } = null!;
+
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime Start{ get; set; }
+
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime End { get; set; }
+
+        [Required]
+        public int TypeId { get; set; }
+        [ForeignKey(nameof(TypeId))]
+        [Required]
+        public Type Type { get; set; } = null!;
+
+        public ICollection<EventParticipant> EventsParticipants { get; set; }
+
+    }
+}
