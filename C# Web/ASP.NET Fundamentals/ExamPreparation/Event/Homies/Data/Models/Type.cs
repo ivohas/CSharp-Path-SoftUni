4cbc5f29a2c5726c11f98963957f201427066fc9
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-12 09:54:42 +0300

    Create Type.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Event/Homies/Data/Models/Type.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Data.Models
+{
+    public class Type
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string Name { get; set; }
+
+        public ICollection<Event> Events { get; set; }
+    }
+}
