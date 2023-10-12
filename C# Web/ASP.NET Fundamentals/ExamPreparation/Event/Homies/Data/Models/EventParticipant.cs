ed9dc76049670a816c7a20cbebc348cc604e9536
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-12 09:54:39 +0300

    Create EventParticipant.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Event/Homies/Data/Models/EventParticipant.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homies.Data.Models
+{
+    public class EventParticipant
+    {
+        [Required]
+        public string HelperId { get; set; } = null!;
+        [ForeignKey(nameof(HelperId))]
+        public IdentityUser Helper { get; set; }
+        [Required]
+        public int EventId { get; set; }
+        [ForeignKey(nameof(EventId))]
+        public Event Event { get; set; }
+    }
+}
