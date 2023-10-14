1ef810a70f9658b424a17f7ecd521e8345c51289
ivohas <105879092+ivohas@users.noreply.github.com>
2023-10-14 17:27:42 +0300

    Create DetailsViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Event/Homies/Models/DetailsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Models
+{
+    public class DetailsViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(150, MinimumLength = 15)]
+        public string Description { get; set; } = null!;
+        [Required]
+        public string Organiser { get; set; } = null!;
+
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime Start { get; set; }
+
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime End { get; set; }
+        [Required]
+        public string Type { get; set; } = null!;
+    }
+}
