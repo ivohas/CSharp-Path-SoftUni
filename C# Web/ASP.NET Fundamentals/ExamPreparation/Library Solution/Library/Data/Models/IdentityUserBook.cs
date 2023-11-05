f6e09d6aaff7395f9cebeefc387565fa8aa12028
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-05 12:00:21 +0200

    Create IdentityUserBook.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Library Solution/Library/Data/Models/IdentityUserBook.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Data.Models
+{
+    public class IdentityUserBook
+    {
+        public string CollectorId { get; set; } = null!;
+        [ForeignKey(nameof(CollectorId))]
+        public IdentityUser Collector { get; set; } = null!;
+
+        public int BookId { get; set; }
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; } = null!;
+    }
+}
