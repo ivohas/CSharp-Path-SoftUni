b22cb3ca66df7a596843e48b8b115b65a5801eee
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-05 12:00:41 +0200

    Create AllBookViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Library Solution/Library/Models/AllBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class AllBookViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;       
+        public string Author { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal? Rating { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
