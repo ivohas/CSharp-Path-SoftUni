2b48532f68ab980901727dd6c1165622c6aea560
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-16 19:18:52 +0200

    Create BaseController.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Library Solution/Library/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        protected string? GetUserId()
+        {
+            string? id = string.Empty;
+            if (User != null)
+            {
+                id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+            return id;
+        }
+    }
+}
