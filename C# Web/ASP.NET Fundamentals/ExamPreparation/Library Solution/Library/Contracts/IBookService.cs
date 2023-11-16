39d83dd8eee6ab15f68818100f33379b9495915e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-16 19:18:49 +0200

    Create IBookService.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/Library Solution/Library/Contracts/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using Library.Models;
+
+namespace Library.Contracts
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync();
+
+        Task<IEnumerable<AllBookViewModel>> GetMyBooksAsync(string userId);
+        Task<BookViewModel?> GetBookByIdAsync(int id);
+        Task AddBookToCollectionAsync(string? userId, BookViewModel book);
+        Task RemoveBookFromCollectionAsync(string? userId, BookViewModel bookToRemove);
+        Task<AddBookViewModel> GetNewAddBookModelAsync();
+        Task AddBookAsync(AddBookViewModel model);
+    }
+}
