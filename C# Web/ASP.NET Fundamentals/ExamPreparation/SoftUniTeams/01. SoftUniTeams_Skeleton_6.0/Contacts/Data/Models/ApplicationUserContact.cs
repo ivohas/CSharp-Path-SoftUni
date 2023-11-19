8339be00983461fb396ba93f11e91977ec0214ee
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-19 09:40:28 +0200

    Create ApplicationUserContact.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/SoftUniTeams/01. SoftUniTeams_Skeleton_6.0/Contacts/Data/Models/ApplicationUserContact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contacts.Data.Entites
+{
+    public class ApplicationUserContact
+    {
+        [Required]
+        [ForeignKey(nameof(ApplicationUserId))]
+        public string ApplicationUserId { get; set; } = null!;
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [ForeignKey(nameof(ContactId))]
+        public int ContactId { get; set; }
+
+        public Contact Contact { get; set; }
+    }
+}
+//•	ApplicationUserId – a string, Primary Key, foreign key (required)
+//•	ApplicationUser – ApplicationUser
+//•	ContactId – an integer, Primary Key, foreign key (required)
+//•	Contact – Contact
