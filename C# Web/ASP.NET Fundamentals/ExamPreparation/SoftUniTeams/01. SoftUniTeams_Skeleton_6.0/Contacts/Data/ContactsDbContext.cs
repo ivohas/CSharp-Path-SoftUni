732d88116d03350bf006a3f2754f53aa9d250130
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-23 17:02:15 +0200

    Create ContactsDbContext.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/SoftUniTeams/01. SoftUniTeams_Skeleton_6.0/Contacts/Data/ContactsDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Contacts.Data.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Data
+{
+    public class ContactsDbContext : IdentityDbContext
+    {
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+            : base(options)
+        {
+            /* builder
+                .Entity<Contact>()
+                .HasData(new Contact()
+                {
+                    Id = 1,
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    PhoneNumber = "+359881223344",
+                    Address = "Gotham City",
+                    Email = "imbatman@batman.com",
+                    Website = "www.batman.com"
+                });
+            */
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUserContact>()
+                .HasKey(au => new { au.ApplicationUserId, au.Contact });
+        }
+    }
+}
