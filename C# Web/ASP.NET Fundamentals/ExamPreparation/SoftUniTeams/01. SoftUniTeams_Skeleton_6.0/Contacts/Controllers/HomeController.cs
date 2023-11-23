76c0e3d142e513cb16c2bcc560da1f6715cd2f8c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-11-23 17:02:12 +0200

    Create HomeController.cs

---
+++ C# Web/ASP.NET Fundamentals/ExamPreparation/SoftUniTeams/01. SoftUniTeams_Skeleton_6.0/Contacts/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Contacts.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Contacts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
