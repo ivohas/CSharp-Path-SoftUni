8752c4a4c56396327dbabf1620d534f45dfd7b81
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-11 18:35:26 +0200

    Create Post.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop Fail/ShoppingListApp/Forum.Data.Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Data.Models;
+
+
+public class Post
+{
+
+    public int Id { get; init; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [MaxLength(1500)]
+    public string Content { get; set; } = null!;
+}
