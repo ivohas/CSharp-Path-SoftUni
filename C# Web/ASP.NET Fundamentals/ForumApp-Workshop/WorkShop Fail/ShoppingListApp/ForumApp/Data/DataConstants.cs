53e2c65844fb6229b0129e12a1101c2386f641e3
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-12 20:21:32 +0200

    Create DataConstants.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop Fail/ShoppingListApp/ForumApp/Data/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ForumApp.Data;
+
+public class DataConstants
+{
+    public class Post
+    {
+        public const int TitleMaxLenght = 50;
+        public const int ContentMaxLenght = 1_500;
+    }
+
+}
