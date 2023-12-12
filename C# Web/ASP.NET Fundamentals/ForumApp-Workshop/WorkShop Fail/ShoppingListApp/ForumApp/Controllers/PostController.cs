4c442b7dc857ba56486fecae75a7a1b16e28160f
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-12 20:21:30 +0200

    Create PostController.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop Fail/ShoppingListApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumAppDbContext _data;
+        public PostController(ForumAppDbContext data)
+        {
+            _data = data;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var post = await _data
+                .Posts
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content
+                })
+                .ToListAsync();
+            return View(post);
+        }
+    }
+}
