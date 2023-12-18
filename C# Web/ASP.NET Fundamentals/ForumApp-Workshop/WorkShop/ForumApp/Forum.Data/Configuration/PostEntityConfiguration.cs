60dc6762faebc56aaaa363c07b7eaf08551bb4fc
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:32 +0200

    Create PostEntityConfiguration.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.Data/Configuration/PostEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ForumApp.Data.Models;
+using ForumApp.Data.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ForumApp.Data.Configuration
+{
+    public class PostEntityConfiguration : IEntityTypeConfiguration<Post>
+    {
+        private readonly PostSeader postSeader;
+        public PostEntityConfiguration()
+        {
+            this.postSeader = new PostSeader();       
+        }
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasData(this.postSeader.GeneratePosts());
+             
+            
+        }
+        
+    }
+}
