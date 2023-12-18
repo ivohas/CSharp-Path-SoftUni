0d599cf1c44081b0d5ae343f64eddac771472766
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:36 +0200

    Create ForumDbConstext.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.Data/ForumDbConstext.cs
@@ -0,0 +1,27 @@
+﻿using ForumApp.Data.Configuration;
+using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ForumApp.Data
+{
+    public class ForumDbConstext : DbContext
+    {
+
+        public ForumDbConstext(DbContextOptions<ForumDbConstext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modulBuilder)
+        {
+
+            modulBuilder.ApplyConfiguration(new PostEntityConfiguration());
+
+            base.OnModelCreating(modulBuilder);
+        }
+    }
+}
