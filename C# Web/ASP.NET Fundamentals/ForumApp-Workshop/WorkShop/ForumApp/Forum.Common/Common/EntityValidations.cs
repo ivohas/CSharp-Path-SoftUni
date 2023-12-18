592d1144c6d872b6d2f100ec36003dc8fecca2f5
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:15 +0200

    Create EntityValidations.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.Common/Common/EntityValidations.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumApp.Common
+{
+    public static class EntityValidations
+    {
+        public static class Post
+        {
+            public const int TitleMinLength = 10;
+            public const int TitleMaxLength = 50;
+            public const int ContentMinLenght = 30;
+            public const int ContentMaxLeght = 1_500;
+        }
+    }
+}
