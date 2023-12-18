1fa34a64b343b388f836dd9b88d9a7a579bebf02
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:45 +0200

    Create PostSeader.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.Data/Seeding/PostSeader.cs
@@ -0,0 +1,39 @@
+﻿using ForumApp.Data.Models;
+
+namespace ForumApp.Data.Seeding
+{
+    class PostSeader
+    {
+
+        ICollection<Post> posts = new HashSet<Post>();
+        Post currentPost;
+        internal Post[] GeneratePosts()
+        {         
+            currentPost = new Post()
+            {
+
+                Title = "My first post",
+                Content = "My first post will be about performing " +
+                "CRUD operations in MVC. It's so much fun!"
+            };
+            posts.Add(currentPost);
+            currentPost = new Post()
+            {
+
+                Title = "My second post.",
+                Content = "This is my second post. " +
+                "CRUD operations in MVC are getting more and more interesting!"
+            };
+            posts.Add(currentPost);
+            currentPost = new Post()
+            {
+
+                Title = "My third post",
+                Content = "Hello there! I'm getting better and better with the" +
+                "CDUD operation in MVC. Stay tuned!"
+            };
+            posts.Add(currentPost);
+            return posts.ToArray();
+        }
+    }
+}
