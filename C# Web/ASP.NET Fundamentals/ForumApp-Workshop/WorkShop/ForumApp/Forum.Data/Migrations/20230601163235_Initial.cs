4795847a58160054b84eb7a8552ecaf404198fa7
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:39 +0200

    Create 20230601163235_Initial.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.Data/Migrations/20230601163235_Initial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts", 
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { new Guid("464e42df-464c-477c-835b-a1ea33f86522"), "Hello there! I'm getting better and better with theCDUD operation in MVC. Stay tuned!", "My third post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { new Guid("61c5ffa8-53d9-4827-9444-b5c385a8e7e9"), "My first post will be about performing CRUD operations in MVC. It's so much fun!", "My first post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { new Guid("ffc539f2-1f06-4ffa-a923-dbc50486eba7"), "This is my second post. CRUD operations in MVC are getting more and more interesting!", "My second post." });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
