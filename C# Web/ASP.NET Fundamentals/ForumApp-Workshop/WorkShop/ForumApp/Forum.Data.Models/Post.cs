6b6b1d7aae4c43e1f287bf2c738c622a61e7b32d
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:27 +0200

    Create Post.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.Data.Models/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace ForumApp.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Post
+{
+    public Post()
+    {
+        this.Id = Guid.NewGuid();
+    }
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [MaxLength(1500)]
+    public string Content { get; set; } = null!;
+}
