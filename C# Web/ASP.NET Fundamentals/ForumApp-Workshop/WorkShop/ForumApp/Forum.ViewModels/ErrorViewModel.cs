685394896a165b1b7a8af7931291e2f189c3d318
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-18 19:17:48 +0200

    Create ErrorViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/ForumApp-Workshop/WorkShop/ForumApp/Forum.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ForumApp.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
