a5be09e4f6f8708025007af5d95e2c2672e648ab
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-21 20:41:47 +0200

    Create HomeController.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Homies.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SoftUniBazar.Models;
+using System.Diagnostics;
+
+namespace SoftUniBazar.Controllers
+{
+    public class HomeController : BaseController
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("All", "Ad");
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
