e6ed233ffae051619b1dc48db30f0c81cba61ddd
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-21 20:41:59 +0200

    Create Category.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Data/Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Common.ValidationConstants.CategoryConsts;
+namespace SoftUniBazar.Data.Model
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Ads = new List<Ad>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Ad> Ads { get; set; }
+    }
+}
