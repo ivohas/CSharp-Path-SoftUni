b9fad5a0f16a9340471032ac642b92da932b064c
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-21 20:41:57 +0200

    Create AdBuyer.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Data/Model/AdBuyer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftUniBazar.Data.Model
+{
+    public class AdBuyer
+    {
+        [Required]
+        public string BuyerId { get; set; } = null!;
+        [ForeignKey(nameof(BuyerId))]
+        public IdentityUser Buyer { get; set; }
+        [Required]
+        public int AdId { get; set; }
+        [ForeignKey(nameof(AdId))]
+        public Ad Ad { get; set; }
+    }
+}
