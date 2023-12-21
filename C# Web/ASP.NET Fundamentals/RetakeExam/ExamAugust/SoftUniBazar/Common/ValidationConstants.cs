1c6e9d644034ddf3a05b2a9bce009cd57ddbe033
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-21 20:41:36 +0200

    Create ValidationConstants.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Common/ValidationConstants.cs
@@ -0,0 +1,24 @@
+﻿using Humanizer.Localisation.TimeToClockNotation;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
+
+namespace SoftUniBazar.Common
+{
+    public static class ValidationConstants
+    {
+
+        public static class AdConsts
+        {
+            public const int NameMaxLenght = 25;
+            public const int NameMinLenght = 5;
+
+            public const int DescriptionMaxLenght = 250;
+            public const int DescriptionMinLenght = 15;
+        }
+
+        public static class CategoryConsts
+        {
+            public const int NameMaxLength = 15;
+            public const int NameMinLegt = 3;
+        }
+    }
+}
