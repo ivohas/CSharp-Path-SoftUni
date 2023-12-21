14c2202cbf6f66b4eefeaf0b0f25d66370d768b3
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-21 20:41:38 +0200

    Create IAdService.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Contracts/IAdService.cs
@@ -0,0 +1,16 @@
+﻿using SoftUniBazar.Models;
+namespace SoftUniBazar.Contracts
+{
+    public interface IAdService
+    {
+        Task AddEventAsync(AddOrEditViewModel model, string v);
+        Task AddToCollectionAsync(string userId, AddOrEditViewModel eventa);
+        Task<ICollection<AllAdsViewModel>> AdsInTheCartViewModel(string userId);
+        Task<ICollection<AllAdsViewModel>> AllAdsAsync();
+        Task<ICollection<CategoryViewModel>> AllCategoriesAsync();
+        Task<bool> AlreadyAdded(string userId, AddOrEditViewModel ad);
+        Task EditAdAsync(AddOrEditViewModel model, int id);
+        Task<AddOrEditViewModel> GetAdByIdAsync(int id);
+        Task LeaveThisEventAsync(string userId, AddOrEditViewModel eventa);
+    }
+}
