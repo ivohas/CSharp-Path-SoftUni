a09e8b3fb45b919527094e8dbee03f54dc3db935
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-21 20:41:46 +0200

    Create BaseController.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Homies.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        protected string GetUserId()
+        {
+            string id = string.Empty;
+
+            if (User != null)
+            {
+                id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+
+            return id;
+        }
+    }
+}
