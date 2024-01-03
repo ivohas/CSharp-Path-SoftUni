8d89a0b415fb8743458e087fc3a393c7e8c519d3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-03 19:54:50 +0200

    Create BoardAllViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy - Copy/TaskBoardApp.Web.ViewModels/Board/BoardAllViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TaskBoardApp.Web.ViewModels.Task;
+
+namespace TaskBoardApp.Web.ViewModels.Board
+{
+    public class BoardAllViewModel
+    {
+        public string Name { get; set; } = null!;
+
+        public ICollection<TaskViewModel> Tasks { get; set; } = null!;
+    }
+}
