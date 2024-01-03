deb0ef6d64087f0aafc3abe54e8ffeeb6fea2de6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-03 19:53:17 +0200

    Create 20230816064948_New.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Migrations/20230816064948_New.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftUniBazar.Data.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Ads",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Ads",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
