a4963207db8a7a2d7a5000e889549a3f4afe3549
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-03 19:54:41 +0200

    Create TaskService.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy - Copy/TaskBoardApp.Services/TaskService.cs
@@ -0,0 +1,32 @@
+﻿using TaskBoard.Data;
+using TaskBoardApp.Services.Interfaces;
+using TaskBoardApp.Web.ViewModels.Task;
+
+namespace TaskBoardApp.Services
+{
+    public  class TaskService : ITaskService
+    {
+
+        private readonly TaskBoardDbContext dbContext;
+
+        public TaskService(TaskBoardDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddAsync(string ownerId, TaskFormModel viewModel)
+        {
+            Data.Models.Task task = new Data.Models.Task()
+            {
+                Title = viewModel.Title,
+                Description = viewModel.Description,
+                BoardId = viewModel.BoardId,
+                CreatedOn = DateTime.UtcNow,
+                OwnerId = ownerId
+            };
+
+            await this.dbContext.Tasks.AddAsync(task);
+            await this.dbContext.SaveChangesAsync();
+        }
+    }
+}
