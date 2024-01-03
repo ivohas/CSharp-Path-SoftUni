46a0a973de5e5a55ad95f03be17828e7605bcd94
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-03 19:54:27 +0200

    Create ITaskService.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy - Copy/TaskBoardApp.Services/Interfaces/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBoardApp.Web.ViewModels.Task;
+
+namespace TaskBoardApp.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        Task AddAsync(string userId, TaskFormModel viewModel);
+    }
+}
