22f7196f91183f71131a87c4ffa14728be0bb174
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-04 17:12:14 +0200

    Create CategoryViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Models/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Common.ValidationConstants.CategoryConsts;
+namespace SoftUniBazar.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+    }
+}
