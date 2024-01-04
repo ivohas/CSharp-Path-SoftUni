0b5f79517103b4f5e4a16c4b1557b42404f9ad5f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-04 17:12:12 +0200

    Create AllAdsViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Models/AllAdsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SoftUniBazar.Data.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Common.ValidationConstants.AdConsts;
+namespace SoftUniBazar.Models
+{
+    public class AllAdsViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(NameMaxLenght)]
+        public string Name { get; set; } = null!;
+        [StringLength(DescriptionMaxLenght)]
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Owner { get; set; } = null!;
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public string Category { get; set; }
+    }
+}
