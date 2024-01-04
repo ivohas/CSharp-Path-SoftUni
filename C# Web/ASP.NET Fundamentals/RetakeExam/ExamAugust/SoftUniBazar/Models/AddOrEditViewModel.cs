c32d667543108c41c23a30a6b95030d78d0edaf0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-04 17:12:10 +0200

    Create AddOrEditViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/RetakeExam/ExamAugust/SoftUniBazar/Models/AddOrEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Common.ValidationConstants.AdConsts;
+namespace SoftUniBazar.Models
+{
+    public class AddOrEditViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(NameMaxLenght, MinimumLength = NameMinLenght)]
+        public string Name { get; set; } = null!;
+        [StringLength(DescriptionMaxLenght, MinimumLength = DescriptionMinLenght)]
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public decimal Price { get; set; }      
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd H:mm")]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public List<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+    }
+}
