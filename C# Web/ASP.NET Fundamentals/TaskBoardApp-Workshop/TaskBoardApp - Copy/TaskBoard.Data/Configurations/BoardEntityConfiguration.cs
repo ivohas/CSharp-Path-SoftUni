5e9482d259ae2cc2cb08c7c4a26848ebf8d3f6db
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-06 15:17:34 +0200

    Create BoardEntityConfiguration.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoard.Data/Configurations/BoardEntityConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskBoardApp.Data.Models;
+
+namespace TaskBoard.Data.Configurations
+{
+    internal class BoardEntityConfiguration : IEntityTypeConfiguration<Board>
+    {
+        public void Configure(EntityTypeBuilder<Board> builder)
+        {
+            ICollection<Board> boards = this.GenereBoards();
+
+            builder
+                .HasData(boards);
+        }
+        private ICollection<Board> GenereBoards()
+        {
+            ICollection<Board> boards = new HashSet<Board>();
+
+            Board currentBoard;
+            currentBoard = new Board()
+            {
+                Name = "Open",
+                Id = 1
+            };
+            boards.Add(currentBoard);
+            currentBoard = new Board()
+            {
+                Id = 2,
+                Name = "In Progress"
+            };
+            boards.Add(currentBoard);
+            currentBoard = new Board()
+            {
+                Id = 3,
+                Name = "Done"
+            };
+            boards.Add(currentBoard);
+
+            return boards;
+        }
+    }
+}
