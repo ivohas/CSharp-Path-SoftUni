aab84d92a259edbb710aa0c1d32f83fc01f0cbb9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-06 15:17:02 +0200

    Create BoardController.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy - Copy/TaskBoardApp/Controllers/BoardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskBoardApp.Services.Interfaces;
+using TaskBoardApp.Web.ViewModels.Board;
+
+namespace TaskBoardApp.Controllers
+{
+    [Authorize]
+    public class BoardController : Controller
+    {
+        private readonly IBoardService boardService;
+
+        public BoardController(IBoardService boardService)
+        {
+            this.boardService = boardService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            IEnumerable<BoardAllViewModel> allBoards = 
+                await this.boardService.AllAsync();
+            return View(allBoards);
+        }
+    }
+}
