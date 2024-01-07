43a33e683cce341dc23284549cb018c47ae3968b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-07 13:52:24 +0200

    Create TaskFormModel.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoardApp.Web.ViewModels/Task/TaskFormModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskBoardApp.Web.ViewModels.Board;
+using static TaskBoardApp.Common.EntityValidationConstants.Task;
+namespace TaskBoardApp.Web.ViewModels.Task
+{
+    public class TaskFormModel
+    {
+        [Required]
+        [StringLength(TitleMaxLenght, MinimumLength = TitleMinLenght,
+            ErrorMessage = "Title should be at least {2} characters long.")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLenght, MinimumLength = DescriptionMinLenght,
+            ErrorMessage = "Description should be at least {2} cheracters long.")]
+        public string Description { get; set; } = null!;
+
+        [Display(Name = "Board")]
+        public int BoardId { get; set; }
+
+        public IEnumerable<BoardSelectViewModel>? AllBoards { get; set; }
+    }
+}
