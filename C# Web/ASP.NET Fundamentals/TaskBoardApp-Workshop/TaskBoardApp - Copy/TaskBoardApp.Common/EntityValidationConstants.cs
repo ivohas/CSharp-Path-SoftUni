aa91218f1e24fa145a3aa645be3733dbe134a813
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-07 13:51:40 +0200

    Create EntityValidationConstants.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoardApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskBoardApp.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Task
+        {
+            public const int TitleMinLenght = 5;
+            public const int TitleMaxLenght = 70;
+
+            public const int DescriptionMinLenght = 10;
+            public const int DescriptionMaxLenght = 1000;
+        }
+        public static class Board
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLenght = 30;
+        }
+    }
+}
