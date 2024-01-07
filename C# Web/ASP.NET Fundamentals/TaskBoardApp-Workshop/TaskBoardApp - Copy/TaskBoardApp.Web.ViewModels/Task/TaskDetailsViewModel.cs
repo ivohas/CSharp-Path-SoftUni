35eaf1c97102d7b7f37d99e144861b9744fd2ef8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-07 13:52:22 +0200

    Create TaskDetailsViewModel.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoardApp.Web.ViewModels/Task/TaskDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoardApp.Web.ViewModels.Task
+{
+    public class TaskDetailsViewModel : TaskViewModel
+    {
+        public string CreatedOn { get; set; } = null!;
+        public string Board { get; set; } = null!;
+    }
+}
