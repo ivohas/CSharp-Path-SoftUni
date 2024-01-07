ca61dd5f2a6de86cdd0026fd9e38808d81062b98
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-07 13:51:45 +0200

    Create Task.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoardApp.Data.Models/Task.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static TaskBoardApp.Common.EntityValidationConstants.Task;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Task
+    {
+        public Task()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLenght)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLenght)]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        [ForeignKey(nameof(Board))]
+        public int BoardId { get; set; }
+
+        public virtual Board Board { get; set; } = null!;
+       
+        [Required]
+        [ForeignKey(nameof(Owner))]
+        public string OwnerId { get; set; } = null!;
+
+        public virtual IdentityUser Owner { get; set; } = null!;
+    }
+}
