2c4ae94f04a0ba4b6b0f25e53ecfd5658d5a231e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-07 13:51:44 +0200

    Create Board.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoardApp.Data.Models/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Common.EntityValidationConstants.Board;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Board
+    {
+        public Board()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLenght)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
