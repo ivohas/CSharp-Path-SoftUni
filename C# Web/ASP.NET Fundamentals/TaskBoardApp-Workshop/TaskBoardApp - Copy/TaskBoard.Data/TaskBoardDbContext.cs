d8c9e47ddc019bdb6819a1520de4be89a463a7dd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-07 13:51:38 +0200

    Create TaskBoardDbContext.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy/TaskBoard.Data/TaskBoardDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using TaskBoardApp.Data.Models;
+using Task = TaskBoardApp.Data.Models.Task;
+
+namespace TaskBoard.Data
+{
+    public class TaskBoardDbContext : IdentityDbContext<IdentityUser>
+    {
+        public TaskBoardDbContext(DbContextOptions<TaskBoardDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Board> Boards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(TaskBoardDbContext))??Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
