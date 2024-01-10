8b27b56b372da564f22aea7939d86811f408ad58
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-10 14:31:05 +0200

    Create ClaimsPrincipalExtention.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp - Copy - Copy/TaskBoardApp/Extensions/ClaimsPrincipalExtention.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace TaskBoardApp.Extensions
+{
+    public static class ClaimsPrincipalExtention
+    {
+
+        public static string GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
