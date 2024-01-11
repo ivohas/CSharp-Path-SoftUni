d639ebd6b15e76f3ff49e39720c6bdcca498d2a7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-11 13:55:04 +0200

    Create Animal.cs

---
+++ CSharp-Advanced--OOP/Advanced/CSharp_Advanced_Retake_Exam_13_April_2022/Zoo/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public string Species { get; set; }
+        public string Diet { get; set; }
+        public double Weight { get; set; }
+        public double Length { get; set; }
+
+        public Animal(string species, string diet, double wieght, double length)
+        {
+            this.Species = species;
+            this.Diet = diet;
+            this.Weight = wieght;
+            this.Length = length;
+        }
+
+        public override string ToString()
+        {
+            return $"The {this.Species} is a {this.Diet} and weighs {this.Weight} kg.";
+        }
+    }
+}
