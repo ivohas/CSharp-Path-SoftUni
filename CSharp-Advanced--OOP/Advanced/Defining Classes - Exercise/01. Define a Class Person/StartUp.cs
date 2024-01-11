a0016aa000e784000eae9c19e7f9334507453d54
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-11 13:55:27 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person person = new Person();
+            Person person2 = new Person("Gosho", 20);
+            Person person3 = new Person("IlchoKukata", 69);
+
+            Console.WriteLine($"{person.Name} is {person.Age} years old");
+        }
+    }
+}
