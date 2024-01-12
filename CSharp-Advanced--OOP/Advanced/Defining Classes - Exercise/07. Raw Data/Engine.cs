7887712792bc54b350974f74f10c68ff8f4561b7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-12 19:29:12 +0200

    Create Engine.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/07. Raw Data/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Raw_Data
+{
+    class Engine
+    {
+        private int speed;
+        private int power;
+
+        public Engine()
+        {
+
+        }
+        public Engine(int speed, int power)
+        {
+            this.speed = speed;
+            this.power = power;
+        }
+        public int Speed { get { return speed; } set { speed = value; } }
+        public int Power { get { return power; } set { power = value; } }
+    }
+}
