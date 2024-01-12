5c685d1c3793ded4b05dbd50e5e165d2f7252e67
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-12 19:28:08 +0200

    Create Person.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/02. Creating Constructors/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age)
+        {
+            this.name = "No name";
+            this.age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
