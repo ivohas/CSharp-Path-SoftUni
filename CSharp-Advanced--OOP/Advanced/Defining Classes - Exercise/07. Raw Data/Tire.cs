c68bd9e1a6ffba706aae8430745d4e4b5027794d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-12 19:29:23 +0200

    Create Tire.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/07. Raw Data/Tire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Raw_Data
+{
+    class Tire
+    {
+        private double tirePressure;
+        private int tireAge;
+
+        public Tire()
+        {
+
+        }
+        public Tire(double tirePressure, int tireAge)
+        {
+            this.tirePressure = tirePressure;
+            this.tireAge = tireAge;
+        }
+        public double TirePressure { get { return tirePressure; } set { tirePressure = value; } }
+        public int TireAge { get { return tireAge; } set { tireAge = value; } }
+    }
+}
