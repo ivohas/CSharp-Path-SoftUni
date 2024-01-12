8c2ced3ff5dac69aa983cf1694346d25dd83dd9a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-12 19:28:34 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string data = Console.ReadLine();
+                string name = data.Split()[0];
+                int age = int.Parse(data.Split()[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine(oldestPerson.Name + " " + oldestPerson.Age);
+        }
+    }
+}
