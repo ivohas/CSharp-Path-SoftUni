eb328b90ecda122537d340607639e6d4d6df136d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-12 19:28:47 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string data = Console.ReadLine();
+                string name = data.Split()[0];
+                int age = int.Parse(data.Split()[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            foreach (var member in family.familyMembers.OrderBy(x => x.Name))
+            {
+                if (member.Age > 30)
+                {
+                    Console.WriteLine($"{member.Name} - {member.Age}");
+                }
+            }
+
+        }
+    }
+}
