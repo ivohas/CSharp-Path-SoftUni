e11bcca91957ec1a9f06b141d65abcc1241da655
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-12 19:28:42 +0200

    Create Family.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> familyMembers { get; set; }
+        public Family()
+        {
+            this.familyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.familyMembers.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            int maxAge = this.familyMembers.Max(member => member.Age);
+            return this.familyMembers.First(member => member.Age == maxAge);
+        }
+    }
+}
