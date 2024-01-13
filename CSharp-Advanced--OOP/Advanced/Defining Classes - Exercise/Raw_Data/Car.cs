2c012af46cefbe78a961527e1d4ed26741a333ee
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-13 18:54:05 +0200

    Create Car.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/Raw_Data/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raw_Data
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private Cargo cargo;
+        private List<Tire> tireList;
+
+        public Car(string model, Engine engine, Cargo cargo, List<Tire> tires)
+        {
+            this.model = model;
+            this.engine = engine;
+            this.cargo = cargo;
+            this.tireList = tires;
+        }
+        public string Model { get { return model; } set { model = value; } }
+        public Engine Engine { get { return engine; } set { engine = value; } }
+        public Cargo Cargo { get { return cargo; } set { cargo = value; } }
+        public List<Tire> TireList { get { return tireList; } set { tireList = value; } }
+    }
+}
