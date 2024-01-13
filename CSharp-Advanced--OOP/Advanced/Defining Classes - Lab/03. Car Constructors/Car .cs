d483a7d2c2a10e0da1cd50b2d85405911bfb311e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-13 18:55:02 +0200

    Create Car .cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Lab/03. Car Constructors/Car .cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string Make;
+        private string Model;
+        private int Year;
+        private double FuelQuantity;
+        private double FuelConsumption;
+
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(double fuelQuantity, double fuelConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * this.FuelConsumption;
+            if (consumption <= this.FuelQuantity)
+                this.FuelQuantity -= consumption;
+            else
+                Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+        public string WhoAmI()
+        {
+            return
+            $"Make: {this.Make}\r\n" +
+            $"Model: {this.Model}\r\n" +
+            $"Year: {this.Year}\r\n" +
+            $"Fuel: {this.FuelQuantity:f2}L";
+        }
+    }
+}
