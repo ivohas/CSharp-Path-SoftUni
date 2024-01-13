b1debf9b542a37daf163e2caa7313c1f447c50c3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-13 18:54:08 +0200

    Create Cargo.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Exercise/Raw_Data/Cargo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raw_Data
+{
+    public class Cargo
+    {
+        private int weight;
+        private string type;
+
+        public Cargo()
+        {
+
+        }
+        public Cargo(int weight, string type)
+        {
+            this.weight = weight;
+            this.type = type;
+        }
+        public int Weight { get { return weight; } set { weight = value; } }
+        public string Type { get { return type; } set { type = value; } }
+    }
+}
