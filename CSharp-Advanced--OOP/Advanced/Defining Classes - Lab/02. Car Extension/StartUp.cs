e35ac588c6ca0631f944fb950bd7dc969f25100f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-13 18:54:55 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            Console.WriteLine(car.WhoAmI());
+
+            car.Drive(100);
+            Console.WriteLine(car.WhoAmI());
+
+            car.Drive(300);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
