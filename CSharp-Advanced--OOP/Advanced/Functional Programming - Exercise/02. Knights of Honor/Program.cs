d19c22eaa7f3533336fe7b766d75c3024f972a02
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-14 11:52:12 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            Action<string> printSirBeforeNames = name => Console.WriteLine($"Sir {name}");
+
+            names.ForEach(printSirBeforeNames);
+        }
+    }
+}
