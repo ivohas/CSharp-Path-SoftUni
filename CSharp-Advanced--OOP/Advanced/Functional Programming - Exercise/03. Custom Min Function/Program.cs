2e45fcefa800168855e3abe4e08824528e5611c8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-14 11:52:16 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            HashSet<int> set = new HashSet<int>();
+
+            for (int i = 0; i < values.Length; i++)
+                set.Add(values[i]);
+
+            Func<HashSet<int>, int> printMinNumber = minNumber => set.Min();
+            Console.WriteLine(printMinNumber(set));
+        }
+    }
+}
