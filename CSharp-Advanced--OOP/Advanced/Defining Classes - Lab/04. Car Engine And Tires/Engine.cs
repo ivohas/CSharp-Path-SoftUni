c0e5e36791ba2a276aedded038dfb2baf374b8d4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-14 11:51:33 +0200

    Create Engine.cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Lab/04. Car Engine And Tires/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+        public Engine(int horsePower, int cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("I am an engine. I am running.");
+        }
+    }
+}
