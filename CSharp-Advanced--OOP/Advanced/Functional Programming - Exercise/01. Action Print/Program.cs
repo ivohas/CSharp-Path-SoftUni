980d34ab70dd586e212f970680d5bf9bb1aabcd4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-14 11:52:06 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            Action<string> print = name => Console.WriteLine(name);
+            names.ForEach(print);
+
+        }
+    }
+}
