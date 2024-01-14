b6cdc44f07dc38a250e953ff4d6da102a7b81e5b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-14 11:51:35 +0200

    Create Car .cs

---
+++ CSharp-Advanced--OOP/Advanced/Defining Classes - Lab/04. Car Engine And Tires/Car .cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public Car() : this("VW", "Golf", 2025)
+        {
+        }
+
+        public Car(string make, string model, int year)
+            : this(make, model, year, 200, 10)
+        {
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * this.FuelConsumption;
+            if (consumption <= this.FuelQuantity)
+                this.FuelQuantity -= consumption;
+            else
+                Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+
+        public string WhoAmI()
+        {
+            string carInfo =
+                $"Make: {this.Make}\r\n" +
+                $"Model: {this.Model}\r\n" +
+                $"Year: {this.Year}\r\n" +
+                $"Fuel: {this.FuelQuantity:F2}L";
+            return carInfo;
+        }
+    }
+}
