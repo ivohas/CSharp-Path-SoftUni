1d61848af9ef0850c2369897179499d902da42c6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-22 17:14:33 +0200

    Create HomeController.cs

---
+++ C# Web/ASP.NET Fundamentals/TaskBoardApp-Workshop/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskBoardApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
