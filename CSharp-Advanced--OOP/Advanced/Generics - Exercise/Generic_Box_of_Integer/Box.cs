a66e3a947c46633b9ad9fcaed51e6c90ea1070fa
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-24 18:49:19 +0200

    Create Box.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Exercise/Generic_Box_of_Integer/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public T Element { get; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{typeof(T)}: {Element}");
+            return sb.ToString();
+        }
+    }
+}
