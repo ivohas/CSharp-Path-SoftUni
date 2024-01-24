1ff6a24b5a0ac4d396e27a7c8b9137ebd904c4dc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-24 18:49:11 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal> addVAT =
+                x => x * 1.20m;
+
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(", ");
+            decimal[] nums = tokens.Select(decimal.Parse).ToArray();
+            decimal[] numsWithVat = nums.Select(addVAT).ToArray();
+
+            Array.ForEach(numsWithVat, num => Console.WriteLine($"{num:f2}"));
+
+        }
+    }
+}
