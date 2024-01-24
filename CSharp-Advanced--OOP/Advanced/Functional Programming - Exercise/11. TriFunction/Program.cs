33500217931b6c1e163a6524ea807da70350fac3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-24 18:48:37 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Console.WriteLine(names
+                .First(n => 
+                n.Select(symbol => (int) symbol).Sum() >= number));
+        }
+    }
+}
