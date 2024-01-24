d92a068f898d8ce22a868320aa9e7ce2cb6074dd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-24 18:49:06 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static Func<string, bool> isFirstLetterCapital = x => x.Length > 0 && char.IsUpper(x[0]);
+        static void Main() =>
+            Console.WriteLine(
+                string.Join(
+                    Environment.NewLine,
+                    Console.ReadLine()
+                        .Split(" ")
+                        .Where(isFirstLetterCapital)
+            ));
+    }
+}
