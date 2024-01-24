36f4b36962d9815e521ec9e1d4291ca45dee12bd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-24 18:48:31 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/10. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _10.The_Party_Reservation_Filter_Module
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var guests = new List<string>(Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            var filters = new List<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Print")
+            {
+
+                var commands = input.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (commands[0] == "Add filter")
+                {
+                    filters.Add(commands[1] + " " + commands[2]);
+                }
+                else if (commands[0] == "Remove filter")
+                {
+                    filters.Remove(commands[1] + " " + commands[2]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var filter in filters)
+            {
+                var commands = filter.Split(' ');
+
+                if (commands[0] == "Starts")
+                {
+                    guests = guests.Where(p => !p.StartsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Ends")
+                {
+                    guests = guests.Where(p => !p.EndsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Length")
+                {
+                    guests = guests.Where(p => p.Length != int.Parse(commands[1])).ToList();
+                }
+                else if (commands[0] == "Contains")
+                {
+                    guests = guests.Where(p => !p.Contains(commands[1])).ToList();
+                }
+            }
+
+            if (guests.Any())
+            {
+                Console.WriteLine(string.Join(" ", guests));
+            }
+        }
+    }
+}
