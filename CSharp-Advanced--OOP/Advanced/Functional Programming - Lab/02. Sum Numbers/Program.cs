74c99e4e398a1ad5901430f465d55a2e255073d8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-24 18:49:02 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(nums.Count());
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
