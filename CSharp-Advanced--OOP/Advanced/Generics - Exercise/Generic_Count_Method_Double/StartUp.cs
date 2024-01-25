911036a7751e135258ec96485fca4dd64a5d9a07
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-25 17:58:48 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Exercise/Generic_Count_Method_Double/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swap
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var list = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<double>(list);
+            var elToCommpare = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.CountOfGreaterElements(list, elToCommpare));
+        }
+    }
+}
