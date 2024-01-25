14f3970723d237c3c7ff3fdc287e4419c052de61
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-25 17:59:21 +0200

    Create Tuple.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Exercise/Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<TFirst, TSecond>
+    {
+        public Tuple(TFirst firstElement, TSecond secondElement)
+        {
+            FirstElement = firstElement;
+            SecondElement = secondElement;
+        }
+
+        public TFirst FirstElement { get; private set; }
+        public TSecond SecondElement { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstElement} -> {SecondElement}";
+        }
+    }
+}
