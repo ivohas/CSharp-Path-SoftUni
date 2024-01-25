d95890bba2727619708f3b823e27b5cc6977def5
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-25 17:58:59 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Exercise/Generic_Swap_Method_Integer/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swap
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<int>(list);
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
