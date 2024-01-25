ace6460faf39b3b0500a5a1bc8f0d9c3d3e662b3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-25 17:58:41 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Exercise/Generic_Box_of_String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
