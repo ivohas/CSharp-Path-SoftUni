bb1746573541038e15cc4789adb276324f6733bd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-27 10:52:06 +0200

    Create EqualityScale.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Lab/Generic_Scale/EqualityScale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Scale
+{
+    public class EqualityScale<T>
+    {
+        public static bool AreEqual(T left, T right)
+        {
+            return left.Equals(right);
+        }
+    }
+}
