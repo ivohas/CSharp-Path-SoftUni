9fb356ece2880941b3b2056fbe299479c56f2e44
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-27 10:52:57 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/CustomComparator/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, int> sortFunction = (x, y) =>
+            (x % 2 == 0 && y % 2 != 0) ? -1 :
+            (x % 2 != 0 && y % 2 == 0) ? 1 : x > y ? 1 : x < y ? -1 : 0;
+
+            Array.Sort(numbers, (x, y) => sortFunction(x, y));
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
