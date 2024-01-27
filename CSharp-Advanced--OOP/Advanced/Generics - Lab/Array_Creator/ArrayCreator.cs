2788781cf8e2f6ebea2d726b2435b667c0162ccb
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-27 10:51:06 +0200

    Create ArrayCreator.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Lab/Array_Creator/ArrayCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            var array = new T[length];
+            for (int i = 0; i < length; i++)
+            { array[i] = item; }
+            return array;
+        }
+    }
+}
