1fa9f571f672cec4fb5dcfc0f235dc65c5f035c0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-27 10:52:16 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Lab/Generic_Scale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Generic_Scale
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            bool equals = EqualityScale<int>.AreEqual(1, 2);
+
+            Console.WriteLine(equals);
+        }
+    }
+}
