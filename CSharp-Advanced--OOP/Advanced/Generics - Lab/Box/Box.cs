1638db41b2c0b12d0eac4d111ec637b18cca10a1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-27 10:51:20 +0200

    Create Box.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Lab/Box/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private readonly List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public int Count => this.data.Count;
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Remove()
+        {
+            var lastElement = this.data.Last();
+            this.data.RemoveAt(this.data.Count - 1);
+            return lastElement;
+        }
+    }
+}
