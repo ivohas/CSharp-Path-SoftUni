1fb39a06f69b6d98d3e6e62869a646917098bbae
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:45 +0200

    Create Book.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Lab/IComparableBook/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} {this.Year}";
+        }
+
+        public int CompareTo([AllowNull] Book other)
+        {
+            if (other == null)
+                return 1;
+            int result = this.Year.CompareTo(other.Year);
+            if (result == 0)
+                result = this.Title.CompareTo(other.Title);
+
+            return result;
+        }
+    }
+}
