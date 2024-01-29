b5c86e7e65587f87e3f14d7f7873b578bff79ddf
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:28 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/Iterators and Comparators - Exercise/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            ListIyterator<string> listy = null;
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] tokens = command.Split();
+                string cmd = tokens[0];
+
+                if (cmd == "Create")
+                {
+                    listy = new ListIyterator<string>(tokens.Skip(1).ToArray());
+                }
+                else if (cmd == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (cmd == "Print")
+                {
+                    listy.Print();
+                }
+                else if (cmd == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
