53f03ed6d327617c04f87d05b6eb62b9fa82acaa
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:24 +0200

    Create ListIyterator.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/Iterators and Comparators - Exercise/ListIyterator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListIyterator<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+
+        public ListIyterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }
+
+        public bool HasNext() => currIndex < collection.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove =  HasNext();
+            if (canMove)
+                currIndex++;
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+                throw new ArgumentException("Invalid Operation!");
+
+            Console.WriteLine(collection[currIndex]);
+        }
+    }
+}
