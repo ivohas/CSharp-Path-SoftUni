147dc49b0a25749165d49b3dd9a8ca79ced439e9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:39 +0200

    Create Comparer.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Lab/BookComparer/Comparer.cs
@@ -0,0 +1,19 @@
+﻿using IteratorsAndComparators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookComparer
+{
+    public class Comparer : Comparer<Book>
+    {
+        public override int Compare(Book x, Book y)
+        {
+            int result = x.Title.CompareTo(y.Title);
+            if (result == 0)
+                result = y.Year.CompareTo(x.Year);
+            return result;
+
+        }
+    }
+}
