71d1e82a1609719c0515f40ea613f489c98dcd66
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:34 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/Stack/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var stack = new Stack<string>();
+            while (true)
+            {
+                var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string cmd = tokens[0];
+
+                if (cmd == "END") break;
+                else if (cmd == "Push")
+                {
+                    stack.Push(tokens.Skip(1).Select(x => x.Split(",").First()).ToArray());
+                }
+                else if (cmd == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+            }
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
