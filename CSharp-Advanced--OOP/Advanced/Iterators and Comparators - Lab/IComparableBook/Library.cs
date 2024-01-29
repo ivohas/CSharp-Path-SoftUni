acad6dee259d638565e5854487fd7ea4f005fc7a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:49 +0200

    Create Library.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Lab/IComparableBook/Library.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> Books;
+
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            this.Books.Sort();
+            for (int i = 0; i < this.Books.Count; i++)
+            {
+                yield return this.Books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        class LibraryIterator : IEnumerator<Book>
+        {
+
+            public List<Book> Books { get; set; }
+            private int position = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Books= books;
+                Reset();
+            }
+            public Book Current => this.Books[position];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                this.position++;
+                return position <= Books.Count;
+            }
+
+            public void Reset()
+            {
+                this.position = -1;
+            }
+        }
+    }
+}
