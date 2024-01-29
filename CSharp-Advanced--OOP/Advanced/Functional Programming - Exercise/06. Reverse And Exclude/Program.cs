da28930ac5266da17d2dbdfd74452cab4ae420a7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:02:50 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int number = int.Parse(Console.ReadLine());
+
+            Predicate<int> isDivisbleBy = num => num %number != 0;
+
+            Action<List<int>> print = nums => Console.WriteLine(String.Join(" ", nums.Where(n => isDivisbleBy(n)).Reverse()));
+            
+            print(numbers);
+        }
+    }
+}
