415ab6b00948aea7b412332fac1da754aa505ed0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-29 17:03:31 +0200

    Create Stack.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/Stack/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+
+        public Stack(params T[] data)
+        {
+            collection = new List<T>(data);
+        }
+
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                collection.Insert(collection.Count, element);
+            }
+        }
+
+        public T Pop()
+        {
+            if (collection.Count == 0)
+                throw new ArgumentException("No elements");
+
+            T element = collection[collection.Count - 1];
+            collection.RemoveAt(collection.Count - 1);
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
