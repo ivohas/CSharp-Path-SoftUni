598542e17135fc102af0a0876b948e48414f97a0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-30 18:04:16 +0200

    Create Book.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Lab/Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+    }
+}
