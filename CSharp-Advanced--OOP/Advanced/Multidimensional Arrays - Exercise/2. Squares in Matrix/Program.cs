4e3ae38cf4c09dc80a6b8793ac01e8ccc0e2e0ab
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-30 18:05:53 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int a = dimensions[0];
+            int b = dimensions[1];
+
+            string[,] matrix = new string[a, b];
+            for (int row = 0; row < a; row++)
+            {
+                string[] els = Console.ReadLine().Split(' ');
+                for (int col = 0; col < b; col++)
+                    matrix[row, col] = els[col];
+            }
+
+            int squaresCount = 0;
+            for (int row = 0; row < a - 1; row++)
+            {
+                for (int col = 0; col < b - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        squaresCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squaresCount);
+        }
+    }
+}
