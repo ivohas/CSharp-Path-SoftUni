9ef44fe0f6e768c77f085ca096baf706180ed7f3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-31 18:53:37 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            for (int r = 0; r < n; r++)
+            {
+                int[] colElements = Console.ReadLine()
+                        .Split(' ').Select(int.Parse).ToArray();
+                for (int c = 0; c < n; c++)
+                {
+                    matrix[r, c] = colElements[c];
+                }
+            }
+
+            int sum = 0;
+            for (int r = 0; r < n; r++)
+            {
+                sum += matrix[r, r];
+            }
+            Console.WriteLine(sum); 
+        }
+    }
+}
