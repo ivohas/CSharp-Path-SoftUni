7cdf68bee8e9f5e44a5b7a1ddc3a3c37b7dad08c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-31 18:53:25 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Read the matrix deminesions
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            (int rowsCount, int colsCount) = (dimensions[0], dimensions[1]);
+
+            // read the numbers of the matrix
+            int[,] matrix = new int[rowsCount, colsCount];
+            for (int row = 0; row < rowsCount; row++)
+            {
+                int[] colElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < colsCount; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            long sum = 0;
+            for (int row = 0; row < rowsCount; row++)
+            {
+                for (int col = 0; col < colsCount; col++)
+                {
+                    sum+= matrix[row,col];
+                }
+            }
+
+            Console.WriteLine(rowsCount);
+            Console.WriteLine(colsCount);
+            Console.WriteLine(sum);
+        }
+    }
+}
