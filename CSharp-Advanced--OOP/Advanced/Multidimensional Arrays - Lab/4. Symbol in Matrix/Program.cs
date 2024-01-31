92f254b0317649a89bbfd003a9c2201258284256
Ivaylo Hasov <ivailohasov@gmail.com>
2024-01-31 18:53:41 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                char[] els = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = els[col];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+            bool found = false; 
+            for (int r = 0; r < n; r++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (matrix[r, c] == symbol)
+                    {
+                        Console.WriteLine($"({r}, {c})");
+                        found = true;
+                        goto HERE;
+                    }
+                }
+            }
+            HERE:
+            if (!found)
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
