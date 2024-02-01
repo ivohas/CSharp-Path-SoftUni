b73a87c19e13f443721ccd2f88ea9ecece09cdf6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:15:55 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var chemCompounds = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemCompound = Console.ReadLine().Split();
+                foreach (string cc in chemCompound)
+                {
+                    chemCompounds.Add(cc);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemCompounds.OrderBy(x => x)));
+        }
+    }
+}
