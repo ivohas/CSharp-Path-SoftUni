77b3ad777e6bc568cacce7d584997f218bd70c38
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:17:11 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(string.Join(Environment.NewLine, names.Where(x => x.Length <= n)));
+        }
+    }
+}
