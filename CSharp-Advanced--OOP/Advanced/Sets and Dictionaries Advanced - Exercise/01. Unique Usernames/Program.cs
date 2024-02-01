fe1338c278a0ffe7797338c280a973c869aef28e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:15:30 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string username = Console.ReadLine();
+                usernames.Add(username);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}
