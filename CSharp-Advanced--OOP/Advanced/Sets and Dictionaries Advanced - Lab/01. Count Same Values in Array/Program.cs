2b444fe7fb646bd21539a0e88d16e1a051d35168
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:17:29 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            foreach (double num in nums)
+            {
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers[num] = 0;
+                }
+                numbers[num]++;
+            }
+
+            foreach (var kvp in numbers)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
