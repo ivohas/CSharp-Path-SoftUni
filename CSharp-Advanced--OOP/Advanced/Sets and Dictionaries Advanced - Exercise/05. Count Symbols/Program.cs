46820c237c1fca065f8d927c1543bc93e649b3da
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:16:02 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = new Dictionary<char, int>();
+            string text = Console.ReadLine();
+
+            foreach (char ch in text)
+            {
+                if (!letters.ContainsKey(ch))
+                {
+                    letters[ch] = 0;
+                }
+                letters[ch]++;
+            }
+
+            foreach (var letter in letters.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{letter.Key}: {letter.Value} time/s");
+            }
+        }
+    }
+}
