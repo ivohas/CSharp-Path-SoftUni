b8126613ea36d4dfbb55f94fef259cfd5ccdbc01
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:15:58 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers[num] = 0;
+                }
+                else numbers[num]++;
+            }
+
+            foreach (var num in numbers)
+            {
+                if (num.Value % 2 != 0)
+                {
+                    Console.WriteLine(num.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
