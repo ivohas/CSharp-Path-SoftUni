b71774cf3109da29c01167af2b2c7e60eb60edf7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-01 16:15:27 +0200

    Create Lake.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] lake;
+        private Func<char, int, StringSplitOptions, string[]> split;
+        private string v;
+
+        public Lake(params int[] rocks)
+        {
+            lake = rocks;
+        }
+
+        public Lake(Func<char, int, StringSplitOptions, string[]> split, string v)
+        {
+            this.split = split;
+            this.v = v;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < lake.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return lake[i];
+                }
+            }
+
+            for (int i = lake.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return lake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
