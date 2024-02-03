513645cf7cb4891e3eff50535c642d32d282a63f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-03 15:20:28 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parkingLot = new HashSet<string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(", ");
+
+                if (command[0] == "END")
+                    break;  
+                if (command[0] == "IN")
+                    parkingLot.Add(command[1]);
+                else if (command[0] == "OUT")
+                    parkingLot.Remove(command[1]);
+
+            }
+
+            if (parkingLot.Any())
+            {
+                foreach (string car in parkingLot)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else Console.WriteLine("Parking Lot is Empty");
+
+        }
+    }
+}
