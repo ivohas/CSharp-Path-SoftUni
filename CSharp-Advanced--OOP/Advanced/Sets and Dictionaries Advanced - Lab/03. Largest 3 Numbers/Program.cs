9313bfa295b6c70c98b27be96e36fd579b1402e0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-03 15:18:26 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
