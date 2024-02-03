f3c01e4f461e27824852a98270de7bcd0ee40587
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-03 15:19:02 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
