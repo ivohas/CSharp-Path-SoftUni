c0608e81278956f423e5899d014c133e102855b0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-04 11:06:08 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
