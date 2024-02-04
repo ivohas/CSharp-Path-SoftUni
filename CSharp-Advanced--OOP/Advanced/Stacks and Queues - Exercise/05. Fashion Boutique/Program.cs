23c0a976382a8f253ffbf42e95f58c93db97b01f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-04 10:48:14 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rackCap = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(clothes);
+
+            int sum = 0;
+            int neededRacks = 1;
+            while (stack.Count > 0)
+            {
+                if (sum + stack.Peek() < rackCap)
+                {
+                    sum += stack.Pop();
+                }
+                else if (sum + stack.Peek() == rackCap)
+                {
+                    sum += stack.Pop();
+                    sum = 0;
+                    if (stack.Count > 0)
+                    {
+                        neededRacks++;
+                    }
+                }
+                else if (sum + stack.Peek() > rackCap)
+                {
+                    sum = 0;
+                    sum += stack.Pop();
+                    neededRacks++;
+                }
+            }
+            Console.WriteLine(neededRacks);
+        }
+    }
+}
