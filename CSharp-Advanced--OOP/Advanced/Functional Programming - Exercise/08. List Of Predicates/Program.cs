5734eccfcc7eac1f3f5c1bb43508b20f7e7d1c30
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-04 10:49:20 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            bool canBeDivided = false;
+
+            if (n == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (i % numbers[j] == 0)
+                    {
+                        canBeDivided = true;
+                    }
+                    else canBeDivided = false;
+                }
+                if(canBeDivided)
+                        Console.Write(i + " ");
+            }
+        }
+    }
+}
