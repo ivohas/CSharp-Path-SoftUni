8189e8eca283ff7f6ec2808ed49c7ee7c1e44b53
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-04 10:48:27 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songsQueue = new Queue<string>(Console.ReadLine().Split(", "));
+
+            while (songsQueue.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string song = command.Substring(4);
+                    if (songsQueue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songsQueue.Enqueue(song);
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
