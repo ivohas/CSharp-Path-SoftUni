e58395a74fe580c2ad89b356e7a299d38ba3cf95
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:16:38 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            int queueSum = queue.Sum();
+            int sum = 0;
+            while (sum != queueSum)
+            {
+                sum += queue.Peek();
+                if (queue.Peek() % 2 == 1)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
