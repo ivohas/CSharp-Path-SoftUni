ee93d428165a957fb24974f17b06533a2637c656
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:17:13 +0200

    Create CopyDirectory.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Exercise/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,28 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath =  @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            Directory.CreateDirectory(outputPath);
+            var files = Directory.GetFiles(inputPath);
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(file, copyDestination);
+            }
+        }
+    }
+}
