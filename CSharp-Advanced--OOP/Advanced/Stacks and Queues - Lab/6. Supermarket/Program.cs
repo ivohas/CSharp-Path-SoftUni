fd3841ef7137163ec91723551e7bae8e1eff9eb2
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:16:42 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> q = new Queue<string>();
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+
+                if (name == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, q));
+                    q.Clear();
+                }
+                else if (name == "End")
+                {
+                    Console.WriteLine($"{q.Count} people remaining.");
+                    break;
+                }
+                else q.Enqueue(name);
+            }
+        }
+    }
+}
