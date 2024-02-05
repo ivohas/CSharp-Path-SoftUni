31474a72bc845fe3c0d787c5c78f100092bd5035
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:17:21 +0200

    Create DirectoryTraversal.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Exercise/DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,59 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            string[] files = Directory.GetFiles(inputFolderPath);
+            Dictionary<string, List<FileInfo>> extensionInfo = new Dictionary<string, List<FileInfo>>();
+            StringBuilder sb = new StringBuilder();
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string extension = fileInfo.Extension;
+
+                if(!extensionInfo.ContainsKey(extension))
+                    extensionInfo.Add(extension, new List<FileInfo>());
+                extensionInfo[extension].Add(fileInfo);
+            }
+
+            foreach (var entry in extensionInfo.OrderByDescending(entry => entry.Value.Count).ThenBy(entry => entry.Key))
+            {
+                string extension = entry.Key;
+                sb.AppendLine(extension);
+                List<FileInfo> filesInfo = entry.Value;
+                filesInfo.OrderByDescending(file => file.Length);
+
+                foreach (FileInfo fileInfo in filesInfo)
+                {
+                    sb.AppendLine($"--{fileInfo.Name} - {fileInfo.Length / 1024}kb");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string pathReport = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+            File.WriteAllText(pathReport, textContent);
+        }
+    }
+}
