49bb2cbcccb1227c8f08052b712a19bc7d43a5b4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:16:34 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char ch = expression[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subExpression = expression.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+        }
+    }
+}
