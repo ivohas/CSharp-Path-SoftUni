388031a12bfe8009234dc53338c933c13a2ac58e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:17:05 +0200

    Create CopyBinaryFile.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,37 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesCounter = reader.Read(buffer, 0, buffer.Length);
+                        if (bytesCounter == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, bytesCounter);
+                    }
+                }
+            }
+        }
+    }
+}
