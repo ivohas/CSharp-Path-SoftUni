4cf465c01803e2e31aa3f5ddf1da397ef9d80fee
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-05 10:16:25 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(elements);
+
+            string command = Console.ReadLine().ToLower();
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (cmdArgs[0] == "add")
+                {
+                    // Push two integers, given as parameters
+                    int n1 = int.Parse(cmdArgs[1]);
+                    int n2 = int.Parse(cmdArgs[2]);
+                    stack.Push(n1);
+                    stack.Push(n2);
+                }
+                else if (cmdArgs[0] == "remove")
+                {
+                    // Pop "count" of integers
+                    int count = int.Parse(cmdArgs[1]);
+                    if (stack.Count >= count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+
+            int sum = stack.ToArray().Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
