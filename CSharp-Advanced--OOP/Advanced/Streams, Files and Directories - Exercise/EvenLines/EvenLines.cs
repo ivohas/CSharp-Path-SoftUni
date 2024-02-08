258507e08ef5e98575f094432af5934628e5dabb
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-08 17:54:00 +0200

    Create EvenLines.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,54 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int i = 0;
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if (i % 2 == 0)
+                    {
+                        line = Replace(line);
+                        line = Reverse(line);
+                        Console.WriteLine(line);
+                    }
+
+                    line = reader.ReadLine();
+                    i++;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static string Reverse(string line)
+        {
+            return string.Join(", ", line.Split().Reverse());
+        }
+
+        private static string Replace(string line)
+        {
+            return line.Replace("-", "@")
+                .Replace(",", "@")
+                .Replace(".", "@")
+                .Replace("!", "@")
+                .Replace("?", "@");
+        }
+    }
+}
