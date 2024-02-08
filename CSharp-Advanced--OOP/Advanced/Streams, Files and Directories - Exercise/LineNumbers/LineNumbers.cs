5932d041bf63f3035eebda7a2f479a4c8c6f6ffa
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-08 17:54:04 +0200

    Create LineNumbers.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int countLetter = lines[i].Count(char.IsLetter);
+                int countSymbol = lines[i].Count(char.IsPunctuation);
+                Console.Write($"Line {i + 1}: ");
+                Console.WriteLine($"{lines[i]} ({countLetter})({countSymbol})");
+            }
+        }
+    }
+}
