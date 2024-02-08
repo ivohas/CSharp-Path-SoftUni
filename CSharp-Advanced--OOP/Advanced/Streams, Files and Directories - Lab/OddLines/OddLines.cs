1e6aabc0950241370dbc926f7b74c96f44c005d7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-08 17:54:43 +0200

    Create OddLines.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Lab/OddLines/OddLines.cs
@@ -0,0 +1,36 @@
+﻿
+namespace OddLines
+{
+    using System;
+    using System.IO;
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputPath, outputPath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int lineNumber = 0;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (lineNumber % 2 == 1)
+                            writer.WriteLine(line);
+                        lineNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
