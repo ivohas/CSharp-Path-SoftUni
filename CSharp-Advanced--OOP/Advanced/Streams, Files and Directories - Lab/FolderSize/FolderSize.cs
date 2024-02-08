eb25c861afa30e00d11bbd628cc708c17eca16bc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-08 17:54:22 +0200

    Create FolderSize.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,25 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            decimal folderSize = 0;
+            var dirInfo = new DirectoryInfo(folderPath);
+            foreach (var f in dirInfo.EnumerateFiles("*.*", SearchOption.AllDirectories))
+            {
+                fols
+            }
+        }
+    }
+}
