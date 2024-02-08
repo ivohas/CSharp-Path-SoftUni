f059a64d46510b298e4d5cf8ecc8470704ea4b96
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-08 17:54:30 +0200

    Create LineNumbers.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader inputStreamReader = new StreamReader(inputFilePath);
+            using (inputStreamReader)
+            {
+                StreamWriter outputStreamWriter = new StreamWriter(outputFilePath);
+                using (outputStreamWriter)
+                {
+                    string line;
+                    int lineCount = 0;
+                    while ((line = inputStreamReader.ReadLine()) != null)
+                    {
+                        outputStreamWriter.WriteLine($"{lineCount}. {line}");
+                        lineCount++;
+                    }
+                }
+            }
+        }
+    }
+}
