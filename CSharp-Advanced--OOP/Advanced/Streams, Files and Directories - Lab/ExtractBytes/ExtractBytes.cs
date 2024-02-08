fb41394904f34157bcc6e61d41f3308e495d9b91
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-08 17:54:16 +0200

    Create ExtractBytes.cs

---
+++ CSharp-Advanced--OOP/Advanced/Streams, Files and Directories - Lab/ExtractBytes/ExtractBytes.cs
@@ -0,0 +1,20 @@
+﻿namespace ExtractBytes
+{
+    using System;
+    using System.IO;
+    public class ExtractBytes
+    {
+        static void Main(string[] args)
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+        }
+    }
+}
