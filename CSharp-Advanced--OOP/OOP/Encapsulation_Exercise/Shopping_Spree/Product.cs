7258d89cc308ac0de3ebc80bffb2339ff0b21b2d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-10 21:06:57 +0200

    Create Product.cs

---
+++ CSharp-Advanced--OOP/OOP/Encapsulation_Exercise/Shopping_Spree/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace T03.ShoppingSpree
+{
+    public class Product
+    {
+        private string ime;
+        private decimal cena;
+
+        public Product(string ime, decimal cena)
+        {
+            this.Ime = ime;
+            this.Cena = cena;
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return this.ime;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.ime = value;
+            }
+        }
+
+        public decimal Cena
+        {
+            get
+            {
+                return this.cena;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.cena = value;
+            }
+        }
+    }
+}
+
