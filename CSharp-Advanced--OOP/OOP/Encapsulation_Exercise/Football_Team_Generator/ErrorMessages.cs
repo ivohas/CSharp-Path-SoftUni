b40a14664de9c40aeb6a5c1bd6f00fa171710e51
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-10 21:06:25 +0200

    Create ErrorMessages.cs

---
+++ CSharp-Advanced--OOP/OOP/Encapsulation_Exercise/Football_Team_Generator/ErrorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Team_Generator
+{
+    public static class ErrorMessages
+    {
+        public const string NullOrWhiteSpaceName = "A name should not be empty.";
+        public const string StatInvalidValue = "{0} should be between 0 and 100.";
+        public const string PlyaerNotInTeam = "Player {0} is not in {1} team.";
+        public const string TeamNotExisting = "Team {0} does not exist.";
+    }
+}
