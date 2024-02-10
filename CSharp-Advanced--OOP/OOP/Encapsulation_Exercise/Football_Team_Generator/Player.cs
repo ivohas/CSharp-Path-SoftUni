41e859db5e37671d31edbe984db07854b8567b2c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-10 21:06:45 +0200

    Create Player.cs

---
+++ CSharp-Advanced--OOP/OOP/Encapsulation_Exercise/Football_Team_Generator/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Team_Generator
+{
+    public class Player
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ErrorMessages.NullOrWhiteSpaceName);
+                }
+                this.name = value;
+            }
+        }
+
+        public Player(string name, Stats stats)
+        {
+            this.Name = name;
+            this.Stats = stats;
+        }
+        public Stats Stats { get; private set; }
+    }
+}
