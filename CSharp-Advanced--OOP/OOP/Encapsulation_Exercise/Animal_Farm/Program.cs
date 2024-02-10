f7b1e11853854169b954569d70830da4c70a5ec3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-10 21:05:59 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/OOP/Encapsulation_Exercise/Animal_Farm/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    $"Chicken {chicken.Ime} (age {chicken.Godini}) can produce {chicken.productionPerDay} eggs per day.");
+            }
+            catch (Exception msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+        }
+    }
+}
