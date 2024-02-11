d900437e76e2104565d6dcc940d5b28a2d853de1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-11 11:02:21 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Encapsulation_Lab/Validation_of_Data/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                string firstName = cmdArgs[0];
+                string lastName = cmdArgs[1];
+                int age = int.Parse(cmdArgs[2]);
+                decimal salary = decimal.Parse(cmdArgs[3]);
+
+                var person = new Person(firstName, lastName, age, salary);
+
+                persons.Add(person);
+            }
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+    }
+}
