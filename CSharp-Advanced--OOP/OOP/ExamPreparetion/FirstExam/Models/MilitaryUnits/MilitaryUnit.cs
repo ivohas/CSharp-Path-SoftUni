69dbab7551b5ae0667a7b22b63a7a26f0ff899d9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-12 18:14:24 +0200

    Create MilitaryUnit.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,38 @@
+﻿namespace PlanetWars.Models.MilitaryUnits
+{
+    using System;
+    using Contracts;
+
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        private int enduranceLevel = 1;
+
+        public MilitaryUnit(double cost)
+        {
+            this.Cost = cost;
+        }
+
+        public double Cost { get; private set; }
+
+        public int EnduranceLevel
+        {
+            get => this.enduranceLevel;
+            private set
+            { 
+                this.enduranceLevel = value;
+            }
+        }
+
+        public void IncreaseEndurance()
+        {
+            this.EnduranceLevel++;
+
+            if (this.EnduranceLevel > 20)
+            {
+                this.EnduranceLevel = 20;
+                throw new ArgumentException("Endurance level cannot exceed 20 power points.");
+            }
+        }
+    }
+}
+
