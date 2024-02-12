f26de65ed4aa5c808403436aaca5d3d10d542f79
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-12 18:14:39 +0200

    Create SpaceMissles.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/Models/Weapons/SpaceMissles.cs
@@ -0,0 +1,12 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    public class SpaceMissles : Weapon
+    {
+        private const double Price = 8.75;
+
+        public SpaceMissles(int destructionLevel) : base(Price, destructionLevel)
+        {
+        }
+    }
+}
+
