24a8e531d17aaadb7618641f73fc6f10da4648c4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-12 18:14:34 +0200

    Create BioChemicalWeapon.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/Models/Weapons/BioChemicalWeapon.cs
@@ -0,0 +1,12 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    public class BioChemicalWeapon : Weapon
+    {
+        private const double Price = 3.2;
+
+        public BioChemicalWeapon(int destructionLevel) : base(Price, destructionLevel)
+        {
+        }
+    }
+}
+
