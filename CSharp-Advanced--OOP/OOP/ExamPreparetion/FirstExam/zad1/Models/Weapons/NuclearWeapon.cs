f1924575bb80bfc1d5a9205c612d56900599dfc1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-13 17:56:50 +0200

    Create NuclearWeapon.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/zad1/Models/Weapons/NuclearWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public class NuclearWeapon : Weapon
+    {
+        const double price = 15;
+        public NuclearWeapon(int destructionLevel) 
+            : base(destructionLevel, price)
+        {
+        }
+    }
+}
