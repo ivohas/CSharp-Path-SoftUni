8c396646af9cb7932d6d26a48d9e7d4b391898a9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-13 17:56:31 +0200

    Create AnonymousImpactUnit.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/zad1/Models/MilitaryUnits/AnonymousImpactUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public class AnonymousImpactUnit : MilitaryUnit
+    {
+        const double cost = 30;
+        public AnonymousImpactUnit() : base(cost)
+        {
+        }
+    }
+}
