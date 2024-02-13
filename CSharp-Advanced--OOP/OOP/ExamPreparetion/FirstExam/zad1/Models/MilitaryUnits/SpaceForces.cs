085277b0023da31629ca3d6b34349558319d9a8a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-13 17:56:37 +0200

    Create SpaceForces.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/zad1/Models/MilitaryUnits/SpaceForces.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public class SpaceForces : MilitaryUnit
+    {
+        const double cost = 11;
+        public SpaceForces() 
+            : base(cost)
+        {
+        }
+    }
+}
