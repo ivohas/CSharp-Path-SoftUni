b4217cea06d1800e0c879049cb965b3aff44af1a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-13 17:56:40 +0200

    Create StormTroopers.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/zad1/Models/MilitaryUnits/StormTroopers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public class StormTroopers : MilitaryUnit
+    {
+        const double cost = 2.5;
+        public StormTroopers() 
+            : base(cost)
+        {
+        }
+    }
+}
