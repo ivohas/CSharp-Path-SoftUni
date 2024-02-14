2eebc24f57ed2260f0f021cda27d01b3c745fc69
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-14 16:09:58 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Iterators and Comparators - Exercise/Equality_Logic/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                Person person = new Person(tokens[0], int.Parse(tokens[1]));
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
