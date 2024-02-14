c6c66de0d655f226c7d492f1e68a80fb5712ead9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-14 16:10:07 +0200

    Create UnitRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/FirstExam/zad1/Repositories/UnitRepository.cs
@@ -0,0 +1,36 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace PlanetWars.Repositories
+{
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        private ICollection<IMilitaryUnit> models;
+        public UnitRepository()
+        {
+            models = new HashSet<IMilitaryUnit>();
+        }
+        public IReadOnlyCollection<IMilitaryUnit> Models => (IReadOnlyCollection<IMilitaryUnit>)this.models;
+
+        public void AddItem(IMilitaryUnit model)
+        {
+            models.Add(model);
+        }
+
+        public IMilitaryUnit FindByName(string name)
+        {
+            IMilitaryUnit unit = models.FirstOrDefault(x => x.GetType().Name == name);
+            return unit;
+        }
+
+        public bool RemoveItem(string name)
+        {
+            IMilitaryUnit unit=models.FirstOrDefault(x => x.GetType().Name == name);
+            bool removed = models.Remove(unit);
+            return removed;
+        }
+    }
+}
