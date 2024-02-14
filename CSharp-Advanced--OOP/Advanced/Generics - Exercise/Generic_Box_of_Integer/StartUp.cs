77b1bfed11a0079cb01605cf86a24819f96fdd46
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-14 16:09:54 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/Advanced/Generics - Exercise/Generic_Box_of_Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
