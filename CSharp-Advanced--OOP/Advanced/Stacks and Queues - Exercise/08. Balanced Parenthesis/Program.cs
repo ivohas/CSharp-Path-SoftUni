571ddc23e48fa85bcae67ff76576685d87d5619f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-27 18:50:29 +0200

    Create Program.cs

---
+++ CSharp-Advanced--OOP/Advanced/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> parenthesStack = new Stack<char>();
+            foreach (var symbol in input)
+            {
+                if (parenthesStack.Any())
+                {
+                    char check = parenthesStack.Peek();
+                    if (check == '{' && symbol == '}')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                    else if (check == '[' && symbol == ']')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                    else if (check == '(' && symbol == ')')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                }
+                parenthesStack.Push(symbol);
+            }
+            Console.WriteLine(!parenthesStack.Any() ? "YES" : "NO");
+        }
+    }
+}
