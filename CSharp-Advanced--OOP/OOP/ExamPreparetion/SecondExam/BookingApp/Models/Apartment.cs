c8e435f2e78c4fe9ce1df7abea87ab596f830994
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-27 18:51:10 +0200

    Create Apartment.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/SecondExam/BookingApp/Models/Apartment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models
+{
+    public class Apartment : Room
+    {
+        const int bedCapacity = 6;
+        public Apartment() : base(bedCapacity)
+        {
+        }
+    }
+}
