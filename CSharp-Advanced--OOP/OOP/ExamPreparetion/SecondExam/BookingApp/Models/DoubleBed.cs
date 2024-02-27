0aba04eab10c9ac1aba4c96257b2451bc93be6d6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-27 18:51:17 +0200

    Create DoubleBed.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/SecondExam/BookingApp/Models/DoubleBed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models
+{
+    public class DoubleBed : Room
+    {
+        const int bedCoount = 2;
+        public DoubleBed() : base(bedCoount)
+        {
+        }
+    }
+}
