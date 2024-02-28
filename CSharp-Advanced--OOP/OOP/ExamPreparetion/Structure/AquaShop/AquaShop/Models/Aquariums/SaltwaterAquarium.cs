a175f81b1d5a8d01dae4199ecbcbc48060a4ff4f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-02-28 18:05:25 +0200

    Create SaltwaterAquarium.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/AquaShop/AquaShop/Models/Aquariums/SaltwaterAquarium.cs
@@ -0,0 +1,15 @@
+﻿namespace AquaShop.Models.Aquariums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class SaltwaterAquarium:Aquarium
+    {
+        const int capacity = 25;
+        public SaltwaterAquarium(string name):base(name,capacity)
+        {
+               
+        }
+    }
+}
