1f031204e1654911e0743dfe2cc42f557fb8fe92
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-02 10:29:22 +0200

    Create Ornament.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/AquaShop/AquaShop/Models/Decorations/Ornament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    public class Ornament : Decoration
+    {
+        const int comfort = 1;
+        const int price = 10;
+        
+        public Ornament() 
+            : base(comfort, price)
+        {
+        }
+    }
+}
