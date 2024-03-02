1d6ae097c181a25d4de0d94b6df023d23d9ecc8d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-02 10:29:30 +0200

    Create FreshwaterFish.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/AquaShop/AquaShop/Models/Fish/FreshwaterFish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public class FreshwaterFish : Fish
+    {
+        const int FishSize = 3;
+
+        public FreshwaterFish(string name, string species, decimal price) : base(name, species, price)
+        {
+            this.Size = FishSize;
+        }
+
+        public override void Eat()
+        {
+           this.Size +=3;
+        }
+    }
+}
