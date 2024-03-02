9bea6b24d9a5e6c7aebbd6a32423b2219696c4dc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-02 10:29:36 +0200

    Create DecorationRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/AquaShop/AquaShop/Repositories/DecorationRepository.cs
@@ -0,0 +1,35 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AquaShop.Repositories
+{
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private ICollection<IDecoration> models;
+        public DecorationRepository()
+        {
+            this.models = new HashSet<IDecoration>();
+        }
+        public IReadOnlyCollection<IDecoration> Models => (IReadOnlyCollection<IDecoration>)this.models;
+
+        public void Add(IDecoration model)
+        {
+            models.Add(model);
+        }
+
+        public IDecoration FindByType(string type)
+        {
+            return models.FirstOrDefault(x=>x.GetType().Name==type);
+        }
+
+        public bool Remove(IDecoration model)
+        {
+          bool removed = models.Remove(model);
+            return removed;
+        }
+    }
+}
