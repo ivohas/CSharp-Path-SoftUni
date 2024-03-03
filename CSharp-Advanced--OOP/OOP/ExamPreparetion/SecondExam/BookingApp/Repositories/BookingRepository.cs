c934577db35bf2e6118c034b773175f43b92aed5
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-03 10:54:35 +0200

    Create BookingRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/SecondExam/BookingApp/Repositories/BookingRepository.cs
@@ -0,0 +1,27 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private ICollection<IBooking> bookings;
+        public void AddNew(IBooking model)
+        {
+            bookings.Add(model);
+        }
+
+        public IReadOnlyCollection<IBooking> All() => (IReadOnlyCollection<IBooking>)this.bookings;
+       
+
+        public IBooking Select(string criteria)
+        {
+            var book = bookings.FirstOrDefault(x => x.BookingNumber == int.Parse(criteria));
+            return book;
+        }
+    }
+}
