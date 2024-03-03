bf4517e156e7203613eedbd7151508749a738e0d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-03 10:54:23 +0200

    Create Room.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/SecondExam/BookingApp/Models/Room.cs
@@ -0,0 +1,40 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models
+{
+    public class Room : IRoom
+    {
+        private int bedcapacity;
+        private double pricePerNight = 0;
+
+        protected Room(int bedCapacity)
+        {
+            BedCapacity = bedCapacity;
+        }
+
+        public int BedCapacity
+        { 
+        get=>this.bedcapacity;
+            private set => this.bedcapacity = value;
+        }
+
+        public double PricePerNight { 
+        get=>this.pricePerNight;
+           private set
+            {
+                if (value < 0) {
+                    throw new ArgumentException("Price cannot be negative!");
+                }
+                this.pricePerNight = value;
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+           PricePerNight= price;
+        }
+    }
+}
