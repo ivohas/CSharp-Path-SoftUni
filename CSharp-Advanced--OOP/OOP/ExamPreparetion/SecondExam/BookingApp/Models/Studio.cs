509a5e200afaf750f58c019800eaf06555e7b2d4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-03 10:54:28 +0200

    Create Studio.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/SecondExam/BookingApp/Models/Studio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models
+{
+    public class Studio : Room
+    {
+        const int bedCapacity = 4;
+        public Studio() : base(bedCapacity)
+        {
+        }
+    }
+}
