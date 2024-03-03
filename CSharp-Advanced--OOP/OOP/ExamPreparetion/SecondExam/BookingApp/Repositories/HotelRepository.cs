10a60679ad25bd2372b948d58ebd929c3f723538
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-03 10:54:41 +0200

    Create HotelRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/SecondExam/BookingApp/Repositories/HotelRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    public class HotelRepository : IRepository<IHotel>
+    {
+        private ICollection<IHotel> hotels;
+        public HotelRepository()
+        {
+          hotels=new List<IHotel>();
+        }
+        public void AddNew(IHotel model)
+        {
+           hotels.Add(model);
+        }
+
+        public IReadOnlyCollection<IHotel> All() => (IReadOnlyCollection<IHotel>)this.hotels;
+        
+
+        public IHotel Select(string criteria)
+        {
+            var rhotel = hotels.FirstOrDefault(x => x.FullName == criteria);
+            return rhotel;
+        }
+    }
+}
