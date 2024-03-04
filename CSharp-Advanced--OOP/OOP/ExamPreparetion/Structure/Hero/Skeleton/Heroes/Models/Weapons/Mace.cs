6361d909fc6796e2e3a94ccfe8b107e0ce6cf364
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-04 11:56:48 +0200

    Create Mace.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/Hero/Skeleton/Heroes/Models/Weapons/Mace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        const int DAMAGE = 25;
+        public   Mace(string name, int durability) 
+            : base(name, durability, DAMAGE)
+        {
+        }
+    }
+}
