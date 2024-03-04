11d752a1b367f30f1862c91fd89ad33b78a7ed8e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-04 11:56:50 +0200

    Create Weapon.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/Hero/Skeleton/Heroes/Models/Weapons/Weapon.cs
@@ -0,0 +1,69 @@
+﻿using Heroes.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Heroes.Models.Weapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        private string name;
+        private  int durability;
+        private int damage;
+
+        protected Weapon(string name, int durability, int damage)
+        {
+            Name = name;
+            Durability = durability;
+            this.damage = damage;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Weapon type cannot be null or empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public int Durability
+        {
+            get => this.durability;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Durability cannot be below 0.");
+                }
+                this.durability = value;
+            }
+        }
+        public int DoDamage()
+        {
+            if (Durability>0)
+            {
+                this.Durability--;
+            if (this.Durability == 0)
+            {
+                return 0;
+            }
+            return damage ;
+            }
+            else
+            {
+                return 0;
+            }
+            
+          
+        }
+    }
+
+
+}
+
