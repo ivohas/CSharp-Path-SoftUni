3ff1ba9b98958052fddfd3cf15abf3abcd319910
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-04 11:56:46 +0200

    Create Claymore.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/Hero/Skeleton/Heroes/Models/Weapons/Claymore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        const int DAMAGE = 20;
+        public Claymore(string name, int durability )
+            : base(name, durability, DAMAGE)
+        {
+        }
+    }
+}
