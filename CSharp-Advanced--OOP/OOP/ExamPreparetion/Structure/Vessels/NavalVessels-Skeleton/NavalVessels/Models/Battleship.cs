fba2543aff39527521ba06f70831034537e6d2c2
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-05 19:53:40 +0200

    Create Battleship.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/Vessels/NavalVessels-Skeleton/NavalVessels/Models/Battleship.cs
@@ -0,0 +1,58 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Battleship : Vessel
+    {
+        private bool sonarMode = false;
+       const int armourThcness = 300;
+        protected Battleship(string name, double mainWeaponCaliber, double speed) :
+           base(name,armourThcness,mainWeaponCaliber,speed )
+        {
+           
+        }
+        public bool SonarMode { get=>this.sonarMode; set=> this.sonarMode = value ; }
+       
+
+
+
+        public void ToggleSonarMode()
+        {
+            if (SonarMode==false)
+            {
+                SonarMode = true;
+               
+            }
+            else
+            {
+                SonarMode = false;
+               
+            }
+        }
+
+        public override void RepairVessel()
+        {
+            if (ArmorThickness < 300)
+            {
+               
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"- {Name}")
+                .AppendLine($"*Type: {GetType()}")
+                .AppendLine($"*Armor thickness: {ArmorThickness}")
+                .AppendLine("$*Main weapon caliber: { MainWeaponCaliber}")
+                .AppendLine($"*Speed: {Speed}")
+                .AppendLine($"*Targets: {(this.Targets == null ? "None" : String.Join(", ", Targets))}")
+                .Append($"*Sonar mode: {(this.SonarMode==true?"ON":"OFF")}");
+            return base.ToString();
+        }
+    }
+}
