8123bd6bf0c51ada7a25e844b671d396e4ba3939
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-20 18:08:32 +0200

    Create Pilot.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-09-April-2022/Business-Logic/Models/Pilot.cs
@@ -0,0 +1,71 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Pilot : IPilot
+    {
+        private string fullName;
+        private IFormulaOneCar car;
+        private int numberOfWins;
+        private bool canRace;
+
+        public Pilot(string fullName)
+        {
+            this.FullName = fullName;
+        }
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPilot, value));
+                }
+                fullName = value;
+            }
+        }
+
+        public IFormulaOneCar  Car
+        {
+            get { return car; }
+            private set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(ExceptionMessages.InvalidCarForPilot);
+                }
+                car = value;
+            }
+        }
+
+        public int NumberOfWins
+        {
+            get { return numberOfWins; }
+            private set { numberOfWins = value; }
+        }
+
+        public bool CanRace
+        {
+            get { return canRace; }
+            private set { canRace = value; }
+        }
+
+        public void AddCar(IFormulaOneCar car)
+        {
+            this.Car = car;
+            this.CanRace = true;
+        }
+
+        public void WinRace() => this.NumberOfWins++;
+
+        public override string ToString()
+        {
+            return $"Pilot {this.FullName} has {this.NumberOfWins} wins.";
+        }
+    }
+}
