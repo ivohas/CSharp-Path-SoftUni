db75a7dd7a2dba974fd42b975a8733c9590e8b5e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-20 18:07:30 +0200

    Create RobotsTests.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/UnitTests/Robots/Robots.Tests/RobotsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Robots.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class RobotsTests
+    {
+        [Test]
+        public void InvalidCapacityBelowZero() {
+            Robot robot = new Robot("sttcs", 323);
+            Assert.Throws<ArgumentException>(() =>
+            {
+              
+
+            }, "Invalid capacity!");
+        }
+    }
+}
