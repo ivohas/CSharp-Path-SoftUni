0147642cdd8927b9d44bda5950dfcca51fedd342
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-20 18:08:29 +0200

    Create FormulaOneCar.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-09-April-2022/Business-Logic/Models/FormulaOneCar.cs
@@ -0,0 +1,64 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+        private int horsepower;
+        private double engineDisplacement;
+
+        protected FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            this.Model = model;
+            this.Horsepower = horsepower;
+            this.EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+                model = value;
+            }
+        }
+
+        public int Horsepower
+        {
+            get { return horsepower; }
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
+                horsepower = value; 
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get { return engineDisplacement; }
+            private set
+            {
+                if (value < 1.60 || value > 2.00)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+                engineDisplacement = value;
+            }
+        }
+
+
+        public double RaceScoreCalculator(int laps) => this.EngineDisplacement / this.Horsepower * laps;
+    }
+}
