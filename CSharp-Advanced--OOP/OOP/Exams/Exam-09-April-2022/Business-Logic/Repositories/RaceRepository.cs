16fd0b3d195ddcc6026144ce82ddad0753168fac
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-23 14:03:38 +0200

    Create RaceRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-09-April-2022/Business-Logic/Repositories/RaceRepository.cs
@@ -0,0 +1,27 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly List<IRace> models;
+
+        public RaceRepository()
+        {
+            this.models = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => this.models;
+
+        public void Add(IRace model) => this.models.Add(model);
+
+        public IRace FindByName(string name) => this.Models.FirstOrDefault(m => m.RaceName == name);
+
+        public bool Remove(IRace model) => this.models.Remove(model);
+    }
+}
