8949551bb18f3da4ed0b43c9c807296ee6a3fa34
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-25 12:21:34 +0200

    Create RaceRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/ExamPreparetion/Structure/Formula1/Formula1/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,36 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly ICollection<IRace> races;
+        public RaceRepository()
+        {
+            races = new HashSet<IRace>();
+        }
+        public IReadOnlyCollection<IRace> Models => (IReadOnlyCollection<IRace>)this.races;
+
+        public void Add(IRace model)
+        {
+          races.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            IRace race = races.FirstOrDefault(x => x.RaceName == name);
+            return race;
+        }
+
+        public bool Remove(IRace model)
+        {
+            bool result= races.Remove(model);
+            return result;
+        }
+    }
+}
