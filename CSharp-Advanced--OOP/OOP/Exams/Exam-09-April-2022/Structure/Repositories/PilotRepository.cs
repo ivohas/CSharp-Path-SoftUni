740f810373c607fa4e10fd489a9b1eb606d7fa9a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-25 12:22:13 +0200

    Create PilotRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-09-April-2022/Structure/Repositories/PilotRepository.cs
@@ -0,0 +1,27 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private readonly List<IPilot> models;
+
+        public PilotRepository()
+        {
+            this.models = new List<IPilot>();
+        }
+
+        public IReadOnlyCollection<IPilot> Models => this.models;
+
+        public void Add(IPilot model) => this.models.Add(model);
+
+        public IPilot FindByName(string name) => this.Models.FirstOrDefault(m => m.FullName == name);
+
+        public bool Remove(IPilot model) => this.models.Remove(model);
+    }
+}
