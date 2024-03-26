f5e6b453a62802543fbec7b88f3fead186f170cb
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-26 20:25:21 +0200

    Create FreshwaterAquarium.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-10-April-2021/Business-Logic/Models/Aquariums/FreshwaterAquarium.cs
@@ -0,0 +1,11 @@
+﻿namespace AquaShop.Models.Aquariums
+{
+    public class FreshwaterAquarium : Aquarium
+    {
+        private const int CAPACITY = 50;
+        public FreshwaterAquarium(string name)
+            : base(name, CAPACITY)
+        {
+        }
+    }
+}
