960258a78b0b2ef948858c5a3d8ad01297df16f9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-30 22:04:30 +0200

    Create Decoration.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-10-April-2021/Structure/Models/Decorations/Decoration.cs
@@ -0,0 +1,30 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+
+namespace AquaShop.Models.Decorations
+{
+    public abstract class Decoration : IDecoration
+    {
+        protected Decoration(int comfort, decimal price)
+        {
+            this.Comfort = comfort;
+            this.Price = price;
+        }
+        private int comfort;
+
+        public int Comfort
+        {
+            get { return comfort; }
+            private set { comfort = value; }
+        }
+
+        private decimal price;
+
+        public decimal Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+
+    }
+}
