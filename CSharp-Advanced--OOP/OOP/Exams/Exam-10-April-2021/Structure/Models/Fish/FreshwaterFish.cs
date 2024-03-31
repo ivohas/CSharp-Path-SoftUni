a95fce34a37205bd6639c87cca1366d9d22c0620
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-31 15:47:20 +0300

    Create FreshwaterFish.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-10-April-2021/Structure/Models/Fish/FreshwaterFish.cs
@@ -0,0 +1,14 @@
+﻿namespace AquaShop.Models.Fish
+{
+    public class FreshwaterFish : Fish
+    {
+        private const int INITIAL_SIZE = 3;
+        private const int INCREASE_SIZE = 3;
+        public FreshwaterFish(string name, string species, decimal price)
+            : base(name, species, price)
+        {
+            this.Size = INITIAL_SIZE;
+        }
+        public override void Eat() => this.Size += INCREASE_SIZE;
+    }
+}
