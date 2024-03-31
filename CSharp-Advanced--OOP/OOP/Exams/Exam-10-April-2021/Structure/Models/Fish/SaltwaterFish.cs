a98700556770aaf4d3ee3995001f1075f7ce0dfd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-31 15:47:23 +0300

    Create SaltwaterFish.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-10-April-2021/Structure/Models/Fish/SaltwaterFish.cs
@@ -0,0 +1,14 @@
+﻿namespace AquaShop.Models.Fish
+{
+    public class SaltwaterFish : Fish
+    {
+        private const int INITIAL_SIZE = 3;
+        private const int INCREASE_SIZE = 2;
+        public SaltwaterFish(string name, string species, decimal price)
+            : base(name, species, price)
+        {
+            this.Size = INITIAL_SIZE;
+        }
+        public override void Eat() => this.Size += INCREASE_SIZE;
+    }
+}
