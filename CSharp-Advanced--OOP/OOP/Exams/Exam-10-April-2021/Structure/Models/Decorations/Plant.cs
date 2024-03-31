022413ac38fcec81d1dc6ac5e4a727cf39f6d263
Ivaylo Hasov <ivailohasov@gmail.com>
2024-03-31 15:47:16 +0300

    Create Plant.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-10-April-2021/Structure/Models/Decorations/Plant.cs
@@ -0,0 +1,13 @@
+﻿namespace AquaShop.Models.Decorations
+{
+    public class Plant : Decoration
+    {
+        private const int COMFORT = 5;
+        private const decimal PRICE = 10m;
+        public Plant()
+            : base(COMFORT, PRICE)
+        {
+
+        }
+    }
+}
