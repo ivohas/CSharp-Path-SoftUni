e48c36f3c757e2f44fc92e2f2827c07bbf928247
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-02 22:51:55 +0300

    Create Weightlifter.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Business-Logic/Models/Athletes/Weightlifter.cs
@@ -0,0 +1,19 @@
+﻿using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public class Weightlifter : Athlete
+    {
+        private const int INITIAL_STAMINA = 50;
+        private const int WEIGHTLIFTER_STAMINA_PLUS = 10;
+        public Weightlifter(string fullName, string motivation, int numberOfMedals)
+            : base(fullName, motivation, numberOfMedals, INITIAL_STAMINA)
+        {
+        }
+
+        public override void Exercise() => this.Stamina += WEIGHTLIFTER_STAMINA_PLUS;
+    }
+}
