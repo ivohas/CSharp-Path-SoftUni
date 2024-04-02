10f68abf685684e2b309552ae73dea7f25793673
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-02 22:51:35 +0300

    Create FormulaOneCarRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-09-April-2022/Structure/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,27 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private readonly List<IFormulaOneCar> models;
+
+        public FormulaOneCarRepository()
+        {
+            this.models = new List<IFormulaOneCar>();
+        }
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => this.models;
+
+        public void Add(IFormulaOneCar model) => this.models.Add(model);
+
+        public IFormulaOneCar FindByName(string name) => this.Models.FirstOrDefault(m => m.Model == name);
+
+        public bool Remove(IFormulaOneCar model) => this.models.Remove(model);
+    }
+}
