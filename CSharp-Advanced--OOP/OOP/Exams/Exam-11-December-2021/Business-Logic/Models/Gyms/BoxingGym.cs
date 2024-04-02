8358aba921ef6074db0e891fae15380b61f7619d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-02 22:52:10 +0300

    Create BoxingGym.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Business-Logic/Models/Gyms/BoxingGym.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public class BoxingGym : Gym
+    {
+        private const int INITIAL_CAPACITY = 15;
+        public BoxingGym(string name)
+            : base(name, INITIAL_CAPACITY)
+        {
+        }
+    }
+}
