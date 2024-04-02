d11f50747ab68cda38f3caeb9a6ee4868c76352e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-02 22:52:08 +0300

    Create Kettlebell.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Business-Logic/Models/Equipment/Kettlebell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Equipment
+{
+    public class Kettlebell : Equipment
+    {
+        private const double WEIGHT = 10000;
+        private const decimal PRICE = 80;
+        public Kettlebell()
+            : base(WEIGHT, PRICE)
+        {
+        }
+    }
+}
