b0de8e33e023c6d2933a1b8ecc19ac2d7e58589f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-04 12:32:16 +0300

    Create BoxingGloves.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Structure/Models/Equipment/BoxingGloves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Equipment
+{
+    public class BoxingGloves : Equipment
+    {
+        private const double WEIGHT = 227;
+        private const decimal PRICE = 120;
+        public BoxingGloves()
+            : base(WEIGHT, PRICE)
+        {
+        }
+    }
+}
