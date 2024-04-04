591ada260104507f11884b6c1733156537d63adc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-04 12:32:29 +0300

    Create Equipment.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Structure/Models/Equipment/Equipment.cs
@@ -0,0 +1,32 @@
+﻿using Gym.Models.Equipment.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Equipment
+{
+    public class Equipment : IEquipment
+    {
+        protected Equipment(double weight, decimal price)
+        {
+            this.Weight = weight;
+            this.Price = price;
+        }
+        private double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+            private set { weight = value; }
+        }
+
+        private decimal price;
+
+        public decimal Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+    }
+}
