7691c47add35a7f46c43a6cebab567f8d7cedc89
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-04 12:23:18 +0300

    Create Athlete.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Structure/Models/Athletes/Athlete.cs
@@ -0,0 +1,81 @@
+﻿using Gym.Models.Athletes.Contracts;
+using Gym.Utilities.Messages;
+using System;
+
+namespace Gym.Models.Athletes
+{
+    public abstract class Athlete : IAthlete
+    {
+        private const int MAX_STAMINA = 100;
+        private const int MIN_MEDALS = 0;
+        protected Athlete(string fullName, string motivation, int numberOfMedals, int stamina)
+        {
+            this.FullName = fullName;
+            this.Motivation = motivation;
+            this.NumberOfMedals = numberOfMedals;
+            this.Stamina = stamina;
+        }
+        private string fullName;
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteName);
+                }
+                fullName = value;
+            }
+        }
+
+        private string motivation;
+
+        public string Motivation
+        {
+            get { return motivation; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMotivation);
+                }
+                motivation = value;
+            }
+        }
+
+        private int stamina;
+
+        public int Stamina
+        {
+            get { return stamina; }
+            protected set
+            {
+                if (value > MAX_STAMINA)
+                {
+                    this.stamina = MAX_STAMINA;
+                    throw new ArgumentException(ExceptionMessages.InvalidStamina);
+                }
+                stamina = value;
+            }
+        }
+
+        private int numberOfMedals;
+
+        public int NumberOfMedals
+        {
+            get { return numberOfMedals; }
+            private set
+            {
+                if (value < MIN_MEDALS)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMedals);
+                }
+                numberOfMedals = value;
+            }
+        }
+
+        public abstract void Exercise();
+    }
+}
