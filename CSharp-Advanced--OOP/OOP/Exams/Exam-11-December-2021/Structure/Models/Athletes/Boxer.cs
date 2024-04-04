aeec9bb73416f34c0dec4f39b65869fe9b88513d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-04 12:31:06 +0300

    Create Boxer.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Structure/Models/Athletes/Boxer.cs
@@ -0,0 +1,19 @@
+﻿using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public class Boxer : Athlete
+    {
+        private const int INITIAL_STAMINA = 60;
+        private const int BOXER_STAMINA_PLUS = 15;
+        public Boxer(string fullName, string motivation, int numberOfMedals)
+            : base(fullName, motivation, numberOfMedals, INITIAL_STAMINA)
+        {
+        }
+
+        public override void Exercise() => this.Stamina += BOXER_STAMINA_PLUS;
+    }
+}
