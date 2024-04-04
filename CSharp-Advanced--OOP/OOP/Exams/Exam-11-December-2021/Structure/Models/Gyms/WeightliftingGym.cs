bedbd2e2db50f4ce0cac47f0294b84c418e77cd5
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-04 12:37:11 +0300

    Create WeightliftingGym.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-11-December-2021/Structure/Models/Gyms/WeightliftingGym.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public class WeightliftingGym : Gym
+    {
+        private const int INITIAL_CAPACITY = 20;
+        public WeightliftingGym(string name) : base(name, INITIAL_CAPACITY)
+        {
+        }
+    }
+}
