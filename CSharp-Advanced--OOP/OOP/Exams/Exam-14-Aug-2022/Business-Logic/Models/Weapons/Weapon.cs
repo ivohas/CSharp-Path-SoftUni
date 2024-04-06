18bc8ec18162a3af54d6d049f37e8603da9aabd7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-06 16:34:22 +0300

    Create Weapon.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-14-Aug-2022/Business-Logic/Models/Weapons/Weapon.cs
@@ -0,0 +1,44 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        protected Weapon(int destructionLevel, double price)
+        {
+            this.DestructionLevel = destructionLevel;
+            this.Price = price;
+        }
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        private int destructionLevel;
+
+        public int DestructionLevel
+        {
+            get { return destructionLevel; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooLowDestructionLevel);
+                }
+                else if (value > 10)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooHighDestructionLevel);
+                }
+                destructionLevel = value;
+            }
+        }
+
+    }
+}
