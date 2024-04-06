afe758db64a520264c2ef936b97854f66cc9d92a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-06 16:34:16 +0300

    Create SpaceMissiles.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-14-Aug-2022/Business-Logic/Models/Weapons/SpaceMissiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public class SpaceMissiles : Weapon
+    {
+        private const double PRICE = 8.75;
+        public SpaceMissiles(int destructionLevel)
+            : base(destructionLevel, PRICE)
+        {
+        }
+    }
+}
