c329389118ba7731733d2307b3e7f250330fae4f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-11 16:58:06 +0300

    Create WeaponRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-14-Aug-2022/Structure/Repositories/WeaponRepository.cs
@@ -0,0 +1,25 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private readonly HashSet<IWeapon> models;
+        public WeaponRepository()
+        {
+            this.models = new HashSet<IWeapon>();
+        }
+        public IReadOnlyCollection<IWeapon> Models => this.models;
+
+        public void AddItem(IWeapon model) => this.models.Add(model);
+
+        public IWeapon FindByName(string name) => this.models.FirstOrDefault(w => w.GetType().Name == name);
+
+        public bool RemoveItem(string name) => this.models.Remove(this.models.FirstOrDefault(w => w.GetType().Name == name));
+    }
+}
