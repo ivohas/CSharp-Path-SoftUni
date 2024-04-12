b461e5f5352afa83725b7de7c54948ae52c5f9d6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-12 11:48:01 +0300

    Create Claymore.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-18-April-2022/Business-Logic/Models/Weapons/Claymore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        private const int DAMAGE = 20;
+        public Claymore(string name, int durability)
+            : base(name, durability)
+        {
+        }
+        public override int DoDamage()
+        {
+            if (this.durability == 0)
+            {
+                return 0;
+            }
+
+            this.durability--;
+
+            return DAMAGE;
+        }
+    }
+}
