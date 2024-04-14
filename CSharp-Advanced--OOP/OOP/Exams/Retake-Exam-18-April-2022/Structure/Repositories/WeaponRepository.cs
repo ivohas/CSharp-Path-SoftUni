0cb947b7eac42e6f78380e916be0742c64a7268e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-14 16:01:14 +0300

    Create WeaponRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-18-April-2022/Structure/Repositories/WeaponRepository.cs
@@ -0,0 +1,35 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private readonly List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            this.weapons = new List<IWeapon>();
+        }
+        public IReadOnlyCollection<IWeapon> Models => this.weapons.AsReadOnly();
+
+        public void Add(IWeapon model) => this.weapons.Add(model);
+
+        public IWeapon FindByName(string name) => this.weapons.FirstOrDefault(h => h.Name == name);
+
+        public bool Remove(IWeapon model)
+        {
+            if (this.weapons.Any(h => h.Name == model.Name))
+            {
+                this.weapons.Remove(model);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
