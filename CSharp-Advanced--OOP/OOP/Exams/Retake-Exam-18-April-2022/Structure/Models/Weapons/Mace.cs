3f519860af1a67fc4e567848f7469b7f636da22c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-14 16:01:05 +0300

    Create Mace.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-18-April-2022/Structure/Models/Weapons/Mace.cs
@@ -0,0 +1,23 @@
+﻿namespace Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        private const int DAMAGE = 25;
+        public Mace(string name, int durability)
+            : base(name, durability)
+        {
+
+        }
+        public int DoDamage()
+        {
+            if (this.durability == 0)
+            {
+                return 0;
+            }
+
+            this.durability--;
+
+            return DAMAGE;
+        }
+    }
+}
