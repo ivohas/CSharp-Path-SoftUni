9aabd09e35011fa6aa9b3c743a925cb3454e6cc0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-14 16:01:12 +0300

    Create HeroRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-18-April-2022/Structure/Repositories/HeroRepository.cs
@@ -0,0 +1,35 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Heroes.Repositories
+{
+    public class HeroRepository : IRepository<IHero>
+    {
+        private readonly List<IHero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new List<IHero>();
+        }
+        public IReadOnlyCollection<IHero> Models => this.heroes.AsReadOnly();
+
+        public void Add(IHero model) => this.heroes.Add(model);
+
+        public IHero FindByName(string name) => this.heroes.FirstOrDefault(h => h.Name == name);
+
+        public bool Remove(IHero model)
+        {
+            if (this.heroes.Any(h => h.Name == model.Name))
+            {
+                this.heroes.Remove(model);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
