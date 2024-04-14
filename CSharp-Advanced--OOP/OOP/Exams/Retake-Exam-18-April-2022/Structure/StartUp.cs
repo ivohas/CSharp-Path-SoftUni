ef24b76f2406ed6609adcf34ff778f27addad16b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-14 16:01:16 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-18-April-2022/Structure/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Heroes.Core;
+using Heroes.Core.Contracts;
+
+namespace Heroes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+
+        }
+    }
+}
