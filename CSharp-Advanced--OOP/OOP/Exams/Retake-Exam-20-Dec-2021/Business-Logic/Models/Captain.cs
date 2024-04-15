2bf9d7ea66f04a8cd55790268bf52964af268498
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-15 16:41:33 +0300

    Create Captain.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-20-Dec-2021/Business-Logic/Models/Captain.cs
@@ -0,0 +1,69 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Captain : ICaptain
+    {
+        private string fullName;
+        private int combatExperience;
+        private List<IVessel> vessels;
+
+        public Captain(string fullName)
+        {
+            this.FullName = fullName;
+            this.vessels = new List<IVessel>();
+        }
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidCaptainName);
+                }
+                fullName = value;
+            }
+        }
+
+        public int CombatExperience
+        {
+            get { return combatExperience; }
+            private set { combatExperience = value; }
+        }
+
+        public ICollection<IVessel> Vessels => this.vessels;
+        public void AddVessel(IVessel vessel)
+        {
+            if (vessel == null)
+            {
+                throw new NullReferenceException(ExceptionMessages.InvalidVesselForCaptain);
+            }
+            this.vessels.Add(vessel);
+        }
+
+        public void IncreaseCombatExperience()
+        {
+            this.CombatExperience+= 10;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb
+                .AppendLine($"{this.FullName} has {this.CombatExperience} combat experience and commands {this.Vessels.Count} vessels.");
+                if (Vessels.Any(v => v.Captain == this))
+                {
+                    sb.AppendLine(Vessels.FirstOrDefault(v => v.Captain == this).ToString());
+                }
+            return sb.ToString().Trim();
+        }
+    }
+}
