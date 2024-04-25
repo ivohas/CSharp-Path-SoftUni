d651bc24fe93b74cbb4190c874ab55185cec743e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-25 20:53:40 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Exceptions and Error Handling - Lab/Square_Root/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Square_Root
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Sqrt(number);
+                Console.WriteLine(Sqrt(number));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.Error.WriteLine("Invalid number.");
+                throw ex;
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static double Sqrt(double value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("Invalid number.");
+            }
+            return Math.Sqrt(value);
+        }
+    }
+}
