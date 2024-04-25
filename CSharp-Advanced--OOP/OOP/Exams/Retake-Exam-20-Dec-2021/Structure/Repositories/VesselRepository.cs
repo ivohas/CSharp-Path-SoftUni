572726b25c70a98746a8cc14a3ebad882b1942d5
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-25 20:53:10 +0300

    Create VesselRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Retake-Exam-20-Dec-2021/Structure/Repositories/VesselRepository.cs
@@ -0,0 +1,26 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavalVessels.Repositories
+{
+    public class VesselRepository : IRepository<IVessel>
+    {
+        private readonly List<IVessel> models;
+
+        public VesselRepository()
+        {
+            this.models = new List<IVessel>();
+        }
+        public IReadOnlyCollection<IVessel> Models => this.models;
+
+        public void Add(IVessel model) => this.models.Add(model);
+
+        public IVessel FindByName(string name) => this.Models.FirstOrDefault(m => m.Name == name);
+
+        public bool Remove(IVessel model) => this.models.Remove(model);
+    }
+}
