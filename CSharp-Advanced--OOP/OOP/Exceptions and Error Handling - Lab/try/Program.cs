72963e1d4c90c58b12533ed6721cdf6232624ed4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-25 20:53:43 +0300

    Create Program.cs

---
+++ CSharp-Advanced--OOP/OOP/Exceptions and Error Handling - Lab/try/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace trye
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+            int sum = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split();
+                string kinti = cmdArgs[1];
+
+                string substromg = kinti.Substring(1, kinti.Length - 4);
+                sum += int.Parse(substromg);
+            }
+            Console.WriteLine(sum);
+    }
+}
+}
