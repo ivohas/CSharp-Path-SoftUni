083035ac5b3d7f3b73ea953bae595fe7faf55ae7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-26 22:34:56 +0300

    Create Person.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/Person/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
