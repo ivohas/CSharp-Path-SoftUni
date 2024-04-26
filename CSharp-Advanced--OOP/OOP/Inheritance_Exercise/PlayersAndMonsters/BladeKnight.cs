350de4ba61b2b53ca6ab0e00de1ed46eb8af2413
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-26 22:35:02 +0300

    Create BladeKnight.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/PlayersAndMonsters/BladeKnight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class BladeKnight : DarkKnight
+    {
+        public BladeKnight(string name, int level) : base(name, level)
+        {
+
+        }
+    }
+}
