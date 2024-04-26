34f525df111b835f7e736e7940652b9cee57c221
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-26 22:34:13 +0300

    Create Car.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/NeedForSpeed/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double DefaultCarFuelConsumption = 3;
+        public Car(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+        public override double FuelConsumption => DefaultCarFuelConsumption;
+    }
+}
