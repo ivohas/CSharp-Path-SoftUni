c304065672e6677b3cb8b0fb601c05da1f300866
Ivaylo Hasov <ivailohasov@gmail.com>
2024-04-26 22:34:54 +0300

    Create Child.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/Person/Child.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
