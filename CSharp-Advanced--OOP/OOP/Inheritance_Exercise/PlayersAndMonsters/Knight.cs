49d5b1ac90d992df00a29f1d8f7e43cf1fc7911c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-07 16:57:29 +0300

    Create Knight.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/PlayersAndMonsters/Knight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Knight : Hero
+    {
+        public Knight(string name, int level) : base(name, level)
+        {
+
+        }
+    }
+}
