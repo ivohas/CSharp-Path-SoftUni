beab26e887b6dbc541dc2c0482b1a4d9949f7d90
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-07 16:57:42 +0300

    Create Cake.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double CakeGrams = 250;
+        private const double CakeCalories = 1000;
+        private const decimal CakePrice = 5m;
+        public Cake(string name) : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+
+        }
+    }
+}
