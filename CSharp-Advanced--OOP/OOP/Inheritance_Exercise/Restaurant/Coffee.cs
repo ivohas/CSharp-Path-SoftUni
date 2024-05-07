f665593c59c07408d94fe68790b743f37772ed34
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-07 16:57:44 +0300

    Create Coffee.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double caffeine) : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+        public const double CoffeeMilliliters = 50;
+        public const decimal CoffeePrice = 3.50m;
+        public double Caffeine { get; set; }
+    }
+}
