e3fd26ecec7eef4f34038daee6687a8244e31a79
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-08 18:22:27 +0300

    Create Cat.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Lab/Hierarchical_Inheritance/Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public Cat()
+        {
+
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+}
