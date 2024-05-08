9231e246323834fad49222f0c17ae38efb43266a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-08 18:22:02 +0300

    Create Теа.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Exercise/Restaurant/Теа.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Теа : HotBeverage
+    {
+        public Теа(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+
+        }
+    }
+}
