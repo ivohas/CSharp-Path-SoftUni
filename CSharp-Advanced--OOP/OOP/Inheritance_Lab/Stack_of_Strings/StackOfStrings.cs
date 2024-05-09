675cdbc65171f4b1d9054d81270bb36a80d1ca34
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:39:44 +0300

    Create StackOfStrings.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Lab/Stack_of_Strings/StackOfStrings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public StackOfStrings(IEnumerable<string> collection)
+        {
+            this.AddRange(collection);
+        }
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange()
+        {
+            return new Stack<string>(this);
+        }
+        private void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var el in collection)
+            {
+                this.Push(el);
+            }
+        }
+    }
+}
