83f624e0aff9eff8e23e7202aed79b405d94fbdb
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:31:19 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Lab/Random_List/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rl = new RandomList();
+
+            rl.Add("kote");
+            rl.Add("kuche");
+
+            Console.WriteLine(rl.RandomString());
+        }
+    }
+}
