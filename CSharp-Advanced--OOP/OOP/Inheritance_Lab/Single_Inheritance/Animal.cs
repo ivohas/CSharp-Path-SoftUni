e89683311e852fa107fbaf6178054ac883d9ed35
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:31:20 +0300

    Create Animal.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Lab/Single_Inheritance/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
