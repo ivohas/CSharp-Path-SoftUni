28d7bfc21cc2f471ad32f0b87784e7e3f736b169
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:31:22 +0300

    Create Dog.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Lab/Single_Inheritance/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
