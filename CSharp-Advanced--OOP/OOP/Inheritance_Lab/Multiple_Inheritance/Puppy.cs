0297a9b5ddee05f05d0d99e3221676ce563e42b4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:31:08 +0300

    Create Puppy.cs

---
+++ CSharp-Advanced--OOP/OOP/Inheritance_Lab/Multiple_Inheritance/Puppy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Puppy : Dog
+    {
+        public Puppy()
+        {
+
+        }
+
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
