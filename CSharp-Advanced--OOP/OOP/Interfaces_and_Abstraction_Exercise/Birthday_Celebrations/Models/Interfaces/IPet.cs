af6aa5792e5b8bde4899e375f16cb901a4fea7e2
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:39:53 +0300

    Create IPet.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Birthday_Celebrations/Models/Interfaces/IPet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birthday_Celebrations.Models.Interfaces
+{
+    public interface IPet
+    {
+        public string Name { get; }
+        public string Birthdate { get; }
+    }
+}
