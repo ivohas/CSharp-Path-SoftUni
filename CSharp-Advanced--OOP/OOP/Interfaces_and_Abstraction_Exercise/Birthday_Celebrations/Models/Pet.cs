4108a5dbfea43a1ceed579aa1333936d013e8025
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:39:58 +0300

    Create Pet.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Birthday_Celebrations/Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birthday_Celebrations.Models
+{
+    using Models.Interfaces;
+    public class Pet : IPet
+    {
+        public string Name { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
