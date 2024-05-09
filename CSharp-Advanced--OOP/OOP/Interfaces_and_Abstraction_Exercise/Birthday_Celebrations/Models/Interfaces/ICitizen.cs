3c9359c6172b7f404fc3acfbc559f60da2f3bf5e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-09 17:39:51 +0300

    Create ICitizen.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Birthday_Celebrations/Models/Interfaces/ICitizen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Border_Control.Models.Interfaces
+{
+    public interface ICitizen
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public string ID { get; }
+        public string Birthdate { get; }
+    }
+}
