430495b8fee0cdd5d37eccbb60f99d2b9c757e65
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-10 11:13:42 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Border_Control/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Border_Control
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> IDs = new List<string>();
+            
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(' ', '-');
+                string id = cmdArgs[cmdArgs.Length - 1];
+
+                IDs.Add(id);
+            }
+
+            string fakeID = Console.ReadLine();
+            foreach (string id in IDs)
+            {
+                string idCheckSubstring = id.Substring(id.Length - fakeID.Length);
+                if (idCheckSubstring == fakeID)
+                {
+                    Console.WriteLine(id);
+                }
+            }
+        }
+    }
+}
