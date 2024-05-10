e8ed21afdfc4a257e60391e88b0bcc78c1a8a546
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-10 11:13:28 +0300

    Create IRobot.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Border_Control/Models/Interfaces/IRobot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Border_Control.Models.Interfaces
+{
+    public interface IRobot
+    {
+        public string Model { get; }
+        public string ID { get; }
+    }
+}
