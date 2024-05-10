f30abd0a17b224b7f6a128a0b8dfe8db4d04ab3f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-10 11:14:02 +0300

    Create Citizen.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Food_Shortage/Models/Citizen.cs
@@ -0,0 +1,24 @@
+﻿namespace Border_Control
+{
+    using Models.Interfaces;
+    public class Citizen : ICitizen, IBuyer
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.ID = id;
+            this.BirthDate = birthdate;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string ID { get; private set; }
+        public string BirthDate { get; private set; }
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 10;
+        }
+    }
+}
