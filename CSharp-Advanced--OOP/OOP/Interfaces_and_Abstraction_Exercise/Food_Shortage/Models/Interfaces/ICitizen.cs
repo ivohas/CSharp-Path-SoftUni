c8641272c1dc32864acbd37a09c1278024b4c11d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-10 11:14:10 +0300

    Create ICitizen.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Food_Shortage/Models/Interfaces/ICitizen.cs
@@ -0,0 +1,11 @@
+﻿namespace Border_Control.Models.Interfaces
+{
+    public interface ICitizen
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public string ID { get; }
+
+        public string BirthDate { get; }
+    }
+}
