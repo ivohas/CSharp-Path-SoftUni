80ed94073f58c5270eff57ccda524f10249a80e4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-10 11:13:19 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Birthday_Celebrations/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Border_Control
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> birthDates = new List<string>();
+            
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(' ', '-');
+                string birthdate = cmdArgs[cmdArgs.Length - 1];
+
+                birthDates.Add(birthdate);
+            }
+
+            birthDates.RemoveAll(x => String.IsNullOrEmpty(x));
+            string birthDateToFind = Console.ReadLine();
+            
+            foreach (string birthDate in birthDates)
+            {
+                string idCheckSubstring = birthDate.Substring(birthDate.Length - birthDateToFind.Length);
+                if (idCheckSubstring == birthDateToFind)
+                {
+                    Console.WriteLine(birthDate);
+                }
+            }
+        }
+    }
+}
