e7f96a92bbbf6a573f7bd29fd67eccddf977e1bf
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-10 11:13:22 +0300

    Create Citizen.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Border_Control/Models/Citizen.cs
@@ -0,0 +1,12 @@
+﻿namespace Border_Control
+{
+    using Models.Interfaces;
+    public class Citizen : ICitizen
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string ID { get; private set; }
+    }
+}
