d1371fe6017b6a5d451eb86ee5d25379737fcb6d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-15 17:43:23 +0300

    Create StationaryPhone.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Telephony/Models/StationaryPhone.cs
@@ -0,0 +1,24 @@
+﻿namespace Telephony.Models
+{
+    using Interfaces;
+    public class StationaryPhone : ICallable
+    {
+        public string Call(string number)
+        {
+            return $"Dialing... {number}";
+        }
+
+        //private bool ValidateNumber(string number)
+        //{
+        //    foreach (char digit in number)
+        //    {
+        //        if (!char.IsDigit(digit))
+        //        {
+        //            return false;
+        //        }
+        //    }
+
+        //    return true;
+        //}
+    }
+}
