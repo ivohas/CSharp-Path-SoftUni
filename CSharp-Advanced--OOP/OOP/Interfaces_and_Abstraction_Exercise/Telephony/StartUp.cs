b79332fdd32be8f75117b79f9759bf1c207e129a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-15 17:43:26 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Telephony/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Telephony
+{
+    using IO;
+    using IO.Interfaces;
+    using Core;
+    public class StartUp
+    {
+        static void Main()
+        {
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            IEngine engine = new Engine(reader, writer);
+            engine.Start();
+        }
+    }
+}
