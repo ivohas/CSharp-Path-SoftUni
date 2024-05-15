7648a177eb339a5fd8b5a7d4582703f46ceefb26
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-15 17:43:21 +0300

    Create SmartPhone.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Telephony/Models/SmartPhone.cs
@@ -0,0 +1,16 @@
+﻿namespace Telephony.Models
+{
+    using Interfaces;
+    public class SmartPhone : ICallable, IBrowsable
+    {
+        public string BrowseURL(string url)
+        {
+            return $"Browsing: {url}!";
+        }
+
+        public string Call(string number)
+        {
+            return $"Calling... {number}";
+        }
+    }
+}
