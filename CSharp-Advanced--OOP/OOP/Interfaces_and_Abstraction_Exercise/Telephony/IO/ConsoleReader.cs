bbdae0b2babc16dc0ca8a1d0a5d73b25885b5e0a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-15 17:43:01 +0300

    Create ConsoleReader.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Telephony/IO/ConsoleReader.cs
@@ -0,0 +1,13 @@
+﻿namespace Telephony.IO
+{
+    using Interfaces;
+    using System;
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            string text = Console.ReadLine();
+            return text;
+        }
+    }
+}
