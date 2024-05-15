6c90cc1d92835823f48d6b18aba529a7dccc3ec8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-15 17:43:35 +0300

    Create Car.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Lab/Cars/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Car : ICar
+    {
+        public Car(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Model { get; }
+        public string Color { get; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model}";
+        }
+    }
+}
