ad56f77a0495b1c589df0aa68ce7fadf0be14bfb
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-15 17:42:43 +0300

    Create Citizen.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Multiple_Implementation/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthDate;
+        }
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}
