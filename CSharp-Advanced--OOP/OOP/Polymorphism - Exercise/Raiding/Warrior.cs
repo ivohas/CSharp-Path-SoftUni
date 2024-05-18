8fa77aee6f298bd6151b8a467bfa7cdec41d65d3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:42:02 +0300

    Create Warrior.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Raiding/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Warrior : BaseHero
+    {
+        public Warrior(string name) : base(name)
+        {
+            this.Name = name;
+            this.Power = 100;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Warrior - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
