7511e2ab097193d7f3cac27f74ed30239f09c72f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:10 +0300

    Create ICar.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Lab/Cars/ICar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        string Start();
+        string Stop();
+    }
+}
