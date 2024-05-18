63c855fd41d39b08fc932d3842b180679a9fef03
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:31 +0300

    Create Rectangle.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Lab/Shapes/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+            for (int i = 0; i < this.Height; ++i)
+            {
+                DrawLine(this.Width, '*', ' ');
+            }
+            DrawLine(this.Width, '*', '*');
+        }
+
+        public void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 0; i < width; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
