389324f8364daa9850fba9882194e9e2380a097e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:45 +0300

    Create BaseHero.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Raiding/BaseHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class BaseHero
+    {
+        public BaseHero(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public virtual string CastAbility()
+        {
+            return $"BaseHero - {this.Name} healed for {this.Power} damage";
+        }
+    }
+}
