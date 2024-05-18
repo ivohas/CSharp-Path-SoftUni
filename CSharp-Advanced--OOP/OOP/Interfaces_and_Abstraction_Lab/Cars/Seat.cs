4ba7f3cbf849418b34eca6a28349bdccfee79dfd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:14 +0300

    Create Seat.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Lab/Cars/Seat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : Car
+    {
+        public Seat(string model, string color) : base(model, color)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(base.ToString());
+            result.AppendLine(this.Start());
+            result.AppendLine(this.Stop());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
