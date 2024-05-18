cbda03e293a2be175f85a88b39dddcd8f4a72968
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:57 +0300

    Create Rogue.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Raiding/Rogue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        public Rogue(string name) : base(name)
+        {
+            this.Name = name;
+            this.Power = 80;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Rogue - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
