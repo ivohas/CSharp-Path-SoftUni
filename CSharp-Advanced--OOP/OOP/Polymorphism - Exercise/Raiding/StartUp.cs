66af343fb0baf11c6284fe7c0b93afd321298d8c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:42:00 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Raiding/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<BaseHero> heroes = new List<BaseHero>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+                if (type == "Druid")
+                {
+                    heroes.Add(new Druid(name));
+                }
+                else if (type == "Paladin")
+                {
+                    heroes.Add(new Paladin(name));
+                }
+                else if (type == "Rogue")
+                {
+                    heroes.Add(new Rogue(name));
+                }
+                else if (type == "Warrior")
+                {
+                    heroes.Add(new Warrior(name));
+                }
+            }
+
+            int bossPower = int.Parse(Console.ReadLine());
+            int heroPower = 0;
+            foreach (BaseHero hero in heroes)
+            {
+                heroPower += hero.Power;
+                Console.WriteLine(hero.CastAbility());
+            }
+
+            if (heroPower > bossPower)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else Console.WriteLine("Defeat...");
+        }
+    }
+}
