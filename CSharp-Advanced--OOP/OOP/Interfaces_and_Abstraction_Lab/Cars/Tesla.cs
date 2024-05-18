9e51f1a0fd34747aa8d14c7804324351106c9f15
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:19 +0300

    Create Tesla.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Lab/Cars/Tesla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+            :base(model, color)
+        {
+            this.Battery = battery;
+        }
+
+        public int Battery { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"{base.ToString()} with {this.Battery} batteries");
+            result.AppendLine(this.Start());
+            result.AppendLine(this.Stop());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
