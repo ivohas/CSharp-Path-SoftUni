8aad73d4180973fbe3f58e10054a251158d8e2ac
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-18 13:41:51 +0300

    Create Paladin.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Raiding/Paladin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Paladin : BaseHero
+    {
+        public Paladin(string name) : base(name)
+        {
+            this.Name = name;
+            this.Power = 100;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Paladin - {this.Name} healed for {this.Power}";
+        }
+    }
+}
