6f3edf84ecc67d459c4c2384a3a52eac85bef68c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:51:28 +0300

    Create IAnimalFactory.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Factories/Interfaces/IAnimalFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace Wild
+{
+
+    public interface IAnimalFactory
+    {
+        Animal CreateAnimal(string type, string name, double weight, string param3, string param4 = null);
+    }
+}
