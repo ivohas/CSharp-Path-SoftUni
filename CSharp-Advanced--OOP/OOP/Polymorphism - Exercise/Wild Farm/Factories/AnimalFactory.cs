96fdddb40e77d5d425af44c6161d4a5b4a9b8852
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:51:23 +0300

    Create AnimalFactory.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Factories/AnimalFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Wild
+{
+
+    public class AnimalFactory : IAnimalFactory
+    {
+        public Animal CreateAnimal(string type, string name, double weight, string param3, string param4 = null)
+        {
+            Animal anim;
+            if (type == "Owl")
+            {
+                anim = new Owl(name, weight, double.Parse(param3));
+            }
+            else if (type == "Mouse")
+            {
+                anim = new Mouse(name, weight, param3);
+            }
+            else if (type == "Hen")
+            {
+                anim = new Hen(name, weight, double.Parse(param3));
+            }
+            else if (type == "Cat")
+            {
+                anim = new Cat(name, weight, param3, param4);
+            }
+            else if (type == "Dog")
+            {
+                anim = new Dog(name, weight, param3);
+            }
+            else if (type == "Tiger")
+            {
+                anim = new Tiger(name, weight, param3, param4);
+            }
+            else
+            {
+                throw new InvalidFactoryTypeException();
+            }
+
+            return anim;
+        }
+    }
+}
