8d5bf5f9940c1fc61c28d60206ba9878ea4febbd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:50:58 +0300

    Create Bus.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Vehicles_Extension/Bus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Bus : Vehicle
+    {
+        public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) :
+        base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public override void Drive(double distance)
+        {
+            double currentFuelConsumption = this.FuelConsumption + 1.4;
+            double neededFuel = distance * currentFuelConsumption;
+            if (FuelQuantity >= neededFuel)
+            {
+                Console.WriteLine($"Bus travelled {distance} km");
+                this.FuelQuantity -= neededFuel;
+            }
+            else
+            {
+                Console.WriteLine($"Bus needs refueling");
+            }
+        }
+    }
+}
