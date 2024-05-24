97e808ca6603ccc87cd04d55df77157e3a410aa6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:51:06 +0300

    Create Truck.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Vehicles_Extension/Truck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) :
+    base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption + 1.6;
+        }
+        public override void Drive(double distance)
+        {
+            double neededFuel = distance * this.FuelConsumption;
+            if (FuelQuantity >= neededFuel)
+            {
+                Console.WriteLine($"Truck travelled {distance} km");
+                this.FuelQuantity -= neededFuel;
+            }
+            else
+            {
+                Console.WriteLine($"Truck needs refueling");
+            }
+        }
+
+    }
+}
