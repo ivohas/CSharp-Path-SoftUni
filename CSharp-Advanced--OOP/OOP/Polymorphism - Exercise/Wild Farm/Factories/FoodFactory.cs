fb124092d7c24e08a904dd8d6319f010182ee690
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:51:26 +0300

    Create FoodFactory.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Factories/FoodFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace Wild
+{
+
+    public class FoodFactory : IFoodFactory
+    {
+        public Food CreateFood(string type, int q)
+        {
+            Food food;
+            if (type == "Vegetable")
+            {
+                food = new Vegetable(q);
+            }
+            else if (type == "Meat")
+            {
+                food = new Meat(q);
+            }
+            else if (type == "Seeds")
+            {
+                food = new Seeds(q);
+            }
+            else if (type == "Fruit")
+            {
+                food = new Fruit(q);
+            }
+            else
+            {
+                throw new InvalidFactoryTypeException();
+            }
+
+            return food;
+        }
+    }
+}
