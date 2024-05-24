f9f62ada1530b5c29397be2b728915f3cf2bcf79
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:51:21 +0300

    Create InvalidFactoryTypeException.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Exceptions/InvalidFactoryTypeException.cs
@@ -0,0 +1,21 @@
+﻿namespace Wild
+{
+    using System;
+
+    public class InvalidFactoryTypeException : Exception
+    {
+        private const string msg = "Invalid type!";
+
+        public InvalidFactoryTypeException()
+            : base(msg)
+        {
+
+        }
+
+        public InvalidFactoryTypeException(string msg)
+            : base(msg)
+        {
+
+        }
+    }
+}
