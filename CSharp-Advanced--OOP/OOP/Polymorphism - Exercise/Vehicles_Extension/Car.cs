318af7deec43a37dc60c903010c6ff4b7222aabd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-24 11:51:02 +0300

    Create Car.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Vehicles_Extension/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car : Vehicle
+    {
+
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) :
+            base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption + 0.9;
+        }
+
+        public override void Drive(double distance)
+        {
+            double neededFuel = distance * this.FuelConsumption;
+            if (FuelQuantity >= neededFuel)
+            {
+                Console.WriteLine($"Car travelled {distance} km");
+                this.FuelQuantity -= neededFuel;
+            }
+            else
+            {
+                Console.WriteLine($"Car needs refueling");
+            }
+        }
+    }
+}
