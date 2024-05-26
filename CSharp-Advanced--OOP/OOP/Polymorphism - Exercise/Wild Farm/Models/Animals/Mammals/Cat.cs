1397491b271f5e6f3f2e0953528f398381ac2db1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:43 +0300

    Create Cat.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Mammals/Cat.cs
@@ -0,0 +1,27 @@
+﻿namespace Wild
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Cat : Feline
+    {
+        private const double catWMultiplier = 0.30;
+
+        public Cat(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion, breed)
+        {
+
+        }
+
+        protected override IReadOnlyCollection<Type> FoodItLovesToEat
+            => new List<Type> { typeof(Vegetable), typeof(Meat) }.AsReadOnly();
+
+        protected override double Weghaitmultipleyer
+            => catWMultiplier;
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
