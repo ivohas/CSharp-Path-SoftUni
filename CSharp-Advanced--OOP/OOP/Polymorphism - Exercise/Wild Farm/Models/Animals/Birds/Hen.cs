7c99e3d904800e034006fa975b2f5dee5987db7e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:36 +0300

    Create Hen.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Birds/Hen.cs
@@ -0,0 +1,29 @@
+﻿namespace Wild
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Hen : Bird
+    {
+        private const double henweghitmultiplayer = 0.35;
+
+        public Hen(string name, double weight, double wingS)
+            : base(name, weight, wingS)
+        {
+
+        }
+
+        protected override IReadOnlyCollection<Type> FoodItLovesToEat
+            => new List<Type>
+                    { typeof(Fruit), typeof(Meat), typeof(Seeds), typeof(Vegetable) }
+                .AsReadOnly();
+
+        protected override double Weghaitmultipleyer
+            => henweghitmultiplayer;
+
+        public override string ProduceSound()
+        {
+            return "Cluck";
+        }
+    }
+}
