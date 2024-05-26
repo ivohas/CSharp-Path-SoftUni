96ec6d30c2aa484525cbdfd916fe8063e2695eaf
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:33:18 +0300

    Create Cat.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Animals/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favFood) :
+            base(name, favFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favFood;
+        }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + " Meow";
+        }
+    }
+}
