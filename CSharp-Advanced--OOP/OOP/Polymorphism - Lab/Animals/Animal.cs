682f4a80db39c9d40a89e91956271401aa886f55
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:33:14 +0300

    Create Animal.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, string favFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favFood;
+        }
+        public string Name { get; set; }
+        public string FavouriteFood { get; set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}
