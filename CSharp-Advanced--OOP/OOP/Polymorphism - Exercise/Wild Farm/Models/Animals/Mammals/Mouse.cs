2f25a47525bf7d00ac11ca8ef5c19a62234249ac
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:52 +0300

    Create Mouse.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Mammals/Mouse.cs
@@ -0,0 +1,32 @@
+﻿namespace Wild
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Mouse : Mammal
+    {
+        private const double moseWMultiplier = 0.10;
+
+        public Mouse(string name, double weight, string region)
+            : base(name, weight, region)
+        {
+
+        }
+
+        protected override IReadOnlyCollection<Type> FoodItLovesToEat
+            => new List<Type> { typeof(Fruit), typeof(Vegetable) }.AsReadOnly();
+
+        protected override double Weghaitmultipleyer
+            => moseWMultiplier;
+
+        public override string ProduceSound()
+        {
+            return "Squeak";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{this.Weight}, {this.Region}, {this.Footeaten}]";
+        }
+    }
+}
