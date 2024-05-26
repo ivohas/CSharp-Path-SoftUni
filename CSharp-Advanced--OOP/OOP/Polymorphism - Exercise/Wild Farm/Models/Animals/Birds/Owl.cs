ef0f8e468fe2764c06cf2b0f12b8eba2f7209090
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:41 +0300

    Create Owl.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Birds/Owl.cs
@@ -0,0 +1,27 @@
+﻿namespace Wild
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Owl : Bird
+    {
+        private const double OwlMultiplier = 0.25;
+
+        public Owl(string name, double weight, double wingS)
+            : base(name, weight, wingS)
+        {
+
+        }
+
+        protected override IReadOnlyCollection<Type> FoodItLovesToEat
+            => new List<Type> { typeof(Meat) }.AsReadOnly();
+
+        protected override double Weghaitmultipleyer
+            => OwlMultiplier;
+
+        public override string ProduceSound()
+        {
+            return "Hoot Hoot";
+        }
+    }
+}
