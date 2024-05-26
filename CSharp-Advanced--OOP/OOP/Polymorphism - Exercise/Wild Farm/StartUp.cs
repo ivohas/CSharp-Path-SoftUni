da475e7dfb46c1c448db94e0b9b10abd7dae3e0c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:33:09 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Wild
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            IFoodFactory foodFactory = new FoodFactory();
+            IAnimalFactory animalFactory = new AnimalFactory();
+
+            IEngine engine = new Engine(foodFactory, animalFactory);
+            engine.Start();
+        }
+    }
+}
