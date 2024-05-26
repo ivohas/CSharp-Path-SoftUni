4989060c134097c96de816d9c3f284d0a6e7250f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:54 +0300

    Create Tiger.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Mammals/Tiger.cs
@@ -0,0 +1,27 @@
+﻿namespace Wild
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tiger : Feline
+    {
+        private const double tigerMultiplier = 1;
+
+        public Tiger(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion, breed)
+        {
+
+        }
+
+        protected override IReadOnlyCollection<Type> FoodItLovesToEat
+            => new List<Type> { typeof(Meat) }.AsReadOnly();
+
+        protected override double Weghaitmultipleyer
+            => tigerMultiplier;
+
+        public override string ProduceSound()
+        {
+            return "ROAR!!!";
+        }
+    }
+}
