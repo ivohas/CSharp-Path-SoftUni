1c3caedaa97331414dfd2d4cc109e2989acd4649
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:33:21 +0300

    Create Dog.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Animals/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string favFood) : base(name, favFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favFood;
+        }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + " DJAAF";
+        }
+    }
+}
