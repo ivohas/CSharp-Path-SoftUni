9119f16ce3ba17369229a40dc0426f6fb1fa3ffa
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:39 +0300

    Create Bird.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Birds/Bird.cs
@@ -0,0 +1,18 @@
+﻿namespace Wild
+{
+    public abstract class Bird : Animal
+    {
+        protected Bird(string name, double weight, double wingS)
+            : base(name, weight)
+        {
+            this.WingS = wingS;
+        }
+
+        public double WingS { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{this.WingS}, {this.Weight}, {this.Footeaten}]";
+        }
+    }
+}
