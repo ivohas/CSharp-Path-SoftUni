f9badf1b82b0016a881cb3e43d6e1045c6754c2c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:50 +0300

    Create Mammal.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Mammals/Mammal.cs
@@ -0,0 +1,13 @@
+﻿namespace Wild
+{
+    public abstract class Mammal : Animal
+    {
+        protected Mammal(string name, double weight, string livingRegion)
+            : base(name, weight)
+        {
+            this.Region = livingRegion;
+        }
+
+        public string Region { get; }
+    }
+}
