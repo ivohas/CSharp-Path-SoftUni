fe1ac739a8e02bb28900094f17eb6de873726fa2
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:47 +0300

    Create Feline.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Mammals/Feline.cs
@@ -0,0 +1,19 @@
+﻿namespace Wild
+{
+    public abstract class Feline : Mammal
+    {
+        protected Feline(string name, double weight, string region, string breed)
+            : base(name, weight, region)
+        {
+            this.Breed = breed;
+        }
+
+        public string Breed { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"{this.Breed}, {this.Weight}, {this.Region}, {this.Footeaten}]";
+        }
+    }
+}
