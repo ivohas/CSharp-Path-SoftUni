b2d7f92e867f5a7a4a28b4598cbe518eec680363
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-26 19:32:45 +0300

    Create Dog.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Wild Farm/Models/Animals/Mammals/Dog.cs
@@ -0,0 +1,32 @@
+﻿namespace Wild
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Dog : Mammal
+    {
+        private const double dogWMultiplier = 0.40;
+
+        public Dog(string name, double weight, string livingRegion)
+            : base(name, weight, livingRegion)
+        {
+
+        }
+
+        protected override IReadOnlyCollection<Type> FoodItLovesToEat
+            => new List<Type> { typeof(Meat) }.AsReadOnly();
+
+        protected override double Weghaitmultipleyer
+            => dogWMultiplier;
+
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{this.Weight}, {this.Region}, {this.Footeaten}]";
+        }
+    }
+}
