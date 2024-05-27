c62aa1222322ee23ba9a88dc3edb85b4074bc2d1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:34:39 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-14-Aug-2022/Business-Logic/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using PlanetWars.Core;
+using System;
+
+namespace PlanetWars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
