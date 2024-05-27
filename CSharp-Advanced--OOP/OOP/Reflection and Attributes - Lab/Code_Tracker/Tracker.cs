37829be8e3f511ac7e7f29b32b5b10d064d05c6a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:31:38 +0300

    Create Tracker.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection and Attributes - Lab/Code_Tracker/Tracker.cs
@@ -0,0 +1,35 @@
+﻿using AuthorProblem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public Tracker()
+        {
+
+        }
+
+
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
