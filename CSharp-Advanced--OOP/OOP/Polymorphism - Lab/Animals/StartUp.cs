3a162af74ed3d5a3852f47066f54d12b6dd9ebdf
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:30:33 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Animals/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Animal cat = new Cat("Peter", "Whiskas");
+            Animal dog = new Dog("George", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+
+        }
+    }
+}
