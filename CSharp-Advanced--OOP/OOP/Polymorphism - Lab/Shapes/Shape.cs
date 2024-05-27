b96d71b15f9692fc4e0c66b261699384146e7fac
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:30:57 +0300

    Create Shape.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Shapes/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public Shape(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        private double height;
+
+        public double Height
+        {
+            get { return height; }
+            protected set { height = value; }
+        }
+
+        private double width;
+
+        public double Width
+        {
+            get { return width; }
+            protected set { width = value; }
+        }
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+        public virtual string Draw()
+        {
+            return "";
+        }
+    }
+}
