572df59720d7a34ff3fae8ffb596422965b3a908
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:34:48 +0300

    Create Robot.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Birthday_Celebrations/Models/Robot.cs
@@ -0,0 +1,9 @@
+﻿namespace Border_Control.Models
+{
+    using Models.Interfaces;
+    public class Robot : IRobot
+    {
+        public string Model { get; private set; }
+        public string ID { get; private set; }
+    }
+}
