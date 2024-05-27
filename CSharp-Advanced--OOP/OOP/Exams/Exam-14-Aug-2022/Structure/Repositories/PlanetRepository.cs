f4f7efc933cd74490eebd073fb371800d7a04cc6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:34:42 +0300

    Create PlanetRepository.cs

---
+++ CSharp-Advanced--OOP/OOP/Exams/Exam-14-Aug-2022/Structure/Repositories/PlanetRepository.cs
@@ -0,0 +1,25 @@
+﻿using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly HashSet<IPlanet> models;
+        public PlanetRepository()
+        {
+            this.models = new HashSet<IPlanet>();
+        }
+        public IReadOnlyCollection<IPlanet> Models => this.models;
+
+        public void AddItem(IPlanet model) => this.models.Add(model);
+
+        public IPlanet FindByName(string name) => models.FirstOrDefault(x => x.Name == name);
+
+        public bool RemoveItem(string name) => models.Remove(models.FirstOrDefault(x => x.Name == name));
+    }
+}
