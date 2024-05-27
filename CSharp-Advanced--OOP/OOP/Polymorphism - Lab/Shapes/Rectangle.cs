7ff602807834b528f438e39acb50e535cb4c2a91
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:30:52 +0300

    Create Rectangle.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width) : base(height, width)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public override double CalculateArea()
+        {
+            return base.Height * base.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (base.Height + base.Width);
+        }
+
+        public override string Draw()
+        {
+            return "Rectangle";
+        }
+    }
+}
