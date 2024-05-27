1a159bc7a5ecb9ce870e0613b923f6fa2bb4d22d
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-27 17:30:47 +0300

    Create Circle.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius, double height, double width) : base(height, width)
+        {
+            this.Radius = radius;
+            this.Height = height;
+            this.Width = width;
+        }
+
+        private double radius;
+
+        public double Radius
+        {
+            get { return radius; }
+            private set { radius = value; }
+        }
+        public override double CalculateArea()
+        {
+            return  Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius ;
+        }
+
+        public override string Draw()
+        {
+            return "Circle";
+        }
+    }
+}
