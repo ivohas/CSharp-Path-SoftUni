60141b473e5e8ec03b2f7e27d129838ef4c50123
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-28 18:47:22 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Food_Shortage/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Border_Control
+{
+    using Models;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Citizen> citizens = new List<Citizen>();
+            List<Rebel> rebels = new List<Rebel>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                if (input.Length == 4)
+                {
+                    string id = input[2];
+                    string birthDate = input[3];
+                    Citizen citizen = new Citizen(name, age, id, birthDate);
+                    citizens.Add(citizen);
+                }
+                else
+                {
+                    string group = input[2];
+                    Rebel rebel = new Rebel(name, age, group);
+                    rebels.Add(rebel);
+                }
+            }
+
+            int food = 0;
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string name = command;
+                if (citizens.Contains(citizens.FirstOrDefault(x => x.Name == name)))
+                {
+                    food += 10;
+                }
+                else if (rebels.Contains(rebels.FirstOrDefault(x => x.Name == name)))
+                {
+                    food += 5;
+                }
+            }
+            Console.WriteLine(food);
+        }
+    }
+}
