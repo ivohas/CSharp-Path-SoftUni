19736377fdc0ed06db7c1776bfc1ed117d0ccf7b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-28 18:46:44 +0300

    Create Spy.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection and Attributes - Lab/Mission_Private_Impossible/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string investigatedClass)
+        {
+            Type typeOfClass = Type.GetType(investigatedClass);
+            MethodInfo[] privateMethods = typeOfClass.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            Console.WriteLine($"All Private Methods of Class: {typeOfClass.FullName}");
+            Console.WriteLine($"Base class: {typeOfClass.BaseType.Name}");
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in privateMethods)
+            {
+                sb.AppendLine(method.Name);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
