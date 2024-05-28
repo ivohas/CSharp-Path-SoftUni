4bc80492562dbc5f86de40c6121819516c2a70fd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-28 18:47:16 +0300

    Create Rebel.cs

---
+++ CSharp-Advanced--OOP/OOP/Interfaces_and_Abstraction_Exercise/Food_Shortage/Models/Rebel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Border_Control.Models
+{
+    using Models.Interfaces;
+    public class Rebel : IBuyer
+    {
+        public Rebel(string name, int age, string group)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Group = group;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Group { private get; set; }
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 5;
+        }
+    }
+}
