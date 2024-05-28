28dd15b448e948ac3ab95414c2335d13f945b832
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-28 18:47:24 +0300

    Create Druid.cs

---
+++ CSharp-Advanced--OOP/OOP/Polymorphism - Exercise/Raiding/Druid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        public Druid(string name) : base(name)
+        {
+            this.Name = name;
+            this.Power = 80;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Druid - {this.Name} healed for {this.Power}";
+        }
+    }
+}
