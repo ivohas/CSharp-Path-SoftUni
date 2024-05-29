53aae4b4472799a84f84c8b8b40eb0422a29b76c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:43:03 +0300

    Create MyValidationAttribute.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Validation_Attributes/Utilities/Attributes/MyValidationAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Validation_Attributes.Utilities
+{
+    public abstract class MyValidationAttribute : Attribute
+    {
+        public abstract bool IsValid(object obj);
+    }
+}
