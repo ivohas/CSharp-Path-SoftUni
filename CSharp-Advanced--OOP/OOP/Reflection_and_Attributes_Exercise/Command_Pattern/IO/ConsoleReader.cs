2537e40a0e7f9dbfe7c49cba08ede4f87c1c3206
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:41:29 +0300

    Create ConsoleReader.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Command_Pattern/IO/ConsoleReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            string line = Console.ReadLine();
+            return line;
+        }
+    }
+}
