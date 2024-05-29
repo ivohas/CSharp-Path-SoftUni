f617879842f563bd0b085d1ec9ceac808f51a62c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:42:57 +0300

    Create StartUp.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Validation_Attributes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var person = new Person
+            (
+                "Gosho",
+                18
+            );
+            goto LOOP;
+            LOOP:
+            bool isValid = Validator.IsValid(person);
+
+            Console.WriteLine(isValid);
+        }
+    }
+}
