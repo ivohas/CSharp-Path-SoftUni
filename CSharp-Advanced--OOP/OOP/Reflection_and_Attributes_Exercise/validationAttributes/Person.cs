4a6ab6efdd279491a76f407e5e53700d3751b3f6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:43:22 +0300

    Create Person.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/validationAttributes/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace ValidationAttributes
+{
+    using Utilities;
+    using Attributes;
+
+    public class Person
+    {
+        private const int godiniMinimum = 12;
+        private const int godiniMaximum = 90;
+
+        [MyRequired]
+        public string PulnoIme { get; set; }
+
+        [MyRange(godiniMinimum, godiniMaximum)]
+        public int Godini { get; set; }
+        public Person(string pulnoime, int age)
+        {
+            this.PulnoIme = pulnoime;
+            this.Godini = age;
+        }
+    }
+}
+
