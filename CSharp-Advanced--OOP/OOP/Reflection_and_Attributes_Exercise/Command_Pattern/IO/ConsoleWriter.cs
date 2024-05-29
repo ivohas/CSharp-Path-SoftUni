42547d401bd31028cfab995ab13cd3b24808eabd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:41:31 +0300

    Create ConsoleWriter.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Command_Pattern/IO/ConsoleWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(string value)
+        {
+            Console.Write(value);
+        }
+
+        public void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
