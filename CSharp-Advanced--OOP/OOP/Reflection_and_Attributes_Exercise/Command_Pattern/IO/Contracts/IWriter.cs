7973fcc95d9455e79ba44cbf7adfd0b0a5c405f4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:42:42 +0300

    Create IWriter.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Command_Pattern/IO/Contracts/IWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public interface IWriter
+    {
+        void Write(string value);
+        void WriteLine(string value); 
+    }
+}
