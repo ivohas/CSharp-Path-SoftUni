3924334686ff2674717071f996a0976eb8163e87
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-29 18:43:05 +0300

    Create MyRangeAttribute.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Validation_Attributes/Utilities/MyRangeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAtribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if (obj is int value)
+            {
+                return value >= minValue && value <= maxValue;
+            }
+
+            return false;
+        }
+    }
+}
+
