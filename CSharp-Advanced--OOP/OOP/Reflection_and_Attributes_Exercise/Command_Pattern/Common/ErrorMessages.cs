59893e1ec59e72f357be5f7e6f0e74c6e92181db
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-30 15:57:23 +0300

    Create ErrorMessages.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Command_Pattern/Common/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public static class ErrorMessages
+    {
+        public const string InvalidCommandType = "Provided type {0} does not exist or it does not implement ICommand Interface";
+    }
+}
