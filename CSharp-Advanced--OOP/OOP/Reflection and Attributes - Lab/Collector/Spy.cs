6feba47adee161d31b94befa6b8d4098d05feec0
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-30 15:57:10 +0300

    Create Spy.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection and Attributes - Lab/Collector/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string AnalyzeAccessModifiers(string investigatedClass)
+        {
+            Type typeOfClass = Type.GetType(investigatedClass);
+            MethodInfo[] methods = typeOfClass.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in methods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            foreach (MethodInfo method in methods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.ReturnType}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
