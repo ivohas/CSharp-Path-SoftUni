1b7158009edcfb0d903f3a7674dff36b0b8f11a4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-01 00:44:03 +0300

    Create HelloCommand.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Command_Pattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using Contracts;
+
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Hello, {args[0]}";
+        }
+    }
+}
+
