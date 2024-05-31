91defd839a037bc8338447c1d7ba24c27d177272
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-01 00:44:00 +0300

    Create ExitCommand.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/Command_Pattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using Contracts;
+    using System;
+
+    public class ExitCommand : ICommand
+    {
+        private int exitCode = 0;
+
+        public string Execute(string[] args)
+        {
+            Environment.Exit(exitCode);
+            return null;
+        }
+    }
+}
+
