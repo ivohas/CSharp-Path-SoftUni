fb8bd1136f4ef7e78caf3610aeef6919570e6b22
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-01 00:44:31 +0300

    Create Program.cs

---
+++ Old projects/Bakery shop/Bakery shop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> water = Console.ReadLine().Split().Select(double.Parse).ToList();
+            List<double> flavour = Console.ReadLine().Split().Select(double.Parse).ToList();
+            Dictionary<string, double> proportions = new Dictionary<string, double>();
+            proportions.Add("Croissant", 0.5);
+            proportions.Add("Muffin", 0.4);
+            proportions.Add("Baguette", 0.3);
+            proportions.Add("Bagel", 0.2);
+            flavour.Remove 
+
+        }
+    }
+}
