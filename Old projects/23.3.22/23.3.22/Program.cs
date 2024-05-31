b75010700cfe78497ffef0dbdac6f2c7a7eeb87b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-31 17:40:11 +0300

    Create Program.cs

---
+++ Old projects/23.3.22/23.3.22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._3._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+               result+=result*i;
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
