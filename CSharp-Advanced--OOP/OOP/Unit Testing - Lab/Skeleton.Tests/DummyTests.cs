540e617a82a3060614d6c2420fc2f4e4d248b648
Ivaylo Hasov <ivailohasov@gmail.com>
2024-05-31 17:39:36 +0300

    Create DummyTests.cs

---
+++ CSharp-Advanced--OOP/OOP/Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLoosesHealth()
+        {
+            Axe axe = new Axe(5, 5);
+            Dummy dummy = new Dummy(10, 10);
+            int dummyStartHP = dummy.Health;
+
+            axe.Attack(dummy);
+            Assert.That(dummy.Health == dummyStartHP - axe.AttackPoints);
+        }
+
+        [Test]
+        public void DummyThrowsAnExceptionIfDeadAndAttacked()
+        {
+            Axe axe = new Axe(5, 5);
+            Dummy dummy = new Dummy(5, 5);
+
+            axe.Attack(dummy);
+            TestDelegate test;
+            test = () => axe.Attack(dummy);
+            Assert.Catch(test);
+        }
+
+        [Test]
+        public void DummyGivesXPAfterDeath()
+        {
+            Axe axe = new Axe(5, 5);
+            int dummyXP = 5;
+            Dummy dummy = new Dummy(5, dummyXP);
+                      
+            axe.Attack(dummy);
+
+            Assert.That(dummy.GiveExperience() == dummyXP);
+        }
+
+        [Test]
+        public void DummyDoesntGiveXP()
+        {
+            Axe axe = new Axe(5, 5);
+            Dummy dummy = new Dummy(6, 5);
+
+            axe.Attack(dummy);
+
+            Assert.That(() =>
+            {
+                dummy.GiveExperience();
+            },
+            Throws.Exception.TypeOf<InvalidOperationException>(),
+             "Target is not dead.");
+        }
+    }
+}
