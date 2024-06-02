d116266b64b19200b04d183333e8de096dfeba21
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-02 13:13:32 +0300

    Create Program.cs

---
+++ Old projects/basic sintacs/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int divider = -1;
+            if (num % 10 == 0)
+            {
+                divider = 10;
+            }
+            else if (num % 7 == 0)
+            {
+                divider = 7;
+            }
+            else if (num % 6 == 0)
+            {
+                divider = 6;
+            }
+            else if (num% 3 == 0)
+            {
+                divider = 3;
+            }
+            else if (num % 2 == 0)
+            {
+                divider = 2;
+            }
+
+            if (divider == -1) 
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by { divider}");
+            }
+        }
+    }
+}
