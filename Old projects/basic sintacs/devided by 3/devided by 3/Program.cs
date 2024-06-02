b2844cc7247a20a4b40b3140078679c57a4fb7d2
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-02 13:14:05 +0300

    Create Program.cs

---
+++ Old projects/basic sintacs/devided by 3/devided by 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace devided_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 3; i <100; i=i+3)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine("Sum: "+sum);
+        }
+    }
+}
