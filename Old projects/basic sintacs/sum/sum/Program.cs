1f96da884f12204af9c098cc25bfcfd76b50dbbc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-03 20:13:46 +0300

    Create Program.cs

---
+++ Old projects/basic sintacs/sum/sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int b = a * i;
+                Console.WriteLine($"{a} X {i} = {b}");
+            }
+        }
+    }
+}
