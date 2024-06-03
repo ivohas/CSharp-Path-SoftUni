a4d2757a9cfc894a84fa72045573549e1119cc96
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-03 20:11:44 +0300

    Create Program.cs

---
+++ Old projects/basic sintacs/language/language/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace language
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string language= Console.ReadLine ();
+            switch (language)
+            {
+                case "USA":
+                case "England": Console.WriteLine("English");break;
+                case "Mexico":
+                case "Spain":
+                case "Argentina": Console.WriteLine("Spanish");break;
+                default: Console.WriteLine("unknown");break;  
+            }
+        }
+    }
+}
