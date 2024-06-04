2310026a99555808e4c9671d95eddc2a1a1d2368
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-04 20:14:09 +0300

    Create Program.cs

---
+++ Old projects/basic sintacs/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes=int.Parse(Console.ReadLine());
+            minutes += 30;
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes -= 60;
+
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+            if(minutes < 10)
+            {
+             Console.WriteLine($"{hours}:{minutes:d2}");
+            }else
+            Console.WriteLine($"{hours}:{minutes}");
+        }
+    }
+}
