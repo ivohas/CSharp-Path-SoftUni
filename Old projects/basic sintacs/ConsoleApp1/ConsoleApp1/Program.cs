121e609489a8d9f555817130b43e08eb48fa4952
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-04 20:13:56 +0300

    Create Program.cs

---
+++ Old projects/basic sintacs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade= double.Parse(Console.ReadLine());
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else { Console.WriteLine("Failed!"); }
+        }
+    }
+}
