fa77de8ca1896862f7f4a2181490eec35c0985e6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-05 21:22:24 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s= Console.ReadLine();
+            Stack<char> strings = new Stack<char>();
+            foreach (var ch in s)
+            {
+                strings.Push(ch);
+            }
+            while (strings.Count>0)
+            {
+                Console.Write(strings.Pop());
+            }
+        }
+    }
+}
