4f73a82b29f6ddb03cdc3482c06e79ba766e9da1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-05 21:21:47 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp10/zad2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Threading.Tasks;
+
+namespace zad2
+{     
+    internal class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {  
+            int totalPoints = 0;
+            List<string>Places=new List<string>();
+            string places = Console.ReadLine();
+            // string pattern = @"(=|/)(?<destination>[A-Z]+[A-Za-z]{2,})(\1)";
+            Regex regex = new Regex(@"(=|/)(?<destination>[A-Z]+[A-Za-z]{2,})(\1)");
+            MatchCollection matches = regex.Matches(places);
+            foreach (var currMatch in matches)
+            {
+                string currentDestination = currMatch.[2].Value;
+                Places.Add(currentDestination);
+                totalPoints+=currentDestination .Length;
+            }
+            Console.WriteLine("Destinations:" + String.Join(", ",Places));
+            Console.WriteLine($"Travel Points: { totalPoints}");
+            
+        }
+    }
+}
