e36a6b2cc77cc27f31321aff177d5d5624cd8a90
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-06 15:28:08 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp11/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var integers = Console.ReadLine().Split(' ').ToArray();
+            List<int> list = new List<int>();
+           int i = 0;
+            foreach (var item in integers)
+            {
+               
+                list.Add(int.Parse(integers[i]));
+                i++;
+            }
+            list.Select(x => x % 2 == 0);
+            string.Join(" ", integers);
+        }
+    }
+}
