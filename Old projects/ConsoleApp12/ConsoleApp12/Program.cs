e2f73b662488e38bc4795a69578f3451ffe177cc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-06 15:29:22 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            Array.Reverse(input);
+            Stack<string> stack = new Stack<string>(input);
+            int sum = int.Parse(stack.Pop());
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                string operation = stack.Pop();
+                int number = int.Parse(stack.Pop());
+
+                if (operation == "+")
+                {
+                    sum += number;
+                }
+                else
+                {
+                    sum -= number;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
