e2d1b50b024cb92ad24fe874dfdca14a33933e85
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-06 15:28:17 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp12/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> paid = new Queue<string>();
+            string name = Console.ReadLine(); int counter = 0;
+
+            while (name != "End")
+            {
+                counter++;
+                paid.Enqueue(name);
+                if (name == "Paid")
+                {
+
+                    for (int i = 1; i < counter; i++)
+                    {
+                        
+                        Console.WriteLine(paid.Dequeue()); 
+
+                    }
+
+                    
+                    counter = 0;
+                }
+
+
+                name = Console.ReadLine();
+            }
+           
+            Console.WriteLine($"{counter} people remaining.");
+        }
+    }
+}
