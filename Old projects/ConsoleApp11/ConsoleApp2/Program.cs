b863a6b30e7780fb76f2270ade06aceb5a1ab839
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-06 15:27:49 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp11/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expresion = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                char ch = expresion[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                if (ch==')')
+                {
+                    int startInd = stack.Pop();
+                    int endInd = i+1;
+                    string sub= expresion.Substring(startInd, endInd - startInd);
+                    Console.WriteLine(sub);
+                }
+            }
+
+        }
+    }
+}
