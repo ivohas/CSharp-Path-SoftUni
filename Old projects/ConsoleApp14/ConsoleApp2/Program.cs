41662f4c327d095ef52ba523e4890cf9531dc814
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-08 14:19:45 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp14/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list=Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity=int.Parse(Console.ReadLine());int sum = 0;int raws = 0;
+            int count = list.Count;
+            while (count>=0 )
+            {
+                int i = 0;
+                if (sum + list[i] < capacity)
+                {
+                    sum = +list[i];
+
+                }
+                else 
+                {
+                    raws++;
+                    sum = 0;
+                }
+                
+
+
+
+                    i++;
+                count--;
+
+            }
+            Console.WriteLine(raws);
+        }
+    }
+}
