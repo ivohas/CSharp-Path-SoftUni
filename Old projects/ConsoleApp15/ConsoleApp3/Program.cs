3c3d15fdbfcfb556e79ef8d3b420ca00cf31da9a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-09 20:45:55 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp15/ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> dict = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                var list = Console.ReadLine().Split(' ').ToList();
+                string name = list[0];
+                double grade = int.Parse(list[1]);
+                if (dict.ContainsKey(name))
+                {
+                    dict[name].Add(grade);
+                }
+                else
+                {
+                    dict[name]= new List<double>{ grade};
+                }
+
+            }
+            foreach (var item in dict)
+            {
+                
+                Console.WriteLine($"{item.Key} ->)");
+            }
+        }
+    }
+}
