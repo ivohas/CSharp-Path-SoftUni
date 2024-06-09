e02706ccd26e354b71cd5512218f21501212321b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-09 20:45:47 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp15/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array= Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x=>x).ToList();
+            for (int i = 0; i < 3; i++)
+            {
+                if (i<array.Count )
+                {
+                    Console.Write(array[i]+" ");
+                }
+            }
+           
+        }
+    }
+}
