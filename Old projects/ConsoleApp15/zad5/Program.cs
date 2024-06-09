9e39db46dbfee5faddeccbe7e83c28d4aa0c9172
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-09 20:46:01 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp15/zad5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, Dictionary<string,string>>();
+            for (int i = 0; i < n; i++)
+            {
+                List<string> list = new List<string>();
+                string continent = list[0];
+                string country = list[1];
+                string capital = list[2];
+                if (dict.ContainsKey(continent))
+                {
+                    dict[continent].Add(country);
+                    dict[country].Add(capital);
+                }
+                else
+                {
+                    dict[continent] = new Dictionary<string, string>[country];
+                }
+               
+            }
+        }
+    }
+}
