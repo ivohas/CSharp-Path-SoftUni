36572c973efe437d168ce07024196b0a859936b2
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-16 12:03:53 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
