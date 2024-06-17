38470b7d5a38010203a07d930015f7afc40d7172
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-17 18:10:04 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+
+            };
+            int day = int.Parse(Console.ReadLine());
+            day --;
+            if (day < 7&&day>=0)
+            {
+                Console.WriteLine(array[day]);
+            }
+            else 
+            {           
+                Console.WriteLine("Invalid day!");
+            }
+            
+        }
+    }
+}
