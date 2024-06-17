5073305dede8249a71422013727da1d86829b261
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-17 18:10:23 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp4/rounding numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rounding_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = int.Parse(Console.ReadLine());
+            double[] arrayOfNums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+          
+            for (int i = 0; i <= count; i++)
+            {
+                Console.WriteLine(($"{arrayOfNums[i]}=>{Math.Round(arrayOfNums[i],MidpointRounding.AwayFromZero)}"));
+            }
+        }
+    }
+}
