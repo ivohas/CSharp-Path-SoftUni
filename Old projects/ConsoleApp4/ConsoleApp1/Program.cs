215c0fb0fdfaaf3cd252f04a63707fed5774c3af
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-17 18:09:26 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp4/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count= int.Parse(Console.ReadLine());
+            int []array= new int[count];
+            for (int i = 0; i < count; i++) 
+            { 
+               array[i]=int.Parse(Console.ReadLine());  
+            }
+            Array.Reverse(array);
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
