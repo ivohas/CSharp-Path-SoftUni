b41bd940bd03aa6fb04eae68a3c2f1e88b1fafb1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-17 18:09:48 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp4/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arrayOfSums = new int[array.Length - 1];int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {   if (array[i] != array.Length)
+                { arrayOfSums[i] = array[i] + array[i + 1];
+
+                }
+                
+            }
+            for (int i = 0; i < arrayOfSums.Length ; i++)
+            {
+                sum += arrayOfSums[i]; 
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
