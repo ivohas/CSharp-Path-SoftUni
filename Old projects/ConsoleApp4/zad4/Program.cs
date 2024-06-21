5a6f648beb789f8fed132b8af9e63b47a9cf94d9
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-21 11:27:47 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp4/zad4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] arrayOfStrings= Console.ReadLine().Split(' ').ToArray();
+            Array.Reverse (arrayOfStrings);
+            Console.WriteLine(string.Join(" ",arrayOfStrings));
+        }
+    }
+}
