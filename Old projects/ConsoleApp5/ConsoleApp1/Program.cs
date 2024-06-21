28d383fd89ba19f4b25847e8635a0e879c67b107
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-21 11:28:23 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp5/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine()
+                .Split();
+
+            string[] second = Console.ReadLine()
+                .Split();
+
+            string result = "";
+
+            foreach (var kvp in second)
+            {
+                if (first.Contains(kvp))
+                {
+                    result += " " + kvp;
+                    //Console.Write($" {kvp}");
+                }
+            }
+
+            Console.Write($"{result.Trim()}");
+        }
+    }
+}
