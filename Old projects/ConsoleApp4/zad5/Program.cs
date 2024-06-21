dd08534afb83b4cb8893f0c404220478bc822f19
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-21 11:28:00 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp4/zad5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfEven = 0;
+            int sumOfOdd = 0;
+            int []array= Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i <array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    sumOfEven += array[i];
+                }
+                else
+                {
+                    sumOfOdd += array[i];
+                }
+                
+            }
+            Console.WriteLine(sumOfEven-sumOfOdd);
+        }
+    }
+}
