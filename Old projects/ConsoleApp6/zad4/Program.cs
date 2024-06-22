9a9e9661f1ba55afb07477f5fcc6ec100c0ea8af
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-22 16:09:31 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp6/zad4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> lists = new List<string>();
+           int i = 0;
+            for (i=0; i < n; i++)
+            { string a=Console.ReadLine ();
+                lists.Add(a);
+            }
+            lists.Sort();
+            
+            for(i = 0; i < n; i++)
+            {
+                
+                Console.WriteLine($"{i + 1}.{lists[i]}");
+                
+            }
+        }
+    }
+}
