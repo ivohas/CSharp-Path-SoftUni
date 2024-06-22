60163f364930101c8f56bc13803a44867026746e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-22 16:09:13 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp6/zad2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int Lenght = list.Count;
+
+            for (int i = 0; i < Lenght / 2; i++)
+            {
+                list[i] += list[list.Count - i];
+                list.RemoveAt(list.Count - i);
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
