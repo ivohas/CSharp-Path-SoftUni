6427d436bcc1f80f76a268bd1ebf210ec464c6a3
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-22 16:08:51 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[]array=new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; i++) 
+            {
+                array[i]= int.Parse(Console.ReadLine());
+                sum+=array[i];
+            }
+            Console.WriteLine(String.Join(" ",array));
+            Console.WriteLine(sum);
+        }
+    }
+}
