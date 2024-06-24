73a32f919f27b4fd5a96d5db7222a2504ee5676c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-24 13:23:47 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp6/zad5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < list.Count ; i++)
+            {
+                if(list[i]<0)
+                {
+                    list.RemoveAt(i);
+                    i = 0;
+                }
+               
+               
+            } list.Reverse();
+            Console.WriteLine(String.Join(" ",list));
+        } 
+       
+    }
+}
