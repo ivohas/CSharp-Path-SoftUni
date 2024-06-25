f19b566c1034d991ead5e44f022f430260fbe262
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-25 15:15:09 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp9/zad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            char[] chars = first.ToCharArray();
+            List<char> charsOfFirst= chars .ToList();
+            char[] charsd = second.ToCharArray();
+            List<char>charsOfSecond= charsd .ToList();
+            for (int i = 0; i <charsOfFirst.Count; i++)
+            {
+                while (charsOfSecond.Contains(charsOfFirst[i]))
+                {
+                    charsOfSecond.Remove(charsOfFirst[i]);
+                }
+            }
+            Console.WriteLine(String.Join("",charsOfSecond));
+        }
+    }
+}
