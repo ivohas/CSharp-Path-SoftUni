c4ebbe02ae31ad2e6a5f84fef6dd1239cd4a3fc7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-06-25 15:15:22 +0300

    Create Program.cs

---
+++ Old projects/ConsoleApp9/zad2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+           
+            while (line != "end")
+            {
+                string reversed = string.Empty;
+
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    reversed += line[i];
+                }
+                Console.WriteLine($"{ line} = {reversed}"); 
+                line = Console.ReadLine();
+            }
+
+        }
+    }
+}
