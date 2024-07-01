fe4f85cde8773b383e14881cc7ee1092ab9be50b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-01 14:15:43 +0300

    Create Drone.cs

---
+++ Old projects/Drones/Drones/Drone.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Drones
+{
+    public class Drone
+    {
+        //        •	Name: string
+        //•	Brand: string
+        //•	Range: int
+        //•	Available: boolean - true by default
+
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Range { get; set; }
+        public bool Available = true;
+
+        public Drone(string name, string brand, int range)
+        {
+            Name = name;
+            Brand = brand;
+            Range = range;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+                       
+            sb.AppendLine($"Drone: {Name}")
+            .AppendLine($"Manufactured by: {Brand}")
+            .AppendLine($" Range: {Range} kilometers");
+
+            return sb.ToString();
+        }
+    }
+}
