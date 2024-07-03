063eaa7c0067e593bf9e82b9a4d7055026507cea
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-03 14:37:31 +0300

    Create Program.cs

---
+++ Old projects/Exercise Func Pr/Exercise Func Pr/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Func_Pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] token = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int minVAlue = token[0];
+            int maxVAlue = token[1];
+            string command = Console.ReadLine();
+            List<int> list = new List<int>();
+            if (command == "even")
+            {
+                for (int i = minVAlue; i <= maxVAlue; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        list.Add(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = minVAlue; i <= maxVAlue; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        list.Add(i);
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ",list));
+
+        }
+    }
+}
