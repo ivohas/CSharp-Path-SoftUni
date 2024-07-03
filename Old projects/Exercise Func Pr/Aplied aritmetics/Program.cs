e94c1112d489983c0888e1b27a6ab4a229e9de8f
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-03 14:37:16 +0300

    Create Program.cs

---
+++ Old projects/Exercise Func Pr/Aplied aritmetics/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplied_aritmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Func<int, int> add = x => x + 1;
+            Func<int,int> multiply = x => x * 2;
+            Func<List<int>, List<int>> substraact = lists => lists.Select(number =>number-=1).ToList();
+            
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                switch (command)
+                {
+
+                    case "add":
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            list[i] =list[i]+1;
+                        }
+                        break;
+                    case "multiply":
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            list[i] = list[i] *2;
+                        }
+                        break;
+                    case "subtract":
+                       list=substraact(list);
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ", list));
+                        break;
+                    default:
+                        break;
+
+
+                }
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
