71e8f663edc7d8f95375f95bcfadcb7843a52839
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-04 11:46:26 +0300

    Create Program.cs

---
+++ Old projects/funcinalPrograming/Action point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var list = Console.ReadLine().Split().ToList();
+            foreach(var item in list)
+                Console.WriteLine(item);
+        }
+    }
+}
