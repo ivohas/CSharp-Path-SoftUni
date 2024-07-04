64609252ba2ac257e811277d70b0590718b7027e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-04 11:46:02 +0300

    Create Program.cs

---
+++ Old projects/Exercise Func Pr/reverse and exclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_and_exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list= Console.ReadLine().Split().Select(int.Parse).ToList();
+            list. Reverse();
+            int n = int.Parse(Console.ReadLine());
+            Func < List<int>, List<int>>  exlude = lists => lists.Where(x => x % n != 0).ToList();
+            list= exlude(list);
+            Console.WriteLine(String.Join(" ",list));
+        }
+    }
+}
