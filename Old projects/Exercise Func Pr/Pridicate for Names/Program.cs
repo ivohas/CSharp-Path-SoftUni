d6cb548c80286b2db8795cf32ccd89bc803f091c
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-04 11:45:55 +0300

    Create Program.cs

---
+++ Old projects/Exercise Func Pr/Pridicate for Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pridicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           List<string> names = Console.ReadLine().Split().ToList();
+            Func<List<string>, List<string>> name = lists => lists.Where(x => x.Length <= n).ToList();
+            names = name(names);
+            Console.WriteLine(String.Join("\r\n", names));
+        }
+    }
+}
