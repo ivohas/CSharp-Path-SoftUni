7a698c09d545c2fe171d8549f51aff97eb539d52
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-05 12:29:30 +0300

    Create Program.cs

---
+++ Old projects/funcinalPrograming/addingVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addingVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal,decimal> Vat = x => x * 1.20m;
+            Console.WriteLine(string.Join("\r\n ", Console.ReadLine().Split(',').Select(x => x.Trim()).Select(decimal.Parse).Select(Vat).Select(x=>Math.Round(x,2)).ToList())); 
+
+
+        }
+    }
+}
