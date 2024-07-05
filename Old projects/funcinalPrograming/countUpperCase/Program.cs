6df3613cdb4ff82be9dae04b255ddb9c2aaafed6
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-05 12:30:06 +0300

    Create Program.cs

---
+++ Old projects/funcinalPrograming/countUpperCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countUpperCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isCapita = (string x) => x.Length > 0 && char.IsUpper(x[0]);
+            Console.WriteLine(string.Join("\r\n ",Console.ReadLine().Split().Where(x=>isCapita(x)).ToArray()));
+           
+            
+            
+               
+        }
+    }
+}
