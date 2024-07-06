50fca6ab443f00b0133b56602b9396ab95c044d1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-06 14:00:50 +0300

    Create Library.cs

---
+++ Old projects/iterarors and compilators/iterarors and compilators/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    internal class Library
+    {
+        public List<Book> lib { get; set; }
+
+        public Library( params Book[] lib)
+        {
+            this.lib = lib.ToList();
+        }
+    }
+}
