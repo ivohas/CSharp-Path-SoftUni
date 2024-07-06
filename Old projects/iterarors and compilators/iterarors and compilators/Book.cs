8c1e92ab5e874e3a4b6311bcbe213a60c063b937
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-06 14:00:43 +0300

    Create Book.cs

---
+++ Old projects/iterarors and compilators/iterarors and compilators/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public  List<string> Autors { get; set; }
+
+        public Book(string title, int year, params string[] autors)
+        {
+            Title = title;
+            Year = year;
+            Autors = autors.ToList();
+        }
+    }
+}
