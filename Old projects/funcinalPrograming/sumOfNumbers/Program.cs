fcc5e68e725ca3b290779402c6eea7517bde8c27
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-06 13:59:36 +0300

    Create Program.cs

---
+++ Old projects/funcinalPrograming/sumOfNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]array=Console.ReadLine().Split(',').Select(n=>n=n.Trim()).Select(n => int.Parse(n)).ToArray();
+            
+           int sum= array.Sum();
+            int lenght= array.Length;
+            Console.WriteLine(lenght );
+            Console.WriteLine(sum);
+        }
+    }
+}
