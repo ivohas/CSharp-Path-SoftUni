4894b203b44977af87104300a27fb198ab5d7163
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-08 19:08:45 +0300

    Create Program.cs

---
+++ Old projects/list exersise/exam zad1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            string price = Console.ReadLine();
+         
+            while (price != "special" || price != "regular")
+            {
+
+                double add = double.Parse(price);
+                
+                sum += add;
+                price = Console.ReadLine();
+            }
+            
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {sum:f2}$");
+                double taxes = 0;
+                taxes = sum / 5;
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                double allsum = taxes + sum;
+                if (price == "special")
+                {
+                    allsum = allsum - allsum / 10;
+                }
+                Console.WriteLine($"Total price: {allsum:f2}");
+            
+
+
+        }
+    }
+}
