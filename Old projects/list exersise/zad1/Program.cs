f4be77f566da98b5b6cb3ddf5d831681d2578361
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-08 19:09:20 +0300

    Create Program.cs

---
+++ Old projects/list exersise/zad1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity= int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            do
+            {
+                string[] parts = command.Split();
+                switch (parts[0])
+                {
+                    case "Add":
+                        int num= int.Parse(parts[1]);
+                        int place= list.Count+1;
+                        list.Insert (place, num);
+                        break;
+                    default:
+                        int numToAdd= int.Parse(parts[0]);
+                        for (int i = 0; i <= list.Count ; i++)
+                        {
+                            int numCap = capacity - list[i];
+                            if (numCap>=numToAdd)
+                            {
+                                int num1 = list[i] + numToAdd;
+                                list.RemoveAt (i);
+                                list.Insert (i, num1);
+                            }
+
+                        }
+                        
+                        break;
+                }
+
+                command = Console.ReadLine();
+
+            } while (command != "end");
+            Console.WriteLine(String.Join(" ", list));
+        }
+    }
+}
