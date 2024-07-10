8ac66f5c540625b648e27aaebc7e89cd42724381
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-10 12:11:26 +0300

    Create Program.cs

---
+++ Old projects/funcinalPrograming/Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Func<string,string> sirs = x => "Sir " + x;
+            Console.WriteLine(string.Join("\r\n",Console.ReadLine().Split().Select(sirs).ToArray()));
+        }
+    }
+}
