8a00fc7b7f375553176ae18cfc0d86fbb2735c34
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-10 12:11:53 +0300

    Create Program.cs

---
+++ Old projects/list exersise/zad5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> bombPower = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < number.Count; i++)
+            {
+                if (number[i] == bombPower[0])
+                {
+                    for (int j = 1; j <= bombPower[1]; j++)
+                    {
+                        if (i - j < 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            number[i - j] = 0;
+                        }
+                    }
+
+                    for (int j = 1; j <= bombPower[1]; j++)
+                    {
+                        if (i + j > number.Count - 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            number[i + j] = 0;
+                        }
+                    }
+
+                    number[i] = 0;
+                }
+
+            }
+
+            int sum = number.Sum();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
