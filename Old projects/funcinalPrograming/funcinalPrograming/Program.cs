dae05fbadd3d492f70e4875b11ccc35f325edca8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-11 18:30:51 +0300

    Create Program.cs

---
+++ Old projects/funcinalPrograming/funcinalPrograming/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcinalPrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>integers= Console.ReadLine().Split(',').Select(n=>int.Parse(n)).Where(x => x % 2 == 0) .OrderBy(x=>x).ToList();
+            Console.WriteLine(String.Join(", ",integers));
+            
+        }
+    }
+}
