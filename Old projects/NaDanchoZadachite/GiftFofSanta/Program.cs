a129faa0c83da31c4d4b0a538a3cb14a899062ae
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-11 18:31:13 +0300

    Create Program.cs

---
+++ Old projects/NaDanchoZadachite/GiftFofSanta/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SantaGifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int S = int.Parse(Console.ReadLine());
+
+            for (int i = M; i >= N; i--)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    if (i == S)
+                    {
+                        break;
+                    }
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
