3540132262389465b6e654b505d83be4fca75eb8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-11 18:31:28 +0300

    Create Program.cs

---
+++ Old projects/NaDanchoZadachite/SpaceShips/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace SpacecraftProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double length = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double averageAstronautHeight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double spacecraftVolume = width * length * height;
+            double astronautRoomVolume = (averageAstronautHeight + 0.4) * 2 * 2;
+            int astronautsCount = (int)(spacecraftVolume / astronautRoomVolume);
+
+            if (astronautsCount >= 3 && astronautsCount <= 10)
+            {
+                Console.WriteLine($"The spacecraft holds {astronautsCount} astronauts.");
+            }
+            else if (astronautsCount < 3)
+            {
+                Console.WriteLine("The spacecraft is too small.");
+            }
+            else
+            {
+                Console.WriteLine("The spacecraft is too big.");
+            }
+        }
+    }
+}
