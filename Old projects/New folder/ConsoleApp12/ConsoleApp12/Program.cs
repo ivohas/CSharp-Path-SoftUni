6bb58621536b69a75eda7ed97a611c941a4b145a
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-23 21:59:55 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { string text= Console.ReadLine();
+            while (text != "Stop")
+            {
+               Console.WriteLine(text);
+                text= Console.ReadLine();
+                
+            }
+           
+            
+             
+            
+        }
+    }
+}
