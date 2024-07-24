b2fa7b0caae33065810d2ea2d0cac77bc4c713dc
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-24 21:22:10 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;int b = a;
+            while (n >= b)
+            {Console.WriteLine(b);
+                b = b * 2 + 1;  
+            }
+           
+
+
+        }
+    }
+}
