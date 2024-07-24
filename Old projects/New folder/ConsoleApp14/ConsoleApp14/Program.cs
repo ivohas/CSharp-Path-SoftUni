d6eb48f18f5e9513b4f363721baee8ae987e0a57
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-24 21:22:19 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();double strana = double.Parse(Console.ReadLine());  ;double b; double razmer;
+;            switch (a)
+            {
+                case "square":
+                    razmer = strana * strana;
+                    Console.WriteLine(razmer);
+                    break;
+                case "rentagular":
+                    b= double.Parse(Console.ReadLine());
+                    razmer = (strana * b) ;
+                    Console.WriteLine(razmer);
+                    break;
+                case "circle":
+                    razmer = strana * Math.PI  ;
+                    Console.WriteLine(razmer );
+                    break;
+                case "triagle":
+                    b = double.Parse(Console.ReadLine());
+                    razmer = (strana * b)/2;
+                    Console.WriteLine(razmer);
+                    break;
+                    default:break;
+            }
+        }
+    }
+}
