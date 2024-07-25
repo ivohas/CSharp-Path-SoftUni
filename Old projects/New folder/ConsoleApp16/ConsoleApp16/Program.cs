021e44e82c0c8c5295055c900fe603e16d6536aa
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-25 20:40:28 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+            double statues =(double)rent- ((double)rent * 30 / 100);
+            double food= (double)statues-((double)statues  * 15 / 100);
+            double sound = (double)food / 2;
+            double sum= rent+ statues + food+ sound;
+            Console.WriteLine(sum);
+
+        }
+    }
+}
