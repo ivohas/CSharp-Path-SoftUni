5d5a8eb15a77effe1bfab9c56bcb2ac0fd897362
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-26 20:09:51 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+            var dalzhinarabotnimesata = Math.Round(width / 1.20);
+            var shirochinarabotnimesta = Math.Round((height - 1) / 0.70);
+            var zaguba = 3;
+
+            var obsto = (dalzhinarabotnimesata * shirochinarabotnimesta) - 3;
+
+            Console.WriteLine($"{obsto}");
+        }
+    }
+}
