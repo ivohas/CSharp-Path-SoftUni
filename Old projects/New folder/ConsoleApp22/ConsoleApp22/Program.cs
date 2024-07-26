b8d0d0cf001e8cdf49ef6cfdc50bad7b6dc3ed02
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-26 20:09:10 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age= int.Parse(Console.ReadLine());
+            double grade= double.Parse(Console.ReadLine());
+            Console.WriteLine($" Name: {name}, Age: {age}, Grade: {grade:f2}");
+        }
+    }
+}
