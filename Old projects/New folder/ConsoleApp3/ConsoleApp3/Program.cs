30f7e56277edda325e4c5aa8b02d2c63f78717ed
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-26 20:09:24 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         double a =double.Parse( Console.ReadLine ());
+            double pr = 7.61;
+            double b = a * pr;
+            double c = b * 0.18;
+            double d = b - c;
+            Console.WriteLine("The final price is: " + d);
+            Console.WriteLine("The discount is: "+ c);
+        }
+    }
+}
