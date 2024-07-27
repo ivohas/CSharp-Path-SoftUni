4b747ff232ba1bf24cee4b33d0899d41f12c2d94
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-27 16:54:30 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    double inc= double.Parse(Console.ReadLine());
+            double sm = 0;
+            sm= inc * 2.54;
+            Console.WriteLine(sm);
+        }
+    }
+}
