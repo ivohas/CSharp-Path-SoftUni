ee6c8ec81d13a4be98e1fb0e7016afe0553cdbf7
Ivaylo Hasov <ivailohasov@gmail.com>
2024-07-28 13:27:30 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var s= int.Parse (Console.ReadLine());
+            if (s<=10)
+            {
+                Console.WriteLine("slow");
+            }
+            else
+            {
+                if (s <= 50) { Console.WriteLine("average"); } else
+                {
+                    if (s<=150)
+                    {
+                        Console.WriteLine("ultra fast");
+                    }
+                    else
+                    {
+                        if (s<=1000)
+                        {
+                            Console.WriteLine("ultra fast");
+                        }
+                        else
+                        {
+                            Console.WriteLine("extremely fast ");
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
