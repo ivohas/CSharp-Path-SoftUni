6a8a3377c4527faaba8c9eaf57b02f77b237f27b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-02 18:32:24 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int h=0;
+            int m=0;
+            int a=0;
+            do
+            {
+                m++;
+                if (m==60)
+                {
+                    h++;
+                    a = m % 60;
+                    Console.WriteLine(h+":"+a);
+                }
+                Console.WriteLine(h+":"+m);
+            } while (h != 24);
+        }
+    }
+}
