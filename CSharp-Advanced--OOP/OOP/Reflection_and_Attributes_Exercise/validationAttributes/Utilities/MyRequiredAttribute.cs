d54467c6e28e17451a46d205b5db5f5d0e812524
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-03 00:34:02 +0300

    Create MyRequiredAttribute.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/validationAttributes/Utilities/MyRequiredAttribute.cs
@@ -0,0 +1,18 @@
+﻿using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public class MyRequiredAttribute : MyValidationAtribute
+    {
+        public override bool IsValid(object obj)
+        {
+            if (obj is string str)
+            {
+                return !string.IsNullOrEmpty(str);
+            }
+
+            return obj != null;
+        }
+    }
+}
+
