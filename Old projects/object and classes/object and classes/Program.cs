803c8496abb1ad5711cb266edac824d8fd7a32e4
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-03 00:34:57 +0300

    Create Program.cs

---
+++ Old projects/object and classes/object and classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_and_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
