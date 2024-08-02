72b640de78fcf044ea85fa91ce315340adc786af
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-03 00:35:09 +0300

    Create Program.cs

---
+++ Old projects/ParkingLot/ParkingLot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parking = new HashSet<string>();
+            while (true)
+            {
+                string number = Console.ReadLine();
+                if (number == "END")
+                {
+                    break;
+                }
+                var tokens = number.Split(',').ToList();
+                string command = tokens[0];
+                string carNum = tokens[1];
+                if (command == "IN")
+                {
+                    parking.Add(carNum);
+                }
+                else
+                {
+                    parking.Remove(carNum);
+                }
+
+
+            }
+            if (parking.Count > 0)
+            {
+                foreach (var num in parking)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
