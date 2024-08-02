cc4b08f6ed9c944ea492a7bfc23984c69e94a83b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-02 18:32:14 +0300

    Create Program.cs

---
+++ Old projects/New folder/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Vavedi chas:");
+            //  Console.WriteLine("Vavedi minuti:");
+            // int ch = int.Parse(Console.ReadLine());
+            // int min=int.Parse(Console.ReadLine());
+            // int m = min + 15;
+          //  int a;
+          //  if (min>=60)
+           // {
+           //     ch++;
+               //  a= min%60;
+          //  }
+           // Console.WriteLine(ch+":"+a);
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a==b&&a==c)
+            {
+                Console.WriteLine("yes");
+            }
+            else { Console.WriteLine("no"); }
+        }
+    }
+}
