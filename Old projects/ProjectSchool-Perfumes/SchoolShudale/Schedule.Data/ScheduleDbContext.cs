1b480e19d08b48d14f8f4d1c4c0157a8b0f319ec
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-06 20:41:17 +0300

    Create ScheduleDbContext.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/Schedule.Data/ScheduleDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BookFindingAndRatingSystem.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Data.Models;
+
+namespace Schedule.Data
+{
+    public class ScheduleDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public ScheduleDbContext(DbContextOptions<ScheduleDbContext> options)
+           : base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        // db sets
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUserLogin<Guid>>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey });
+        }
+    }
+}
