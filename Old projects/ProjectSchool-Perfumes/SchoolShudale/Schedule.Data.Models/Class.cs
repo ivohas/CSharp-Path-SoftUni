4671883c0c6bc2bfdfceac79f3e92101f81b3afd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-06 20:38:44 +0300

    Create Class.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/Schedule.Data.Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using BookFindingAndRatingSystem.Data.Models;
+
+namespace Schedule.Data.Models
+{
+    public class Class
+    {
+        public Class()
+        {
+            Students = new List<ApplicationUser>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<ApplicationUser> Students { get; set; }
+        public ICollection<Teacher> Teacher { get; set; }
+    }
+}
