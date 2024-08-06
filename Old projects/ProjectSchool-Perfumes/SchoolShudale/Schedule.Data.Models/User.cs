801c0611cdcd33c86892de6bc17c046d8efd151e
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-06 20:39:29 +0300

    Create User.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/Schedule.Data.Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace BookFindingAndRatingSystem.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        
+        public string? ImageUrl { get; set; }
+    }
+}
