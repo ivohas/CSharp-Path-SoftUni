9b5ba7451ad6099db5bc8dbcc423b14aa0f373fa
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-06 20:39:34 +0300

    Create 20230928071211_ConnectionBetweenClassTeacher.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/Schedule.Data/Migrations/20230928071211_ConnectionBetweenClassTeacher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schedule.Data.Migrations
+{
+    public partial class ConnectionBetweenClassTeacher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClassTeacher",
+                columns: table => new
+                {
+                    ClassesId = table.Column<int>(type: "int", nullable: false),
+                    TeacherId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassTeacher", x => new { x.ClassesId, x.TeacherId });
+                    table.ForeignKey(
+                        name: "FK_ClassTeacher_Classes_ClassesId",
+                        column: x => x.ClassesId,
+                        principalTable: "Classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClassTeacher_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClassTeacher_TeacherId",
+                table: "ClassTeacher",
+                column: "TeacherId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassTeacher");
+        }
+    }
+}
