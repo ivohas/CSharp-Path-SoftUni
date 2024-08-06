3ce84b5a7c19627e178f1e39c3b2f976c240e526
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-06 20:39:25 +0300

    Create Subject.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/Schedule.Data.Models/Subject.cs
@@ -0,0 +1,10 @@
+﻿namespace Schedule.Data.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public Teacher Teacher { get; set; } = null!;
+    }
+}
