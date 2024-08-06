498e6856daaecf005c44bb85949e4b1027408d55
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-06 20:39:27 +0300

    Create Teacher.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/Schedule.Data.Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿namespace Schedule.Data.Models
+{
+    public class Teacher
+    {
+        public Teacher()
+        {
+            this.Id = Guid.NewGuid();
+            this.Subjects = new List<Subject>();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Subject> Subjects { get; set; }
+        public ICollection<Class> Classes { get; set; }
+        public int NeededHours { get; set; }
+    }
+}
