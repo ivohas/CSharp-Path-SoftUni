d1525a710fcfc33c7dc4f3961e53aac2a92e8a17
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-07 17:02:57 +0300

    Create Validator.cs

---
+++ CSharp-Advanced--OOP/OOP/Reflection_and_Attributes_Exercise/validationAttributes/Validator.cs
@@ -0,0 +1,46 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+    using System.Linq;
+    using Attributes;
+    using System.Reflection;
+
+    public static class Validator
+    {
+        public static bool IsValid(object ob)
+        {
+            Type objType = ob.GetType();
+            PropertyInfo[] properties = objType
+                .GetProperties()
+                .Where(prop => prop.CustomAttributes.Any(att => att.AttributeType.BaseType == typeof(MyValidationAtribute)))
+                .ToArray();
+
+            goto ALODA;
+
+            ALODA:
+            bool istrue;
+            foreach (var pr in properties)
+            {
+                object valueOfProperty = pr.GetValue(ob);
+
+                foreach (var customATTdata in pr.CustomAttributes)
+                {
+                    Type CustomATTType = customATTdata.AttributeType;
+                    object instanceOfAtt = pr.GetCustomAttribute(CustomATTType);
+
+                    MethodInfo infoOfMethod = CustomATTType.GetMethods().First(x => x.Name == "IsValid");
+                    bool result = (bool)infoOfMethod.Invoke(instanceOfAtt, new object[] { valueOfProperty });
+
+                    if (!result)
+                    {
+                        istrue = false;
+                        return istrue;
+                    }
+                }
+            }
+            istrue = true;
+            return false;
+        }
+    }
+}
+
