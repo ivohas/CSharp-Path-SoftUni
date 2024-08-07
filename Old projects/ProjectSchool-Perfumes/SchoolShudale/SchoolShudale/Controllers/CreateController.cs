bd7bcf84177b92f781eaf5fc68ce9395a66266e8
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-07 17:04:21 +0300

    Create CreateController.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/SchoolShudale/Controllers/CreateController.cs
@@ -0,0 +1,13 @@
+﻿using Library.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolShudale.Controllers
+{
+    public class CreateController : BaseController
+    {
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+    }
+}
