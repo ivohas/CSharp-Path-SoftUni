c657209c973137e2e1f08f660c08169b63449857
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-07 17:04:17 +0300

    Create BaseController.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/SchoolShudale/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+namespace Library.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        protected string? GetUserId()
+        {
+            string? id = string.Empty;
+            if (User != null)
+            {
+                id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+            return id;
+        }
+
+        //protected bool IsAdmin()
+        //{
+        //    return User?.IsInRole(AdminRoleName) ?? false;
+        //}
+    }
+}
