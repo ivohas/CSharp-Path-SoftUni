d1e0f23b6abd3de398f674891847ecfb63ce33dd
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-08 20:57:28 +0300

    Create HomeController.cs

---
+++ Old projects/ProjectSchool-Perfumes/SchoolShudale/SchoolShudale/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Library.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using SchoolShudale.Models;
+using System.Diagnostics;
+
+namespace SchoolShudale.Controllers
+{
+    public class HomeController : BaseController
+    {       
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int statusCode)
+        {
+            if (statusCode == 400 || statusCode == 404)
+            {
+                //TempData[ErrorMessage] = "Page not found or invalid request.";
+                return View("Error404");
+            }
+            if (statusCode == 401)
+            {
+                //TempData[ErrorMessage] = "Unauthorized access.";
+                return View("Error401");
+            }
+
+           // TempData[ErrorMessage] = "An error occurred.";
+            return View();
+        }
+    }
+}
