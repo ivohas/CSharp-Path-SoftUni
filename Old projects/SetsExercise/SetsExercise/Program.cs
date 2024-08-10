0296c34fa4dc9b81ccaa8ad975b20e9c5f5f7c3b
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-10 12:27:39 +0300

    Create Program.cs

---
+++ Old projects/SetsExercise/SetsExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+            foreach (string name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
