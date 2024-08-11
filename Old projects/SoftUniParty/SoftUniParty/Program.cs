c0ac5c559a374e6392f9d0c6487899b706e54e45
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-11 16:29:48 +0300

    Create Program.cs

---
+++ Old projects/SoftUniParty/SoftUniParty/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names= new HashSet<string>();
+            var reservanion = new HashSet<string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command=="END")
+                {
+                    break;
+                }
+                if (command=="PARTY")
+                {
+                    foreach (var item in names)
+                    {
+                        reservanion.Add(item);
+                    }
+                    names.Clear();
+                }
+                names.Add(command);
+
+            }
+            foreach (var item in names)
+            {
+                if (reservanion.Contains(item))
+                {
+                    names.Remove(item);
+                }
+                
+            }
+            int lenght= names.Count;
+            Console.WriteLine(names);
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
