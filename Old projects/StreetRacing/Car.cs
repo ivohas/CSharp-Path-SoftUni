b750871dac81e7d86667b76a38c0a730ee5c2382
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-11 16:29:30 +0300

    Create Car.cs

---
+++ Old projects/StreetRacing/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            Make = make;
+            Model = model;
+            LicensePlate = licensePlate;
+            HorsePower = horsePower;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+                      
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}")
+                .AppendLine($"Model: {Model}").
+                AppendLine($"License Plate: {LicensePlate}")
+                .AppendLine($"Horse Power: {HorsePower}")
+                .Append($"Weight: {Weight}");
+
+            return sb.ToString();
+        }
+    }
+}
