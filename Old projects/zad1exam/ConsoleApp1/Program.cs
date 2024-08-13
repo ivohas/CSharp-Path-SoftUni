552aa1160d7a4911b70e03a41e7ac772d8623454
Ivaylo Hasov <ivailohasov@gmail.com>
2024-08-13 22:36:51 +0300

    Create Program.cs

---
+++ Old projects/zad1exam/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.BossRush
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfIndicates = int.Parse(Console.ReadLine());
+            Regex pathern = new Regex(@"|(?<names>[A-Z{4,}]+)|:#(?<titles>[A-Za-z]+ [A-Za-z]+)#");
+            string indicate = null;
+            for (int i = 0; i < numOfIndicates; i++)
+            {
+                indicate = Console.ReadLine();
+                Match match = pathern.Match(indicate);
+                if (match.Success)
+                {
+                    string names = match.Groups["names"].Value;
+                    string titl = match.Groups["titles"].Value;
+                    int power = match.Groups["name"].Value.Length;
+                    int armor = match.Groups["title"].Value.Length;
+
+                    Console.WriteLine($"{names}, The {titl}");
+                    Console.WriteLine($">> Strength: {power}");
+                    Console.WriteLine($">> Armor: {armor}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
