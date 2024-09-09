187fd769d7adbb91b4b7d4f4913784fb98f20de1
Ivaylo Hasov <ivailohasov@gmail.com>
2024-09-09 23:13:48 +0300

    Create Program.cs

---
+++ Old projects/SetsExercise/count sim/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_sim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            char[] chars = a.ToCharArray();
+            SortedDictionary<char,int> dict=new SortedDictionary<char,int>();
+            foreach (char item in chars)
+            {
+                if(!dict.ContainsKey(item))
+                    dict.Add(item, 0);
+                dict[item]++;
+            }
+            foreach (var item in dict) 
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
